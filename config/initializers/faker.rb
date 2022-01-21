a1ee6f410c041563ea512d0e5cf6cd63d59712fe
Rob Nichols <rob@undervale.co.uk>
2022-01-21 14:26:16 +0000

    Prevent Faker being called in production mode

--- config/initializers/faker.rb
+++ config/initializers/faker.rb
@@ -1,2 +1,4 @@
-# Set faker to GB region so that setting, such as postcode, work as expected
-Faker::Config.locale = "en-GB"
+if Rails.env.test? || Rails.env.development?
+  # Set faker to GB region so that setting, such as postcode, work as expected
+  Faker::Config.locale = "en-GB"
+end
