83878834fbf11878727af4916286204b68dae4b1
Rob Nichols <rob@undervale.co.uk>
2022-02-01 16:44:18 +0000

    Fix registration failure after sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   resources :training_modules, only: [:index]
   resources :extra_registrations, only: [:index, :edit, :update]
 end
