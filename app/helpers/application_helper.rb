645618fa66076710c9890370a8832e6b9b66949e
Brett McHargue <brett@therubytroubadour.com>
2022-03-15 15:28:44 +0000

    GOVUK designs for remaining forms, add fix for devise / turbo to work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
   def navigation
     govuk_header(service_name: "GOV.UK Early Years Foundation Recovery") do |header|
-      header.navigation_item(text: "Home", href: root_path, active: true)
+      header.navigation_item(text: "Home", href: root_path)
       header.navigation_item(text: "Training Modules", href: training_modules_path)
       if user_signed_in?
+        header.navigation_item(text: "Account", href: edit_user_registration_path)
+        header.navigation_item(text: "Profile", href: edit_user_path)
         header.navigation_item(text: "Sign out", href: destroy_user_session_path, options: { data: { turbo_method: :delete }})
       else
         header.navigation_item(text: "Sign in", href: new_user_session_path)
