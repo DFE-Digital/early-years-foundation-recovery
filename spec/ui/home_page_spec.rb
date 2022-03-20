51bb7bd5ce809636cadebd5b78119f88da5f6b4b
Ade Oke <ade@pitpatpet.com>
2022-03-20 02:42:18 +0000

    removed spec_helper from root .rspec file this however means all files will need to require on first lie, which is undesirable.

--- spec/ui/home_page_spec.rb
+++ spec/ui/home_page_spec.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
+require_relative '../ui_auto_helper'
 
 describe Pages::Home do
   context 'Dfe Homepage' do
-    before(:each) do
-      @home_page = Pages::Home.new
-    end
+    let(:home_page) { Pages::Home.new }
+    let(:sign_in) { Pages::SignIn.new }
 
     it 'Should be displayed when the user navigates to the hame page and clicks the header logo' do
-      @home_page.load
-      @home_page.header.logo.click
+      home_page.load
+      home_page.header.logo.click
 
-      expect(@home_page).to be_displayed
+      expect(home_page).to be_displayed
     end
 
     it 'should navigate away from the home page when the user clicks sign in' do
-      @home_page.load
-      @home_page.header.sign_in.click
-      @sign_in = Pages::SignIn.new
-      @sign_in.wait_until_header_visible
+      home_page.load
+      home_page.header.sign_in.click
 
-      expect(@sign_in).to be_displayed
+      sign_in.wait_until_header_visible
+
+      expect(sign_in).to be_displayed
     end
   end
 end
