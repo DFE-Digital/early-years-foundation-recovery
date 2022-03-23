21d7657f216ed89522b2591e467bfe788104e3be
Ade Oke <ade@pitpatpet.com>
2022-03-23 14:24:44 +0000

    resolving rubocop issues

--- spec/ui/home_page_spec.rb
+++ spec/ui/home_page_spec.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
+
 require_relative '../ui_auto_helper'
 
-describe Pages::Home do
-  context 'Dfe Homepage' do
+describe 'Dfe' do
+  context 'when on the homepage' do
     let(:home_page) { Pages::Home.new }
     let(:sign_in) { Pages::SignIn.new }
 
-    it 'is displayed when the user navigates to the home page and clicks the header logo' do
+    it 'clicking the homepage logo redirects to the homepage' do
       home_page.load
       home_page.header.logo.click
 
       expect(home_page).to be_displayed
     end
 
-    it 'navigates away from the home page when the user clicks sign in' do
+    it 'clicking the sign link navigates away from the home page' do
       home_page.load
       home_page.header.sign_in.click
 
