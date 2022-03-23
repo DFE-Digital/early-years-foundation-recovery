c342ca168bc54f1747fc47d9808129599588303c
Ade Oke <ade@pitpatpet.com>
2022-03-23 14:05:57 +0000

    chrome options for headless amended for linux distros

--- ui_automation/drivers/chrome.rb
+++ ui_automation/drivers/chrome.rb
@@ -16,6 +16,12 @@ module Drivers
 
       options.add_preference(:browser, set_download_behavior: { behavior: 'allow' })
       Capybara.register_driver :headless_chrome do |app|
+        # options.add_argument("start-maximized"); // open Browser in maximized mode
+        # options.add_argument("disable-infobars"); // disabling infobars
+        options.add_argument("--disable-extensions")#; // disabling extensions
+        options.add_argument("--disable-gpu")#; // applicable to windows os only
+        options.add_argument("--disable-dev-shm-usage")#; // overcome limited resource problems
+        options.add_argument("--no-sandbox");
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_argument('--window-size=1280,800')
