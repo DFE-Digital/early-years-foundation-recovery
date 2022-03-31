5d34664f8cbe8906d5ae8acee3c3855f4c8b14f0
tunde-anddigital <borimogunje@and.digital>
2022-03-31 15:42:29 +0100

    fix missing end statements

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -11,27 +11,28 @@ RSpec.describe "User", type: :request do
       get user_path
       expect(response).to have_http_status(:success)
     end
+  end
 
   describe 'GET /user/edit' do
     it 'renders page' do
       get edit_user_path
       expect(response).to have_http_status(:success)
     end
+  end
 
-    describe "patch /user" do
-      let(:update_user) do
-        patch user_path, params: { user: { first_name: first_name } }
-      end
+  describe "patch /user" do
+    let(:update_user) do
+      patch user_path, params: { user: { first_name: first_name } }
+    end
 
-      let(:first_name) { "Foo-Bar" }
+    let(:first_name) { "Foo-Bar" }
 
-      it "updates the user" do
-        expect { update_user }.to change(user.reload, :first_name).to(first_name)
-      end
+    it "updates the user" do
+      expect { update_user }.to change(user.reload, :first_name).to(first_name)
+    end
 
-      it "redirects back to the show page" do
-        expect { update_user }.to redirect_to(user_path)
-      end
+    it "redirects back to the show page" do
+      expect { update_user }.to redirect_to(user_path)
     end
   end
 
