54f8de9901fa5d5bb1f34c858dcd2d4c51a180b6
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-03-31 14:54:38 +0100

    remove trailing comma

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :timeoutable, :trackable, :recoverable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable,
-         :validatable, :rememberable, :confirmable, :lockable,
+         :validatable, :rememberable, :confirmable, :lockable
 
   def name
     [first_name, last_name].compact.join(' ')
