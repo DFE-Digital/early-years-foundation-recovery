475ecae7d8f7edd0a0ec062f079b142448cb7738
Rob Nichols <rob@undervale.co.uk>
2022-03-31 11:20:46 +0100

    Seed a user when a password is defined in the credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Seed data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+Seeder = Dibber::Seeder
+
+Seeder.seed(:user, name_method: :email) if Rails.application.credentials.user_seed_password.present?
+
+puts Seeder.report # rubocop:disable Rails/Output
