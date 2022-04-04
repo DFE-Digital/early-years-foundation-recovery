fce40d9545d499931e0776f7eabb44734af8a946
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-04-04 12:22:22 +0100

    another rubocop change

--- db/migrate/20220331135702_add_lockable_to_devise.rb
+++ db/migrate/20220331135702_add_lockable_to_devise.rb
@@ -3,7 +3,7 @@ class AddLockableToDevise < ActiveRecord::Migration[7.0]
     change_table :users, bulk: true do |t|
       t.integer :failed_attempts, default: 0, null: false
       t.datetime :locked_at
-      
+
       t.string :unlock_token
       t.index :unlock_token
     end
