454736106391eaabe365de0206037508a72a36da
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2022-04-04 16:11:23 +0100

    Er 120 add account unlock devise email (#66)
    * add unlock instructions to notify_mailer
    
    * update notify mailer template id
    
    * configure devise to use lockable
    
    * remove trailing comma
    
    * add lockable, migrate
    
    * add spec for sending correct mailer
    
    * resolved merge conflict
    
    * configure devise to use lockable
    
    * remove trailing comma
    
    * add lockable, migrate
    
    * add spec for sending correct mailer
    
    * rubocop changes
    
    * another rubocop change
    
    * change specs following feedback
    
    * rubocop changes
    
    * replace first with contain_exactly

---
+++ db/migrate/20220331135702_add_lockable_to_devise.rb
@@ -0,0 +1,11 @@
+class AddLockableToDevise < ActiveRecord::Migration[7.0]
+  def change
+    change_table :users, bulk: true do |t|
+      t.integer :failed_attempts, default: 0, null: false
+      t.datetime :locked_at
+
+      t.string :unlock_token
+      t.index :unlock_token
+    end
+  end
+end
