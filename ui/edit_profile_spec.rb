0ddbd6f096fb8aacab5e1fb97794e33f36c1333d
Ade Oke <ade@pitpatpet.com>
2022-04-06 11:56:06 +0100

    happy path ER1 testing is completed

--- ui/edit_profile_spec.rb
+++ ui/edit_profile_spec.rb
@@ -3,15 +3,15 @@
 require_relative '../uat_helper'
 
 describe 'Edit Profile' do
-  # let(:dfe) { Dfe.new }
   let(:home) { Pages::Home.new }
   let(:sign_in) { Pages::SignIn.new }
   let(:profile) { Pages::Profile.new }
   let(:user) { Pages::User.new }
 
-  before(:each) do
+  before do
     home.load
     home.header.sign_in.click
+
     sign_in.wait_until_header_visible
     sign_in.email.set ENV['EMAIL']
     sign_in.password.set ENV['PASS']
@@ -23,7 +23,7 @@ describe 'Edit Profile' do
     profile.wait_until_first_name_visible
   end
 
-  after(:each) do
+  after do
     # Capybara.current_session.driver.quit
     browser = Capybara.current_session.driver.browser
     browser.manage.delete_all_cookies
@@ -31,7 +31,6 @@ describe 'Edit Profile' do
 
   context 'when an existing user' do
     it 'then the first name alone is updated when requested' do
-
       expected_first_name = Faker::Name.first_name
       profile.first_name.set expected_first_name
       profile.update.click
@@ -58,10 +57,57 @@ describe 'Edit Profile' do
     end
 
     it 'then the postcode alone is updated when requested' do
-      fail
+      expected_post_code = Faker::Address.postcode
+      profile.post_code.set expected_post_code
+      profile.update.click
+
+      user.displayed?
+      user.wait_until_edit_visible
+      user.edit.click
+
+      actual_post_code = profile.post_code.value
+      expect(actual_post_code).to eq(expected_post_code)
     end
 
-    it 'then the ofsted nuumber alone is updated when requested'
-    it 'then the changes to the first, last, postcode and ofsted number are updated when requested'
+    it 'then the ofsted nuumber alone is updated when requested' do
+      expected_ofsted_number = Faker::Number.number(digits: 10).to_s
+      profile.ofsted_number.set expected_ofsted_number
+      profile.update.click
+
+      user.displayed?
+      user.wait_until_edit_visible
+      user.edit.click
+
+      actual_ofsted_number = profile.ofsted_number.value
+      expect(actual_ofsted_number).to eq(expected_ofsted_number)
+    end
+
+    it 'then the changes to the first name, last name, postcode and ofsted number are updated when requested', {wiper: true}  do
+      expected_first_name = Faker::Name.first_name
+      expected_last_name = Faker::Name.last_name
+      expected_post_code = Faker::Address.postcode
+      expected_ofsted_number = Faker::Number.number(digits: 10).to_s
+
+      profile.first_name.set expected_first_name
+      profile.last_name.set expected_last_name
+      profile.post_code.set expected_post_code
+      profile.ofsted_number.set expected_ofsted_number
+
+      profile.update.click
+
+      user.displayed?
+      user.wait_until_edit_visible
+      user.edit.click
+
+      actual_first_name = profile.first_name.value
+      actual_last_name = profile.last_name.value
+      actual_post_code = profile.post_code.value
+      actual_ofsted_number = profile.ofsted_number.value
+
+      expect(actual_first_name).to eq(expected_first_name)
+      expect(actual_last_name).to eq(expected_last_name)
+      expect(actual_post_code).to eq(expected_post_code)
+      expect(actual_ofsted_number).to eq(expected_ofsted_number)
+    end
   end
 end
