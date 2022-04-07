a9cdc178085313a56124a3e95cd5a5ee10ad9595
Rob Nichols <rob@undervale.co.uk>
2022-04-07 16:14:22 +0100

    Define user timeout minutes in controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,4 +1,7 @@
 class ErrorsController < ApplicationController
   def not_found; end
-  def timeout; end
+
+  def timeout
+    @user_timeout_minutes = Rails.configuration.x.user_timeout_minutes
+  end
 end
