273b21853d6659e31d6f0c428715da3f900d2702
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-04-11 10:23:01 +0100

    fix merge conflicts

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,7 +1,7 @@
 class PasswordsController < Devise::PasswordsController
   protected
 
-  def after_sending_reset_password_instructions_path_for(resource_name)
+  def after_sending_reset_password_instructions_path_for(_resource_name)
     check_email_user_path
   end
 end
