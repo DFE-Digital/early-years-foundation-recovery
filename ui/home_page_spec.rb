85ff5683624cc40ed295a9867ca536d901c252cd
Ade Oke <adebola.oke@digital.education.gov.uk>
2022-04-11 14:15:15 +0100

    ER-1 Automation completed.

--- ui/home_page_spec.rb
+++ ui/home_page_spec.rb
@@ -4,23 +4,22 @@ require_relative './spec_helper'
 
 describe 'home_page' do
   context 'when unauthenticated' do
-    let(:home_page) { Pages::Home.new }
-    let(:sign_in) { Pages::SignIn.new }
+    let(:dfe) { Dfe.new }
 
     before { home_page.load }
 
     it 'is displayed when the user navigates to the home page and clicks the header logo' do
-      home_page.header.logo.click
+      dfe.home_page.header.logo.click
 
-      expect(home_page).to be_displayed
+      expect(dfe.home_page).to be_displayed
     end
 
     it 'navigates away from the home page when the user clicks sign in' do
-      home_page.header.sign_in.click
+      dfe.home_page.header.sign_in.click
 
-      sign_in.wait_until_header_visible
+      dfe.sign_in.wait_until_header_visible
 
-      expect(sign_in).to be_displayed
+      expect(dfe.sign_in).to be_displayed
     end
   end
 end
