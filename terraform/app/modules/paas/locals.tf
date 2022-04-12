72ff98aaad22a67335a317458bbd9e9414313b1e
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-04-12 07:39:51 +0100

    domain

--- terraform/app/modules/paas/locals.tf
+++ terraform/app/modules/paas/locals.tf
@@ -1,17 +1,17 @@
 # ------------------------------------------------------------------------------
 locals {
-  # HOSTNAME only
-  app_env_domain = {
-    "DOMAIN" = "${var.web_app_name}.london.cloudapps.digital"
 
-    # plek
-    # "GOVUK_APP_DOMAIN"   = "eyfs-${var.environment}.london.cloudapps.digital"
-    # "GOVUK_WEBSITE_ROOT" = "eyfs-${var.environment}.london.cloudapps.digital"
+  # TODO: calculate domain using "is_review" etc
+  # DOMAIN values
+  # These should not be in the module and are app specific
+  app_env_values = {
+    "DOMAIN"             = "${var.web_app_name}.london.cloudapps.digital"
+    "GOVUK_APP_DOMAIN"   = "${var.web_app_name}.london.cloudapps.digital"
+    "GOVUK_WEBSITE_ROOT" = "${var.web_app_name}.london.cloudapps.digital"
   }
 
-  # NB: because of merge order, if present,
-  # the value of DOMAIN in .tfvars will overwrite app_env_domain
-  app_environment = merge(local.app_env_domain, var.app_env_values)
+  # Combine module with main module variables which take precedence
+  app_environment = merge(local.app_env_values, var.app_env_values)
 
   app_cf_service_instances = [cloudfoundry_service_instance.postgres_instance.id]
   app_service_bindings     = concat(local.app_cf_service_instances)
