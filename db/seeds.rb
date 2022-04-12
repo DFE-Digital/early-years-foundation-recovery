af20286f0dd70c82319018e29a77b0027406741f
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-04-12 17:11:33 +0100

    Check for seeded user in deployed app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # Seed data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 Seeder = Dibber::Seeder
 
-Seeder.seed(:user, name_method: :email) if Rails.application.credentials.user_seed_password.present?
+Seeder.seed(:user, name_method: :email) if ENV['DEMO_USER_PASSWORD'].present?
 
 puts Seeder.report # rubocop:disable Rails/Output
