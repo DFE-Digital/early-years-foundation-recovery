a6df1fb48889836072615040c53b9c2759306aba
tunde-anddigital <borimogunje@and.digital>
2022-04-13 23:29:06 +0100

    minor corrections

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/404', to: 'errors#not_found', via: :all
-  get 'users/timeout', to: 'errors#timeout'
   get '/500', to: 'errors#internal_server_error', via: :all
+  get 'users/timeout', to: 'errors#timeout'
   get 'health', to: 'home#show'
 
   devise_for :users, controllers: { sessions: 'users/sessions' }
