118ccb3b5ce2a1d99b259d92587d0de7c6f6f301
tunde-anddigital <50167725+tunde-anddigital@users.noreply.github.com>
2022-04-14 13:24:32 +0100

    Er 143 problem with service error (#76)
    * add internal server error page and test
    
    * add single quotes and remove some content
    
    * remove rails static 500 page
    
    * fix rubocop linting issues
    
    * minor corrections
    
    * wording change
    
    * fix for trivy security issue
    
    * upgrade busybox - fix for trivy security issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/404', to: 'errors#not_found', via: :all
+  get '/500', to: 'errors#internal_server_error', via: :all
   get 'users/timeout', to: 'errors#timeout'
   get 'health', to: 'home#show'
 
