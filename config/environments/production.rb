4d93fcd8e978c219681e519a3d3449e7a2c00c1d
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-04-28 15:32:44 +0100

    Start to consolidate ENV VARs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   # Configure default url for email
   # Needs to be set as the URL for the production app.
-  config.action_mailer.default_url_options = { host: ENV['APP_URL'] }
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
