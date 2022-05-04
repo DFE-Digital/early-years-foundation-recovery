867b3a90f719f5b484ab9051d923dff9f61c5408
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-05-04 16:50:18 +0100

    Abandon attempt to include govuk_component_publishing assets
    Will need to be resolved if Govspeak content is to be rendered nicely,
    ideally by automatically compiling vendor assets in situ.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,10 +6,10 @@ import { initAll } from "govuk-frontend";
 /*
 WIP: asset pipeline needs the following from the gem
 https://github.com/alphagov/govuk_publishing_components/blob/main/docs/install-and-use.md
-*/
 import "govuk_publishing_components/dependencies";
 import "govuk_publishing_components/lib";
 import "govuk_publishing_components/components/button";
+*/
 
 
 initAll();
