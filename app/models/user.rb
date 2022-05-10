0e1073f9c956b22783a48a386432705814249d0a
Brett McHargue <brett@therubytroubadour.com>
2022-05-10 16:03:19 +0100

    Add "Resume" to profile page for training modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
   has_many :visits, class_name: 'Ahoy::Visit'
   has_many :events, class_name: 'Ahoy::Event'
 
+  def last_page_for(training_module:)
+    events.where_properties(training_module_id: training_module.name).last&.properties
+  end
+
+  def started?(training_module:)
+    last_page_for(training_module:).present?
+  end
+
   def name
     [first_name, last_name].compact.join(' ')
   end
