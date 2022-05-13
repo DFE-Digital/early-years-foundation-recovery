6152be65865f04f1549319b82a54a5ef6c4628bd
Peter Hamilton <pete@peterdavidhamilton.com>
2022-05-13 14:55:46 +0100

    Validate user provided OFSTED numbers using regexp (#115)
    8 digits or 6 preceded by either "ey" or "vc"

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :visits, class_name: 'Ahoy::Visit'
   has_many :events, class_name: 'Ahoy::Event'
 
+  validates_with OfstedValidator
+
   def name
     [first_name, last_name].compact.join(' ')
   end
