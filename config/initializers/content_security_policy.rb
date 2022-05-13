ca457fa37277ec8d7e9ee1109a4c557b667ded0f
mohammed Aziz <mo_zag@yahoo.co.uk>
2022-05-13 16:22:10 +0100

    extended Govspeak markdown
    extended Govspeak markdown to include youtube embed.
    Added content policy to allow js files to be processed by browsers for CSP.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,23 +4,82 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-Rails.application.configure do
-  config.content_security_policy do |policy|
-    policy.default_src :self, :https
-    policy.font_src    :self, :https, :data
-    policy.img_src     :self, :https, :data
-    policy.object_src  :none
-    policy.script_src  :self, :https
-    policy.style_src   :self, :https
-    # Specify URI for violation reports
-    # policy.report_uri "/csp-violation-report-endpoint"
+# See https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP for more CSP info.
+#
+# The resulting policy should be checked with:
+#
+# - https://csp-evaluator.withgoogle.com
+# - https://cspvalidator.org
+
+GOVUK_DOMAINS = [
+  "*.education.gov.uk",
+].uniq.freeze
+
+GOOGLE_ANALYTICS_DOMAINS = %w[www.google-analytics.com
+                              ssl.google-analytics.com
+                              stats.g.doubleclick.net
+                              www.googletagmanager.com
+                              *.ytimg.com
+                              www.youtube.com
+                              www.youtube-nocookie.com
+                              *.hotjar.com].freeze
+
+OPTIMIZE_DOMAINS = %w[www.googleoptimize.com
+                      optimize.google.com
+                      fonts.googleapis.com].freeze
+
+GOOGLE_STATIC_DOMAINS = %w[fonts.gstatic.com www.gstatic.com *.hotjar.com].freeze
+
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https, *GOVUK_DOMAINS
+  policy.font_src    :self, :https, *GOVUK_DOMAINS, *GOOGLE_STATIC_DOMAINS, :data
+  policy.frame_src   :self, *GOOGLE_ANALYTICS_DOMAINS, *OPTIMIZE_DOMAINS
+  policy.img_src     :self,
+                     *GOVUK_DOMAINS,
+                     *GOOGLE_ANALYTICS_DOMAINS, # Tracking pixels
+                     *OPTIMIZE_DOMAINS,
+                     :data # Base64 encoded images
+  policy.object_src  :none
+  policy.script_src  :self,
+                     *GOVUK_DOMAINS,
+                     *GOOGLE_ANALYTICS_DOMAINS,
+                     *GOOGLE_STATIC_DOMAINS,
+                     *OPTIMIZE_DOMAINS,
+                     # Allow YouTube Embeds (Govspeak turns YouTube links into embeds)
+                     "*.ytimg.com",
+                     "www.youtube.com",
+                     "www.youtube-nocookie.com",
+                     # Allow all inline scripts until we can conclusively
+                     # document all the inline scripts we use,
+                     # and there's a better way to filter out junk reports
+                     :unsafe_inline
+  policy.style_src   :self, *GOVUK_DOMAINS, *GOOGLE_STATIC_DOMAINS, *OPTIMIZE_DOMAINS, :unsafe_inline
+  if Rails.env.development?
+    # :nocov: by definition will not run in test
+    policy.connect_src :self,
+                       :https,
+                       :wss,
+                       *GOVUK_DOMAINS,
+                       *GOOGLE_ANALYTICS_DOMAINS,
+                       "http://localhost:3035",
+                       "ws://localhost:3035"
+    # :nocov:
+  else
+    policy.connect_src :self,
+                       :https,
+                       :wss,
+                       *GOVUK_DOMAINS,
+                       *GOOGLE_ANALYTICS_DOMAINS
   end
-
-  # Generate session nonces for permitted importmap and inline scripts
-  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  # config.content_security_policy_nonce_directives = %w(script-src)
-
-  # Report CSP violations to a specified URI. See:
-  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-  # config.content_security_policy_report_only = true
 end
+
+# If you are using UJS then enable automatic nonce generation
+# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+
+# Set the nonce only to specific directives
+# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
+
+# Report CSP violations to a specified URI
+# For further information see the following documentation:
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
+# Rails.application.config.content_security_policy_report_only = true
