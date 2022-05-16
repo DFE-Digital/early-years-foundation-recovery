e99ff02fa6c154713d7e6d8d8cc93d946f086b97
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-05-16 15:36:35 +0100

    Append "db" to create unique service name

--- terraform/app/modules/paas/locals.tf
+++ terraform/app/modules/paas/locals.tf
@@ -2,11 +2,6 @@
 locals {
   app_cf_service_instances = [cloudfoundry_service_instance.postgres_instance.id]
   app_service_bindings     = concat(local.app_cf_service_instances)
-
-  # ey-recovery-db-production
-  # ey-recovery-db-staging
-  # ey-recovery-db-pr-123
-  db_service_name = "${var.service_name}-db-${var.web_app_name}"
-
-  db_json_params = jsonencode(var.postgres_json_params)
+  db_service_name          = "${var.web_app_name}-db"
+  db_json_params           = jsonencode(var.postgres_json_params)
 }
