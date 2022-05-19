3ab1532ddb5fdd40621ecdeed4f2ce5c80fca0aa
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-05-19 16:46:47 +0100

    Update validation to permit 6 digit Ofsted numbers

--- app/validators/ofsted_number_validator.rb
+++ app/validators/ofsted_number_validator.rb
@@ -1,11 +1,11 @@
-# This might start with EY, VC followed by 6 numbers, or it might be an 8 letter number.
-# @see locale: extra_registration.setting.ofsted_hint
+# This might start with EY, VC followed by 6 numbers,
+# or it might be a 6 or 8 digit number.
 #
 class OfstedNumberValidator < ActiveModel::Validator
   def validate(record)
     return if record.ofsted_number.blank?
 
-    unless record.ofsted_number.match? %r{\A((ey|vc)\d{6}|\d{8})\z}i
+    unless record.ofsted_number.match? %r{\A((ey|vc)\d{6}|\d{6,8})\z}i
       record.errors.add :ofsted_number,
                         I18n.t('activerecord.errors.models.user.attributes.ofsted_number.invalid')
     end
