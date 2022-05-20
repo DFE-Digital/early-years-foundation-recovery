b79c4f0c3f2296f9def08b62842fae5d58e2bc55
tunde-anddigital <borimogunje@and.digital>
2022-05-20 10:09:04 +0100

    fix rubocop issues

--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Static page', type: :request do
-    specify { expect('/static/terms_and_conditions').to be_successful }
+  specify { expect('/static/terms_and_conditions').to be_successful }
 
-    specify { expect('/static/accessibility_statement').to be_successful }
+  specify { expect('/static/accessibility_statement').to be_successful }
 
-    specify { expect('/static/privacy_policy').to be_successful }
+  specify { expect('/static/privacy_policy').to be_successful }
 
-    specify { expect('/users/timeout').to be_successful }
+  specify { expect('/users/timeout').to be_successful }
 
-    specify { expect('/404').to be_successful }
+  specify { expect('/404').to be_successful }
 
-    specify { expect('/500').to be_successful }
+  specify { expect('/500').to be_successful }
 end
--- spec/support/static_page_matcher.rb
+++ spec/support/static_page_matcher.rb
@@ -1,10 +1,10 @@
-RSpec::Matchers.define :be_successful do |path|
+RSpec::Matchers.define :be_successful do
   match do |path|
     get path
-    expect(response).to have_http_status(:success) 
+    expect(response).to have_http_status(:success)
   end
 
   description do
-    "to render and return http status success."
+    'to render and return http status success.'
   end
 end
