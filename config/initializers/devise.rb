154ee8470d954bd1dae370a913ca2673f7468c2f
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2022-05-24 16:19:45 +0100

    Er 214 account locked for 2 hours (#123)
    * add option set to unlock time as environment variable
    
    * let devise accept env variable as rails duration
    
    * add rubocop changes
    
    * simplify code following feedback

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -265,7 +265,7 @@ Devise.setup do |config|
   config.maximum_attempts = 6
 
   # Time interval to unlock the account if :time is enabled as unlock_strategy.
-  config.unlock_in = 2.hours
+  config.unlock_in = ENV.fetch('UNLOCK_IN_MINUTES', 2 * 60).to_i.minutes
 
   # Warn on the last attempt before the account is locked.
   config.last_attempt_warning = true
