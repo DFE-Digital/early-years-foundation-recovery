cfc204728f02e2a65296362278376a41e251cc68
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-05-30 11:40:16 +0100

    - only render login messages to unauthenticated users on the homepage

---
+++ spec/system/registered_user/front_page_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Front page', type: :system do
+  context 'with an authenticated user' do
+    include_context 'with user'
+
+    it 'logged in content' do
+      visit '/'
+      expect(page).to have_text 'Learn more'
+      expect(page).not_to have_text 'Learn more and enrol'
+      expect(page).not_to have_text 'Sign in to continue learning'
+    end
+  end
+
+  context 'with an unauthenticated visitor' do
+    it 'log in content' do
+      visit '/'
+      expect(page).to have_text('Learn more and enrol')
+        .and have_text('Sign in to continue learning')
+    end
+  end
+end
