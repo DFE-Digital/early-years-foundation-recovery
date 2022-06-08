fbe11a098324fe2ab154f7e28ecd56c0b66b10dd
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-06-08 09:14:38 +0100

    - handle attempt to change password to BLANK which Devise ignores - track the attempt - return a validation error - add spec coverage

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -3,55 +3,63 @@ require 'rails_helper'
 RSpec.describe UserController, type: :controller do
   describe '#update_password' do
     let(:user) { create :user, :registered }
+    let(:params) { {} }
 
     before do
       sign_in user
+      post :update_password, params: { user: params }
     end
 
     context 'when successful' do
-      it 'redirects' do
-        post :update_password, params: {
-          user: {
-            password: 'NewPassword123',
-            confirm_password: 'NewPassword123',
-            current_password: 'StrongPassword123',
-          },
+      let(:params) do
+        {
+          password: 'NewPassword123',
+          confirm_password: 'NewPassword123',
+          current_password: 'StrongPassword123',
         }
+      end
+
+      it 'redirects' do
         expect(response).to have_http_status(:redirect)
       end
 
-      it 'sets notice' do
-        post :update_password, params: {
-          user: {
-            password: 'NewPassword123',
-            confirm_password: 'NewPassword123',
-            current_password: 'StrongPassword123',
-          },
-        }
+      it 'renders a flash notice' do
         expect(flash[:notice]).to match(/Your password has been reset/)
       end
     end
 
-    context 'when unsuccesful' do
-      it 'renders edit' do
-        post :update_password, params: {
-          user: {
-            password: 'NewPassword123',
-            confirm_password: 'NewPassword123',
-            current_password: 'wrongpassword',
-          },
+    context 'when current password is wrong' do
+      let(:params) do
+        {
+          password: 'NewPassword123',
+          confirm_password: 'NewPassword123',
+          current_password: 'wrongpassword',
         }
+      end
+
+      it 'renders edit' do
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
-      it 'sets notice' do
-        post :update_password, params: {
-          user: {
-            password: 'NewPassword123',
-            confirm_password: 'NewPassword123',
-            current_password: 'wrong',
-          },
+      it 'does not render a flash notice' do
+        expect(flash[:notice]).to be_nil
+      end
+    end
+
+    context 'when new password is blank' do
+      let(:params) do
+        {
+          password: '',
+          confirm_password: '',
+          current_password: 'StrongPassword123',
         }
+      end
+
+      it 'renders edit' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'does not render a flash notice' do
         expect(flash[:notice]).to be_nil
       end
     end
