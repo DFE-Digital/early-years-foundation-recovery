e99178d7414b21c26e5bc43131f50ee5a288ed77
tunde-anddigital <borimogunje@and.digital>
2022-06-10 10:03:17 +0100

    hyphenate routes in test

--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Static page', type: :request do
-  specify { expect('/terms_and_conditions').to be_successful }
+  specify { expect('/terms-and-conditions').to be_successful }
 
-  specify { expect('/accessibility_statement').to be_successful }
+  specify { expect('/accessibility-statement').to be_successful }
 
-  specify { expect('/privacy_policy').to be_successful }
+  specify { expect('/privacy-policy').to be_successful }
 
   specify { expect('/contact').to be_successful }
 
