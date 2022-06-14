47968f172c8aa1e80a90d76f961dfc47877fc3bc
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-06-14 11:15:43 +0100

    fix merge conflicts in specs

--- spec/system/all_users_signing_in_spec.rb
+++ spec/system/all_users_signing_in_spec.rb
@@ -31,28 +31,6 @@ RSpec.describe 'Sign in', type: :system do
       expect(page).to have_text('Warning')
         .and have_text('Please ensure you have entered your valid email address and password.')
     end
-
-    it 'provides link when user forgets password' do
-      click_link 'I have forgotten my password', visible: false
-
-      expect(page).to have_text('I have forgotten my password')
-
-      fill_in 'Email', with: user.email
-      click_button 'Send email'
-
-      expect(page).to have_text('Check your email')
-    end
-
-    it 'provides back button to homepage' do
-      visit '/users/password/new'
-      fill_in 'Email', with: user.email
-      click_button 'Send email'
-
-      expect(page).to have_text('Check your email')
-      click_link 'Back'
-      
-      expect(page).to have_current_path(new_user_session_path)
-    end
   end
 
   context 'when user is confirmed' do
