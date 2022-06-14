22ecb47a3a2f54aab23741386937c9e1b9a1bb11
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-06-14 17:18:51 +0100

    fix merge conflicts

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,7 +2,6 @@ class ConfirmationsController < Devise::ConfirmationsController
   protected
   
   def after_resending_confirmation_instructions_path_for(resource_name)
-    # TODO: add check email
-    # check_email_confirmation_user_path
+    check_email_confirmation_user_path
   end
 end
