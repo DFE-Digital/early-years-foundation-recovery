defc8439cecacacea5a0d3ddc11e093cc481ad4e
Brett McHargue <brett@therubytroubadour.com>
2022-06-15 12:13:43 +0100

    Updates to bring UI specs inline with current UI

--- ui/pages/terms_and_conditions.rb
+++ ui/pages/terms_and_conditions.rb
@@ -2,7 +2,7 @@
 
 module Pages
   class TermsAndConditions < Base
-    set_url '/static/terms_and_conditions'
+    set_url '/terms-and-conditions'
 
     element :heading, 'h1', text: 'Terms and conditions'
   end
