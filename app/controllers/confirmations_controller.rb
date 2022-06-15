413695912452b7f06f11484e0ea9a606d5fa5334
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-06-15 10:41:20 +0100

    update to helper method to avoid hyphen/underscore issue, rubocop changes

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,7 +1,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
-  protected
-  
-  def after_resending_confirmation_instructions_path_for(resource_name)
+protected
+
+  def after_resending_confirmation_instructions_path_for(_resource_name)
     check_email_confirmation_user_path
   end
 end
--- spec/system/registration_journey_spec.rb
+++ spec/system/registration_journey_spec.rb
@@ -11,36 +11,36 @@ RSpec.describe 'Following registration journey' do
     context 'and can click on "I haven\'t receieved the email" link' do
       it 'taken to "resend your confirmation" page' do
         click_on 'Send me another email', visible: :hidden
-        
-        expect(page.current_path).to eq(new_user_confirmation_path)
+
+        expect(page).to have_current_path(new_user_confirmation_path, ignore_query: true)
         expect(page).to have_text('Resend your confirmation')
       end
     end
-      
+
     context 'and can click on the "I don\'t have access to the email" link' do
       it 'I am taken to the ‘create your account’ page to re-register' do
         click_on 'create a new account', visible: :hidden
-        
-        expect(page.current_path).to eq(new_user_registration_path)
+
+        expect(page).to have_current_path(new_user_registration_path, ignore_query: true)
         expect(page).to have_text('Create an early years training account')
       end
     end
-      
+
     context 'and can click on the ‘other problems’ link' do
       it 'taken to the ‘MS form to contact us’' do
         expect(page).to have_link 'contact us', href: '#', visible: :hidden
       end
     end
-    
+
     context 'and navigate to "resend your confirmation"' do
       it 'can click on a back button to be taken to the "check your email" page' do
         click_on 'Send me another email', visible: :hidden
         click_on 'Go back to check your email'
-        expect(page).to have_text("Check your email")
+        expect(page).to have_text('Check your email')
       end
     end
   end
-  
+
   context 'when on the ‘resend your confirmation’ page' do
     before do
       visit new_user_confirmation_path
@@ -48,12 +48,11 @@ RSpec.describe 'Following registration journey' do
 
     context 'and can enter email address and click send' do
       it 'taken to the confirmation email sent page' do
-        fill_in "Email address", with: user.email
+        fill_in 'Email address', with: user.email
         click_on 'Send email'
 
-        expect(page).to have_text("Check your email")
+        expect(page).to have_text('Check your email')
       end
     end
-    
   end
 end
