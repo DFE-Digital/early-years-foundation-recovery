f40afb47bc470c7cf79d67d9cd08913326fa01ae
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-06-15 12:10:39 +0100

    change to url helper methods, remove unused commented code, fix failing spec

--- spec/system/locked_account_spec.rb
+++ spec/system/locked_account_spec.rb
@@ -2,41 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'User has locked account' do
   let(:user) { create :user, :confirmed }
-  # returns unlock token
-  let(:token) { user.lock_access! }
-  
-  context 'When the link in the email is clicked' do
-    it 'Then it unlocks the account and shows the sign in page' do
-      visit "/users/unlock?unlock_token=#{token}"
+  let!(:token) { user.lock_access! }
 
-      # fill_in 'Email address', with: user.email
-      # fill_in 'Password', with: 'StrongPassword123'
-      # click_button 'Sign in'
+  context 'when the link in the email is clicked' do
+    it 'then it unlocks the account and shows the sign in page' do
+      visit user_unlock_path + "?unlock_token=#{token}"
 
-      expect(page).to have_current_path('/users/sign_in')
+      expect(page).to have_current_path(new_user_session_path)
         .and have_content('Your account has been unlocked successfully. Please sign in to continue')
-      # expect(user.access_locked?).to eq false
     end
   end
 
-  context 'When the link in the email is clicked a second time' do
-    it 'Then an error message is displayed' do
-      visit "/users/unlock?unlock_token=#{token}"
-      visit "/users/unlock?unlock_token=#{token}"
+  context 'when the link in the email is clicked a second time' do
+    it 'then an error message is displayed' do
+      visit user_unlock_path + "?unlock_token=#{token}"
+      visit user_unlock_path + "?unlock_token=#{token}"
 
       expect(page).to have_content('The link you followed has expired')
     end
   end
-  
-  context 'When a user waits for their account to be unlocked' do
-    context 'And then locks their account again by entering the wrong password' do
-      it 'Then an error message is displayed' do
-        user.unlock_access!
-        user.lock_access!
-  
-        visit "/users/unlock?unlock_token=#{token}"
-  
-        expect(page).to have_content('The link you followed has expired')
+
+  context 'when a user waits for their account to be unlocked' do
+    context 'and then locks their account again by entering the wrong password' do
+      context 'and then clicks on the old unlock link in their inbox' do
+        it 'then an error message is displayed' do
+          user.unlock_access!
+
+          user.lock_access!
+
+          visit user_unlock_path + "?unlock_token=#{token}"
+
+          expect(page).to have_content('The link you followed has expired')
+        end
       end
     end
   end
