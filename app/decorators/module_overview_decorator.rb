e54adc1ab0dfa4228996e367aee74456c3bfdade
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-06-21 16:15:27 +0100

    refactor

--- app/decorators/module_overview_decorator.rb
+++ app/decorators/module_overview_decorator.rb
@@ -21,7 +21,7 @@ class ModuleOverviewDecorator < DelegateClass(ModuleProgress)
       [
         num,                                  # submodule digit
         items.first.model.heading,            # submodule intro heading
-        prev_sub_heading_text(num),           # previous submodule heading
+        previous_submodule_heading_text(num), # previous submodule heading
         status(items),                        # symbol  - all items in the submodule
         topics(submodule: num, items: items), # Array(String, Symbol)
       ]
@@ -65,7 +65,7 @@ private
     topic_status = status(topic_item.current_submodule_topic_items)
 
     if clickable?(topic_item: topic_item, submodule: submodule)
-      next_item_name = topic_status.eql?(:started) ? milestone : topic_item
+      next_item_name = topic_status.eql?(:started) ? resume_page : topic_item
     end
 
     [
@@ -84,14 +84,20 @@ private
   #
   # @return [Boolean] completed or previous mod completed
   def clickable?(submodule:, topic_item:)
-    _previous_topic = previous_topic(topic_item.topic_name, submodule)
-    _previous_topic_items = _previous_topic.values.first.to_a
+    # call to action must be used first
+    return false unless all?([
+      topic_item.parent.interruption_page,
+      topic_item.parent.intro_page
+    ])
 
-    _previous_submodule = prev_submodule(topic_item.submodule_name)
-    _previous_submodule_items = _previous_submodule.values.first.to_a
+    previous_topic = find_previous_topic(topic_item.topic_name, submodule)
+    previous_topic_items = previous_topic.values.first.to_a
 
-    if _previous_topic && _previous_submodule
-      all?(_previous_topic_items) && all?(_previous_submodule_items)
+    previous_submodule = find_previous_submodule(topic_item.submodule_name)
+    previous_submodule_items = previous_submodule.values.first.to_a
+
+    if previous_topic && previous_submodule
+      all?(previous_topic_items) && all?(previous_submodule_items)
     else
       all?([topic_item])
     end
@@ -103,11 +109,18 @@ private
     (num.to_i - 1).to_s
   end
 
+  # @return [Array<Array>] ['1'] [item, item, item]
+  def find_previous_submodule(num)
+    mod.items_by_submodule.select do |sub_num, _|
+      previous(num).eql?(sub_num)
+    end
+  end
+
   # @param current_top_num [?]
   # @param current_sub_num [?]
   #
   # @return [Array<Array>] [1,2] [item, item, item]
-  def previous_topic(current_top_num, current_sub_num)
+  def find_previous_topic(current_top_num, current_sub_num)
     mod.items_by_topic.select do |(sub_num, topic_num), _items|
       sub_num.eql?(current_sub_num) && previous(current_top_num).eql?(topic_num)
     end
@@ -116,17 +129,10 @@ private
   # @param num [?]
   #
   # @return [?]
-  def prev_sub_heading_text(num)
-    prev_submodule(num).map { |_, prev_items|
+  def previous_submodule_heading_text(num)
+    find_previous_submodule(num).map { |_, prev_items|
       prev_items.first.model.heading
     }.first
-  end
-
-  # @return [Array<Array>] ['1'] [item, item, item]
-  def prev_submodule(num)
-    mod.items_by_submodule.select do |sub_num, _|
-      previous(num).eql?(sub_num)
-    end
   end
 
   #
