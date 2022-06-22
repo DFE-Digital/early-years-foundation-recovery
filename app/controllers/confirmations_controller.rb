43581555398ebdd7ba327f80c18ff02217d723fe
Brett McHargue <brett@therubytroubadour.com>
2022-06-22 17:27:15 +0100

    Rubocop cleanup

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,20 +1,19 @@
 class ConfirmationsController < Devise::ConfirmationsController
-
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
     yield resource if block_given?
 
     if resource.errors.empty?
       set_flash_message!(:notice, (resource.registration_complete ? :confirmed : :activated))
-      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+      respond_with_navigational(resource) { redirect_to after_confirmation_path_for(resource_name, resource) }
     else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+      respond_with_navigational(resource.errors, status: :unprocessable_entity) { render :new }
     end
   end
 
 protected
 
-  def after_confirmation_path_for(resource_name, resource)
+  def after_confirmation_path_for(resource_name, _resource)
     if signed_in?(resource_name)
       user_path
     else
