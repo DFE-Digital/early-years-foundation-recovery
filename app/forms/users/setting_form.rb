4285d11d8a7349bb6c669a293c24d22d4224d18c
Brett McHargue <brett@therubytroubadour.com>
2022-06-24 10:21:33 +0100

    Add hints

--- app/forms/users/setting_form.rb
+++ app/forms/users/setting_form.rb
@@ -2,9 +2,9 @@ module Users
   class SettingForm < BaseForm
     attr_accessor :postcode, :ofsted_number, :setting_type, :setting_type_other
 
+    validates :setting_type, presence: true
     validates :ofsted_number, ofsted_number: true
     validates :postcode, presence: true, postcode: true
-    validates :setting_type, presence: true
 
     def save
       if valid?
