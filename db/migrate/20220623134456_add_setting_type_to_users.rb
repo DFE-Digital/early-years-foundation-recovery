aa22f8ffb24407da1b9358854ef0f1a4f7ea29cd
Brett McHargue <brett@therubytroubadour.com>
2022-06-24 09:28:46 +0100

    Rubocop prefers change_table

--- db/migrate/20220623134456_add_setting_type_to_users.rb
+++ db/migrate/20220623134456_add_setting_type_to_users.rb
@@ -1,6 +1,8 @@
 class AddSettingTypeToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :setting_type, :string
-    add_column :users, :setting_type_other, :string
+    change_table :users, bulk: true do |t|
+      t.string :setting_type
+      t.string :setting_type_other
+    end
   end
 end
