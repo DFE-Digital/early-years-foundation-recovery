1dc01207fd81e22fe998c73ae1bb5d8cb2838302
mohammed Aziz <mo_zag@yahoo.co.uk>
2022-06-29 18:40:13 +0100

    Interim password

--- lib/seed_interim_users.rb
+++ lib/seed_interim_users.rb
@@ -4,7 +4,7 @@ class SeedInterimUsers
     users = YAML.load_file(seed_file)
     users.each do |user_attributes|
       if !User.exists?(email: user_attributes['email']) && valid_email?(user_attributes['email'])
-        updated_attributes = user_attributes.merge(password: ENV.fetch('INTERIM_PASSWORD', 'StrongPassword'), confirmed_at: '2022-06-16 00:00:00')
+        updated_attributes = user_attributes.merge(password: ENV.fetch('INTERIM_PASSWORD', SecureRandom.base64(12)), confirmed_at: '2022-06-16 00:00:00')
         user = User.create!(updated_attributes)
         puts("User: #{user.email} succesfully created.")
         module_items = ModuleItem.where(training_module: 'child-development-and-the-eyfs')
