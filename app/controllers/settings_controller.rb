9f0cf3b93144cb74006b276197f09e6cad9112de
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-06-29 11:12:20 +0100

    add rubocop changes

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -11,13 +11,13 @@ class SettingsController < ApplicationController
 private
 
   def track_analytics
-    params.fetch(:track_analytics, "No")
+    params.fetch(:track_analytics, 'No')
   end
 
   def set_cookie_pref
     cookies[:track_analytics] = { value: track_analytics, expires: 6.months.from_now }
     if params.fetch(:notify_if_successful, false)
-      flash[:notice] = t("cookie.preferences_saved_html", return_url: helpers.root_path, scope: :settings)
+      flash[:notice] = t('cookie.preferences_saved_html', return_url: helpers.root_path, scope: :settings)
     end
   end
 end
--- spec/system/cookies_spec.rb
+++ spec/system/cookies_spec.rb
@@ -7,26 +7,26 @@ RSpec.describe 'Selecting cookie options' do
     end
 
     it 'display cookie banner' do
-      expect(page).to have_content("Cookies on Child development training")
+      expect(page).to have_content('Cookies on Child development training')
     end
 
     context 'when cookie banner is displayed' do
       it 'can click to read cookie policy' do
-        click_on "Read the cookie policy"
-        expect(page).to have_content("Cookies")
+        click_on 'Read the cookie policy'
+        expect(page).to have_content('Cookies')
           .and have_current_path('/settings/cookie-policy')
       end
 
       it 'can click to accept analytics cookies' do
-        click_on "Accept analytics cookies"
-        expect(cookies[:track_analytics]).to eq "No"
-        expect(page).not_to have_content("Cookies on Child development training")
+        click_on 'Accept analytics cookies'
+        expect(cookies[:track_analytics]).to eq 'No'
+        expect(page).not_to have_content('Cookies on Child development training')
       end
-      
+
       it 'can click to reject analytics cookies' do
-        click_on "Reject analytics cookies"
-        expect(cookies[:track_analytics]).to eq "Yes"
-        expect(page).not_to have_content("Cookies on Child development training")
+        click_on 'Reject analytics cookies'
+        expect(cookies[:track_analytics]).to eq 'Yes'
+        expect(page).not_to have_content('Cookies on Child development training')
       end
     end
   end
@@ -36,12 +36,12 @@ RSpec.describe 'Selecting cookie options' do
       cookies[:track_analytics] = 'No'
       visit setting_path('cookie-policy')
     end
-    
-    it 'can change cookie choice to accept' do
-      choose("Yes")
-      click_on "Save cookie settings"
 
-      expect(cookies[:track_analytics]).to eq "Yes"
+    it 'can change cookie choice to accept' do
+      choose('Yes')
+      click_on 'Save cookie settings'
+
+      expect(cookies[:track_analytics]).to eq 'Yes'
     end
   end
 
@@ -50,12 +50,12 @@ RSpec.describe 'Selecting cookie options' do
       cookies[:track_analytics] = 'Yes'
       visit setting_path('cookie-policy')
     end
-    
-    it 'can change cookie choice to reject' do
-      choose("No")
-      click_on "Save cookie settings"
 
-      expect(cookies[:track_analytics]).to eq "No"
+    it 'can change cookie choice to reject' do
+      choose('No')
+      click_on 'Save cookie settings'
+
+      expect(cookies[:track_analytics]).to eq 'No'
     end
   end
 end
