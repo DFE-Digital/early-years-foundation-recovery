157b068dc636ba1880b9fa5ff2ad1e1237d01c15
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-06-29 09:48:59 +0100

    add functionality to cookie banner, add form to bottom of cookie page to change decision

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,4 +2,22 @@ class SettingsController < ApplicationController
   def show
     render params[:id].underscore
   end
+
+  def create
+    set_cookie_pref
+    redirect_to params[:return_url]
+  end
+
+private
+
+  def track_analytics
+    params.fetch(:track_analytics, "No")
+  end
+
+  def set_cookie_pref
+    cookies[:track_analytics] = { value: track_analytics, expires: 6.months.from_now }
+    if params.fetch(:notify_if_successful, false)
+      flash[:notice] = t("cookie.preferences_saved_html", return_url: helpers.root_path, scope: :settings)
+    end
+  end
 end
