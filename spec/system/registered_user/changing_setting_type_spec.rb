309bab0d98cde56a6c77a9c9146f09ef0b255450
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-07-11 12:31:04 +0100

    update spec to include another acceptance criteria

--- spec/system/registered_user/changing_setting_type_spec.rb
+++ spec/system/registered_user/changing_setting_type_spec.rb
@@ -9,32 +9,37 @@ RSpec.describe 'Registered user changing setting type', type: :system do
     visit '/my-account/edit-setting-type'
   end
 
-  context 'when valid' do
+  context 'when I am in my account section and update my setting type' do
     let(:updated_setting_type) { 'nursery' }
 
-    it 'updates setting type' do
+    it 'can update successfully' do
       choose updated_setting_type.capitalize
       click_button 'Save'
 
       expect(page).to have_current_path '/my-account'
       expect(page).to have_text('Manage your account')
-        .and have_text('You have saved your details')
         .and have_text('Nursery')
+    end
+
+    it 'displays success message' do
+      choose updated_setting_type.capitalize
+      click_button 'Save'
+
+      expect(page).to have_text('You have saved your details')
     end
   end
 
-  context 'when other' do
+  context 'when updating setting type to other' do
     let(:updated_setting_type) { 'other' }
     let(:setting_type_other) { 'Foo' }
 
-    it 'updates setting type' do
+    it 'displays user entered setting' do
       choose updated_setting_type.capitalize
       fill_in 'Other setting', with: setting_type_other
       click_button 'Save'
 
       expect(page).to have_current_path '/my-account'
       expect(page).to have_text('Manage your account')
-        .and have_text('You have saved your details')
         .and have_text('Foo')
     end
   end
@@ -45,7 +50,6 @@ RSpec.describe 'Registered user changing setting type', type: :system do
 
       expect(page).to have_current_path '/my-account'
       expect(page).to have_text('Manage your account')
-        .and have_text('You have saved your details')
         .and have_text('School')
     end
   end
