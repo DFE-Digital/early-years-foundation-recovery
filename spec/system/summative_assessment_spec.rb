b80daf6871edf0e325a91b89837fe345272a6727
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-07-13 09:57:10 +0100

    add method for visiting each page before the summative assessment, add specs to test summative assessment, change name

---
+++ spec/system/summative_assessment_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe 'Assessment' do
+  include_context 'with progress'
+
+  let(:pass_test) do
+    visit training_module_summative_assessment_path('alpha', '1-3-2-1')
+    2.times do
+      choose '5'
+      click_on 'Save and continue'
+    end
+    choose '5'
+    click_on 'Finish test'
+  end
+
+  let(:fail_test) do
+    visit training_module_summative_assessment_path('alpha', '1-3-2-1')
+    2.times do
+      choose '4'
+      click_on 'Save and continue'
+    end
+    choose '4'
+    click_on 'Finish test'
+  end
+
+  before do
+    view_pages_before_summative_assessment(alpha)
+  end
+
+  include_context 'with user'
+
+  context 'when a user has visited each module up to and including a summative assessment' do
+    it 'can click to resume training to visit the summative assessment page' do
+      visit training_module_path('alpha')
+      click_on 'Resume training'
+      expect(page).to have_current_path training_module_summative_assessment_path('alpha', '1-3-2-1'), ignore_query: true
+    end
+  end
+
+  context 'when a user has passed the summative assessment' do
+    it 'reflect on learning link becomes clickable' do
+      pass_test
+      visit training_module_path('alpha')
+
+      expect(page).to have_link('Reflect on your learning')
+    end
+
+    it 'is not able to retake the assessment' do
+      pass_test
+      visit training_module_summative_assessment_path('alpha', '1-3-2-1')
+
+      expect(page).to have_selector('.govuk-radios__input:disabled')
+    end
+  end
+
+  context 'when a user has failed the summative assessment' do
+    it 'reflect on learning link remains unclickable' do
+      fail_test
+      visit training_module_path('alpha')
+
+      expect(page).to have_content('Reflect on your learning')
+      expect(page).not_to have_link('Reflect on your learning')
+    end
+
+    it 'is able to retake the assessment' do
+      fail_test
+      visit training_module_summative_assessment_path('alpha', '1-3-2-1')
+
+      expect(page).to have_selector('.govuk-radios__input')
+    end
+  end
+end
