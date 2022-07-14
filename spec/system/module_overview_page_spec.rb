7761e3e518cf455ccf7ca00326e23378407e7307
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-07-14 11:19:23 +0100

    add more spec coverage

--- spec/system/module_overview_page_spec.rb
+++ spec/system/module_overview_page_spec.rb
@@ -29,7 +29,59 @@ RSpec.describe 'Module overview page' do
       expect(page).to have_content('3 hours')
     end
 
+    it 'can see the submodule names' do
+      expect(page).to have_content('The first submodule')
+      expect(page).to have_content('The second submodule')
+      expect(page).to have_content('Summary and next steps')
+    end
 
+    it 'can see the topic names' do
+      expect(page).to have_content('1-1-1')
+        .and have_content('1-1-2')
+        .and have_content('1-1-3')
+        .and have_content('1-1-4')
+        .and have_content('1-2-1')
+        .and have_content('Recap')
+        .and have_content('End of module test')
+        .and have_content('Reflect on your learning')
+    end
+
+    it 'can see message telling user to complete previous submodule' do
+      expect(page).to have_content('The first submodule must be completed before you begin this section')
+        .and have_content('The second submodule must be completed before you begin this section')
+    end
+   
+    context 'when the user has not begun the module' do
+      it 'tag says that the module has not been started yet' do
+        within '#section-button-1' do
+          expect(page).to have_content('not started')
+        end
+
+        within '#section-content-1' do
+          expect('.govuk-list:first-child').to have_content('1-1-1')
+            .and have_content('not started')
+          expect('.govuk-list:first-child').not_to have_link('1-1-1')
+        end
+      end
+    end
+
+    context 'when the user has viewed the interruption page and module intro' do
+      before do
+        start_module(alpha)
+      end
+
+      it 'section tag says that the module has not been started yet' do
+        # section heading
+        within '#section-button-1' do
+          expect(page).to have_content('not started')
+        end
+        
+        # section content
+        within '#section-content-1' do
+          expect('.govuk-list:first-child').to have_link('1-1-1')
+          .and have_content('not started')
+        end
+      end
+    end
   end
-
 end
