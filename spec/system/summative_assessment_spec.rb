88f8acb75d5d18b3864326d84a735885fdfb8fdf
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-07-15 11:04:40 +0100

    add another question, add specs for getting 75% score, telling the user they have passed or failed, displaying incorrect answers

--- spec/system/summative_assessment_spec.rb
+++ spec/system/summative_assessment_spec.rb
@@ -17,11 +17,11 @@ RSpec.describe 'Summative assessment' do
     end
   end
 
-  context 'when a user has passed the summative assessment' do
-    # Pass the summative assessment
+  context 'when a user has passed the summative assessment with a score of 100%' do
+    # Pass the summative assessment with score of 100%
     before do
       visit '/modules/alpha/summative-assessments/1-3-2-1'
-      2.times do
+      3.times do
         check 'Correct answer 1'
         check 'Correct answer 2'
         click_on 'Save and continue'
@@ -32,6 +32,11 @@ RSpec.describe 'Summative assessment' do
 
     it 'displays the correct score' do
       expect(page).to have_content 'You scored 100%'
+    end
+
+    it 'displays message telling the user they have passed the summative assessment' do
+      expect(page).to have_content('Congratulations')
+        .and have_content('Congratulations - you have scored highly enough to receive a certificate of achievement for this module.')
     end
 
     it 'can navigate to confidence check from module overview page' do
@@ -47,11 +52,38 @@ RSpec.describe 'Summative assessment' do
     end
   end
 
+  context 'when a user has passed the summative assessment with a score of 75%' do
+    # Pass the summative assessment with score of 75%
+    before do
+      visit '/modules/alpha/summative-assessments/1-3-2-1'
+      2.times do
+        check 'Correct answer 1'
+        check 'Correct answer 2'
+        click_on 'Save and continue'
+      end
+      check 'Wrong answer 1'
+      check 'Wrong answer 2'
+      click_on 'Save and continue'
+
+      choose 'Correct answer 1'
+      click_on 'Finish test'
+    end
+
+    it 'displays the correct score' do
+      expect(page).to have_content 'You scored 75%'
+    end
+
+    it 'displays message telling the user they have passed the summative assessment' do
+      expect(page).to have_content('Congratulations')
+        .and have_content('Congratulations - you have scored highly enough to receive a certificate of achievement for this module.')
+    end
+  end
+
   context 'when a user has failed the summative assessment' do
     # Fail the summative assessment
     before do
       visit '/modules/alpha/summative-assessments/1-3-2-1'
-      2.times do
+      3.times do
         check 'Wrong answer 1'
         check 'Wrong answer 2'
         click_on 'Save and continue'
@@ -62,6 +94,18 @@ RSpec.describe 'Summative assessment' do
 
     it 'displays the correct score' do
       expect(page).to have_content 'You scored 0%'
+    end
+
+    it 'displays message telling the user they have failed the summative assessment' do
+      expect(page).to have_content('Revisit the module')
+        .and have_content('Unfortunately you have not scored highly enough to receive a certificate.')
+    end
+
+    it 'displays the incorrect answers' do
+      expect(page).to have_content('Question One')
+        .and have_content('Question Two')
+        .and have_content('Question Three')
+        .and have_content('Question Four')
     end
 
     it 'cannot navigate to confidence check from module overview page' do
