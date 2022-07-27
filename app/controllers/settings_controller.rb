d334030046119c0525e93b626c87f5a3fd9074c7
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-07-27 15:20:48 +0100

    update to more suitable method name

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,7 +4,7 @@ class SettingsController < ApplicationController
   end
 
   def create
-    set_cookie_pref
+    set_analytics_preference
     redirect_to params[:return_url]
   end
 
@@ -14,7 +14,7 @@ private
     params.fetch(:track_analytics, 'No')
   end
 
-  def set_cookie_pref
+  def set_analytics_preference
     cookies[:track_analytics] = { value: track_analytics, expires: 6.months.from_now }
     if params.fetch(:notify_if_successful, false)
       flash[:notice] = t('cookie.preferences_saved_html', return_url: helpers.root_path, scope: :settings)
