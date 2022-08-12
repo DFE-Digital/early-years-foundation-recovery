ec89503122d5dadca08813b93ccd29c04ca16705
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-08-12 08:56:50 +0100

    Improve link logic and remove unused code

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,4 +1,5 @@
-#
+# TODO: Combine Questionnaire/Question
+# TODO: Restructure YAML to remove #question_list
 #
 # submitted: boolean (answer)
 #
@@ -26,7 +27,7 @@ class Questionnaire < OpenStruct
   include ActiveModel::Validations
   include TranslateFromLocale
 
-  validate :validate_wrong_answers
+  validate :check_answers
 
   # @return [ModuleItem]
   def module_item
@@ -57,7 +58,9 @@ class Questionnaire < OpenStruct
     { current: page_number, total: total_questions }
   end
 
-  # @return [Array<Question>] list of question ostruct objects
+  # OPTIMIZE: There is only ever one question
+  #
+  # @return [Array<Question>]
   def question_list
     questions.map do |name, attrs|
       Question.new(attrs.merge(questionnaire: self, name: name))
@@ -66,17 +69,17 @@ class Questionnaire < OpenStruct
 
   # @return [Boolean]
   def formative?
-    assessments_type == 'formative_assessment'
+    assessments_type.eql?('formative_assessment')
   end
 
   # @return [Boolean]
   def summative?
-    assessments_type == 'summative_assessment'
+    assessments_type.eql?('summative_assessment')
   end
 
   # @return [Boolean]
   def confidence?
-    assessments_type == 'confidence_check'
+    assessments_type.eql?('confidence_check')
   end
 
   # @see QuestionnaireTaker#populate
@@ -88,7 +91,16 @@ class Questionnaire < OpenStruct
 
   # @return [Boolean] end of summative assessment
   def final_question?
-    module_item == module_item.parent.last_assessment_page
+    module_item.parent.last_assessment_page.eql?(module_item)
+  end
+
+  # @return [String]
+  def next_button_text
+    if summative?
+      final_question? ? 'Finish test' : 'Save and continue'
+    else
+      'Next'
+    end
   end
 
   # @param question [Question]
@@ -111,12 +123,9 @@ class Questionnaire < OpenStruct
   end
 
   # @return [String]
-  def summary_for(question)
-    if errors.key?(question.name.to_s)
-      questions.dig(question.name, :assessment_fail_summary)
-    else
-      questions.dig(question.name, :assessment_summary)
-    end
+  def summary_for(key)
+    summary = result_for(key) ? :assessment_summary : :assessment_fail_summary
+    questions.dig(key, summary)
   end
 
   # An attribute is permitted if it is defined in the questionnaire's YAML data
@@ -132,27 +141,13 @@ class Questionnaire < OpenStruct
 
 private
 
-  # Update validation errors to append "is wrong" to the question
-  def validate_wrong_answers
-    return if passed?
-
+  # Validations are not used in any logic currently
+  #
+  # @return [Hash{Symbol=>Boolean}]
+  def check_answers
     results.each do |key, result|
       errors.add(key, 'is wrong') unless result
     end
-  end
-
-  # @return [Boolean] true if threshold is undefined or exceeded
-  def passed?
-    if passmark.zero?
-      true
-    else
-      passmark <= percentage_of_answers_correct
-    end
-  end
-
-  # @return [Integer]
-  def passmark
-    required_percentage_correct.to_i
   end
 
   # @return [Hash{Symbol => Boolean}]
@@ -165,13 +160,6 @@ private
   # @return [Integer] default zero
   def number_of_correct_answers
     results.values.tally.fetch(true, 0)
-  end
-
-  # @return [Float]
-  def percentage_of_answers_correct
-    (number_of_correct_answers.to_f / questions.count) * 100
-  rescue ZeroDivisionError
-    0.0
   end
 
   # @param key [Symbol]
