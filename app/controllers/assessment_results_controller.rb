61ab86ef0ff986153c6f1f43e310c59a0fc58561
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-09-02 14:43:13 +0100

    use after_action

--- app/controllers/assessment_results_controller.rb
+++ app/controllers/assessment_results_controller.rb
@@ -1,6 +1,7 @@
 class AssessmentResultsController < ApplicationController
   before_action :authenticate_registered_user!
   before_action :clear_flash
+  after_action :track_events, only: :show
 
   def new
     helpers.assessment_progress(training_module).archive_attempt
@@ -10,13 +11,6 @@ class AssessmentResultsController < ApplicationController
   def show
     @assessment = helpers.assessment_progress(training_module)
     @module_item = ModuleItem.find_by(training_module: params[:training_module_id], name: params[:id])
-
-    if track_summative_assessment_complete?
-      track('summative_assessment_complete',
-            success: @assessment.passed?,
-            type: 'summative_assessment',
-            score: @assessment.score)
-    end
   end
 
 private
@@ -31,5 +25,14 @@ private
 
   def assessment_untracked?
     untracked?('summative_assessment_complete', training_module_id: params[:training_module_id])
+  end
+
+  def track_events
+    if track_summative_assessment_complete?
+      track('summative_assessment_complete',
+            success: @assessment.passed?,
+            type: 'summative_assessment',
+            score: @assessment.score)
+    end
   end
 end
