4c98a2dd061847f53f3c8b8c4e10051e806f075f
Brett McHargue <brett@therubytroubadour.com>
2022-09-07 11:35:13 +0100

    Create spec/support/matchers folder
    Moved static page matcher and have page title matcher

--- spec/system/page_title_spec.rb
+++ spec/system/page_title_spec.rb
@@ -1,16 +1,5 @@
 require 'rails_helper'
 
-RSpec::Matchers.define :have_page_title do |expected_page_title|
-  match do |path|
-    visit path
-    page.title.eql? "Child development training : #{expected_page_title}"
-  end
-
-  failure_message do |path|
-    "expected when visiting #{path} to have title #{expected_page_title}, not #{page.title} "
-  end
-end
-
 RSpec.describe 'Page' do
   context 'when it is public' do
     it { expect(root_path).to have_page_title('Home page') }
