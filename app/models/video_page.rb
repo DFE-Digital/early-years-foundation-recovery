fac26b730276f0b724a17c61ce91faeef89e2ae3
tunde-anddigital <borimogunje@and.digital>
2022-09-13 16:05:21 +0100

    add hashtoarray method

--- app/models/video_page.rb
+++ app/models/video_page.rb
@@ -49,18 +49,22 @@ class VideoPage
     video_transcript_id = translate(:video)[:id]
     transcript_file = Rails.root.join(%(data/video-transcripts/#{video_transcript_id}.yml))
     transcript_data = YAML.load_file(transcript_file)
-
-    transcript_table = Builder::XmlMarkup.new(:indent => 2)
-    transcript_table.table(class:"govuk-table") {
-      transcript_table.tbody(class:"govuk-table__body first-column-bold")
-      transcript_table.tr(class:"govuk-table__row") { transcript_data[0].keys.each { |key| transcript_table.th(key, class:"hide-header")}}
-      transcript_data.each { |row| transcript_table.tr(class:"govuk-table__row") { row.values.each { |value| transcript_table.td(value, class:"govuk-table__cell remove-borders")}}}
-    }
-    return transcript_table
+    hasharray_to_html(transcript_data)
   end
 
   # @return [ModuleItem]
   def module_item
     @module_item ||= ModuleItem.find_by(training_module: training_module, name: name)
+  end
+
+  # @return [Html]
+  def hasharray_to_html(data)
+    transcript_table = Builder::XmlMarkup.new(:indent => 2)
+    transcript_table.table(class:"govuk-table") {
+      transcript_table.tbody(class:"govuk-table__body first-column-bold")
+      transcript_table.tr(class:"govuk-table__row") { data[0].keys.each { |key| transcript_table.th(key, class:"govuk-!-display-none")}}
+      data.each { |row| transcript_table.tr(class:"govuk-table__row") { row.values.each { |value| transcript_table.td(value, class:"govuk-table__cell remove-borders")}}}
+    }
+    return transcript_table
   end
 end
