553ac289900ef37a40f4ab2827ad0e5a0b1c2909
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-09-21 14:19:13 +0100

    - 404 http://localhost:3000/static/foo - 500 http://localhost:3000/foo

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,17 @@
 class ErrorsController < ApplicationController
   before_action :log_error
 
-  def not_found; end
+  def not_found
+    render status: 404
+  end
 
   def timeout
     @user_timeout_minutes = Rails.configuration.user_timeout_minutes
   end
 
-  def internal_server_error; end
+  def internal_server_error
+    render status: 500
+  end
 
 private
 
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module EarlyYearsFoundationRecovery
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.exceptions_app = self.routes # 404, 500 errors etc
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
