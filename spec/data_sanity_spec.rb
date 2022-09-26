f68e546e2bd7e1a151418a9975243abc44c1578f
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-09-26 15:02:06 +0100

    Fix specs

--- spec/data_sanity_spec.rb
+++ spec/data_sanity_spec.rb
@@ -42,6 +42,6 @@ RSpec.describe 'Page content (locales/modules)' do
     let(:data_dir) { 'data/confidence-questionnaires' }
     let(:type) { 'confidence_questionnaire' }
 
-    specify { expect(ConfidenceQuestionnaire.count).to be 19 }
+    specify { expect(ConfidenceQuestionnaire.count).to be 18 }
   end
 end
