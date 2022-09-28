ddd2b1530f6ed4f310f65724122f69a172507ffa
Peter Hamilton <pete@peterdavidhamilton.com>
2022-09-28 17:13:54 +0100

    ER-411 Fix notification banner styling and content (#295)
    - remove change to I18n load path in govspeak as redundant
    - reinstate omitted styling for notification banners
    - only remove paragraph margin for error and success, not question feedback alert

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -2,8 +2,3 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
 end
-
-# OPTIMIZE: I18n load order in specs as a result of custom Govspeak extensions.
-# Ensure our custom translations are used (config/locales/devise.en.yml)
-# because Devise is appending its defaults to I18n.load_path in the test suite.
-I18n.load_path += Dir[Rails.root.join('config/locales/devise.en.yml')]
