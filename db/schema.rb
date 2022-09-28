ce27bd92c2fee9b5d7e900d2ece44d3775d1677d
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-09-28 12:14:36 +0100

    fix wording of learning log

--- db/schema.rb
+++ db/schema.rb
@@ -122,9 +122,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_26_104904) do
     t.string "unlock_token"
     t.string "setting_type"
     t.string "setting_type_other"
-    t.boolean "display_whats_new", default: false
     t.jsonb "module_time_to_completion", default: {}, null: false
     t.datetime "terms_and_conditions_agreed_at"
+    t.boolean "display_whats_new", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
