81644058de85a9373c4787170f6600bf012b1492
tunde-anddigital <borimogunje@and.digital>
2022-09-29 10:04:41 +0100

    fix test

--- spec/system/video_page_spec.rb
+++ spec/system/video_page_spec.rb
@@ -30,11 +30,11 @@ RSpec.describe 'Video page' do
 
   it 'shows the transcript body after the dropdown has been clicked' do
     find('span', text: 'Transcript').click
-    expect(page).to have_selector('.gem-c-govspeak', text: "So let's look at an example of adult led learning.", visible: :visible)
+    expect(page).to have_selector('.gem-c-govspeak', text: 'The children have gone outside and started a bug hunt.', visible: :visible)
   end
 
   it 'collapses the transcript dropdown and hides the transcript body after being clicked twice' do
     2.times { find('span', text: 'Transcript').click }
-    expect(page).not_to have_selector('.gem-c-govspeak', text: "So let's look at an example of adult led learning.", visible: :visible)
+    expect(page).not_to have_selector('.gem-c-govspeak', text: 'The children have gone outside and started a bug hunt.', visible: :visible)
   end
 end
