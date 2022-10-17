2c2ffe926df6a3f48b2aed9e243ac79a6095c73e
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-10-17 16:54:46 +0100

    - test required form fields pass reassessment - add invisible link text to the module cards - fix duplicate form id for other setting user input

--- app/builders/early_years_recovery_form_builder.rb
+++ app/builders/early_years_recovery_form_builder.rb
@@ -4,10 +4,20 @@ class EarlyYearsRecoveryFormBuilder < GOVUKDesignSystemFormBuilder::FormBuilder
   end
 
   def govuk_email_field(attribute_name, options = {})
-    super(attribute_name, **options.reverse_merge(width: 'two-thirds', label: { text: 'Email address', size: 's' }))
+    super(attribute_name, **options.reverse_merge(width: 'two-thirds'))
   end
 
   def govuk_password_field(attribute_name, options = {})
     super(attribute_name, **options.reverse_merge(width: 'two-thirds'))
+  end
+
+  def terms_and_conditions_check_box
+    govuk_check_box :terms_and_conditions_agreed_at,
+                    Time.zone.now,
+                    nil,
+                    multiple: false,
+                    link_errors: true,
+                    'aria-required': true,
+                    label: { text: 'I confirm that I accept the terms and conditions and privacy policy.' }
   end
 end
