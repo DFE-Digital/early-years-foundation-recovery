a3820cb0dd2fbe22479890458138f2b6da517450
gharteyj1 <joseph.ghartey79@gmail.com>
2022-10-24 15:33:08 +0100

    sign_up test no E2E

--- ui/pages/sign_in.rb
+++ ui/pages/sign_in.rb
@@ -7,7 +7,9 @@ module Pages
     element :email_field, '#user-email-field'
     element :password_field, '#user-password-field'
     element :sign_in_button, 'button.govuk-button', text: 'Sign in'
-
+    element :warning_title, '#govuk-notification-banner-title', text: 'Warning'
+    element :problem_signing_in, '.govuk-details__summary-text'
+    element :forgotten_my_password_link, '.govuk-link', text: 'I have forgotten my password'
     # Authenticate using email and password
     #
     # @param email [String] login email address (default: completed@example.com)
@@ -17,6 +19,28 @@ module Pages
 
       email ||= 'completed@example.com'
       password ||= ENV.fetch('USER_PASSWORD', 'StrongPassword')
+
+      email_field.set(email)
+      password_field.set(password)
+      sign_in_button.click
+    end
+
+    def with_blank_email_and_password(email = nil, password = nil)
+      wait_until_header_visible
+
+      email ||= ''
+      password ||= ENV.fetch('USER_PASSWORD', 'StrongPassword')
+
+      email_field.set(email)
+      password_field.set(password)
+      sign_in_button.click
+    end
+
+    def with_invalid_email_and_password(email = nil, password = nil)
+      wait_until_header_visible
+
+      email ||= 'completedexample.com'
+      password ||= ENV.fetch('USER_PASSWORD', 'Strongassword')
 
       email_field.set(email)
       password_field.set(password)
---
+++ ui/sign_in_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+describe 'Sign in page' do
+  context 'when unauthenticated' do
+    include_context 'as guest'
+
+    before do
+      ui.sign_in.load
+    end
+
+    it 'Warning is displayed when the user clicks login with blank username' do
+      ui.sign_in.with_blank_email_and_password
+      expect(ui.sign_in).to have_warning_title
+    end
+
+    it 'Warning is displayed when the user clicks login with invalid username' do
+      ui.sign_in.with_invalid_email_and_password
+      expect(ui.sign_in).to have_warning_title
+    end
+
+    it 'is shown expanded Help menu' do
+      sleep(10)
+      ui.sign_in.problem_signing_in.click
+      expect(ui.sign_in).to have_content 'I have forgotten my password'
+      expect(ui.sign_in).to have_content 'receive confirmation instructions?'
+      expect(ui.sign_in).to have_content 'Other problems signing in'
+    end
+
+    it 'is shown I have forgotten my Password page' do
+      sleep(10)
+      ui.sign_in.problem_signing_in.click
+      expect(ui.sign_in).to have_content 'I have forgotten my password'
+      ui.sign_in.forgotten_my_password_link.click
+      expect(ui.forgotten_my_password).to have_heading
+    end
+  end
+end
