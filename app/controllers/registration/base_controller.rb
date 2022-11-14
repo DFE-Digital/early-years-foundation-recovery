ac3140fde67e621c1531b9a1bbf578553751f38a
Brett McHargue <brett@therubytroubadour.com>
2022-11-14 15:28:12 +0000

    Update success messages

--- app/controllers/registration/base_controller.rb
+++ app/controllers/registration/base_controller.rb
@@ -16,9 +16,17 @@ private
     current_user.update! registration_complete: true
     if current_user.display_whats_new?
       current_user.update! display_whats_new: false
-      redirect_to static_path('whats-new'), notice: t('.complete_registration')
+      redirect_to static_path('whats-new'), notice: registration_notification
     else
-      redirect_to my_modules_path, notice: t('.complete_registration')
+      redirect_to my_modules_path, notice: registration_notification
+    end
+  end
+
+  def registration_notification
+    if current_user.private_beta_registration_complete?
+      t('.update_registration')
+    else
+      t('.complete_registration')
     end
   end
 
