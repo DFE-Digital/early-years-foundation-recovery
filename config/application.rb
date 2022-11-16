7de742461431ea547ca05572de7b2a7e7b2f6287
Adam Goldstone <13471320+agoldstone93@users.noreply.github.com>
2022-11-16 14:52:18 +0000

    ER-482 Service name update (#370)
    * Updates to service name
    
    Adam will fix this - sorry
    
    * fix broken specs
    
    * Top box on homepage
    
    * move service name to app config variable
    
    * add changes from code review
    
    * rubocop changes
    
    Co-authored-by: Jenny Pendriss <jennifer.pendriss@and.digital>

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,8 @@ module EarlyYearsFoundationRecovery
     config.unlock_in_minutes = ENV.fetch('UNLOCK_IN_MINUTES', '120').to_i
     config.user_timeout_minutes = ENV.fetch('TIMEOUT_MINUTES', '15').to_i
 
+    config.service_name = 'Early years child development training'
+
     config.middleware.use Grover::Middleware
     config.active_record.yaml_column_permitted_classes = [Symbol]
     config.action_view.sanitized_allowed_tags = ALLOWED_TAGS
