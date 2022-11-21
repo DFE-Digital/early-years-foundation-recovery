0429e81e0f98cb538b31a0afe462db2f7de9c934
mohammed Aziz <mo_zag@yahoo.co.uk>
2022-11-21 14:48:30 +0000

    Updated time for test failure fix

--- spec/system/timeout_modal_spec.rb
+++ spec/system/timeout_modal_spec.rb
@@ -23,14 +23,14 @@ RSpec.describe 'Timeout modal', type: :system do
       visit '/my-modules'
       data_minutes_idle_timeout = page.find('#js-timeout-warning')['data-minutes-idle-timeout']
       data_minutes_modal_visible = page.find('#js-timeout-warning')['data-minutes-modal-visible']
-      expect(Rails.configuration.user_timeout_minutes).to be > (data_minutes_idle_timeout.to_i + data_minutes_modal_visible.to_i)
+      expect(Rails.configuration.user_timeout_minutes).to be >= (data_minutes_idle_timeout.to_i + data_minutes_modal_visible.to_i)
     end
   end
 
-  # context 'with an unauthenticated user' do
-  #   it 'data attribute user status' do
-  #     visit '/'
-  #     expect(page).to have_selector("#js-timeout-warning[@data-user-status='false']")
-  #   end
-  # end
+  context 'with an unauthenticated user' do
+    it 'data attribute user status' do
+      visit '/'
+      expect(page).to have_selector("#js-timeout-warning[@data-user-status='false']")
+    end
+  end
 end
