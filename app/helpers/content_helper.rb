5a5e154d1e2f8335f65bdf50df7d177c46693656
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-11-25 14:11:47 +0000

    use mid grey to match design, add method for line with two colours

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -46,6 +46,15 @@ module ContentHelper
                 aria: { label: label }
   end
 
+  # @param percentage [Integer] of line in left colour
+  # @param left_colour [String]
+  # @param right_colour [String]
+  # @return [String]
+  def progress_line(percentage, left_colour, right_colour)
+    content_tag :div, nil, class: "line line--slice", 
+                style: "border-image-source: linear-gradient(to right, #{left_colour}, #{left_colour} #{percentage}%, #{right_colour} #{percentage}%, #{right_colour});"
+  end
+
   # @return [String]
   def print_button(*additional_classes)
     button = '<button class="govuk-link gem-c-print-link__button" onclick="window.print()" data-module="print-link" >Print this page</button>'.html_safe
