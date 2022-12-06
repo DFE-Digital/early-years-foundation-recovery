e8cd6bb576fb713b8442bc969fa942cbb70fd9fc
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-12-06 12:16:06 +0000

    update unit tests

--- spec/decorators/module_progress_bar_decorator_spec.rb
+++ spec/decorators/module_progress_bar_decorator_spec.rb
@@ -8,66 +8,217 @@ RSpec.describe ModuleProgressBarDecorator do
 
   include_context 'with progress'
 
-  describe '#progress_bar_info' do
-    let(:output) do
-      decorator.progress_bar_info do
-        {
-          index: index,
-          heading: heading,
-          class: style,
-          first: first,
-          bold: bold,
-          position: position,
-          content_helper_values: content_helper_values,
-        }
-      end
-    end
+  describe 'indexes' do
+    let(:indexes) { decorator.progress_bar_info.map { |hash| hash[:index] } }
 
-    context 'when on first intro page' do
+    specify { expect(indexes).to eq [0, 1, 2, 3] }
+  end
+
+  describe 'headings' do
+    let(:headings) { decorator.progress_bar_info.map { |hash| hash[:heading] } }
+
+    specify do
+      expect(headings).to eq ['Module introduction', 'The first submodule', 'The second submodule', 'Summary and next steps']
+    end
+  end
+
+  describe 'firsts' do
+    let(:firsts) { decorator.progress_bar_info.map { |hash| hash[:first] } }
+
+    specify { expect(firsts).to eq [true, false, false, false] }
+  end
+
+  describe 'positions' do
+    let(:positions) { decorator.progress_bar_info.map { |hash| hash[:position] } }
+
+    specify { expect(positions).to eq ['Step 1: ', 'Step 2: ', 'Step 3: ', 'Step 4: '] }
+  end
+
+  context 'when on module intro section' do
+    context 'when on interruption page' do
       before do
         view_pages_before(alpha, 'interruption_page')
       end
 
-      [
-        {
-          index: 0,
-          heading: 'Module introduction',
-          class: nil,
-          first: true,
-          bold: true,
-          position: 'Step 1: ',
-          content_helper_values: ['circle', :solid, :green, 'started'],
-        },
-        {
-          index: 1,
-          heading: 'The first submodule',
-          class: 'line line--grey',
-          first: false,
-          bold: false,
-          position: 'Step 2: ',
-          content_helper_values: ['circle', :regular, :grey, 'not started'],
-        },
-        {
-          index: 2,
-          heading: 'The second submodule',
-          class: 'line line--grey',
-          first: false,
-          bold: false,
-          position: 'Step 3: ',
-          content_helper_values: ['circle', :regular, :grey, 'not started'],
-        },
-        {
-          index: 3,
-          heading: 'Summary and next steps',
-          class: 'line line--grey',
-          first: false,
-          bold: false,
-          position: 'Step 4: ',
-          content_helper_values: ['circle', :regular, :grey, 'not started'],
-        },
-      ].each_with_index do |item, index|
-        describe '' do
-          it { expect(item).to eq output[index] }
+      describe 'classes' do
+        let(:classes) { decorator.progress_bar_info.map { |hash| hash[:class] } }
+
+        specify { expect(classes).to eq [nil, 'line line--grey', 'line line--grey', 'line line--grey'] }
+      end
+
+      describe 'bolds' do
+        let(:bolds) { decorator.progress_bar_info.map { |hash| hash[:bold] } }
+
+        specify { expect(bolds).to eq [true, false, false, false] }
+      end
+
+      describe 'content_helper_values' do
+        let(:content_helper_values) { decorator.progress_bar_info.map { |hash| hash[:content_helper_values] } }
+
+        it 'has nodes [started, not started, not started, not started]' do
+          expect(content_helper_values).to eq [
+            ['circle', :solid, :green, 'started'],
+            ['circle', :regular, :grey, 'not started'],
+            ['circle', :regular, :grey, 'not started'],
+            ['circle', :regular, :grey, 'not started'],
+          ]
+        end
+      end
+    end
+
+    context 'when on icons page' do
+      before do
+        view_pages_before(alpha, 'icons_page')
+      end
+
+      describe 'classes' do
+        let(:classes) { decorator.progress_bar_info.map { |hash| hash[:class] } }
+
+        specify { expect(classes).to eq [nil, 'line line--grey', 'line line--grey', 'line line--grey'] }
+      end
+
+      describe 'bolds' do
+        let(:bolds) { decorator.progress_bar_info.map { |hash| hash[:bold] } }
+
+        specify { expect(bolds).to eq [true, false, false, false] }
+      end
+
+      describe 'content_helper_values' do
+        let(:content_helper_values) { decorator.progress_bar_info.map { |hash| hash[:content_helper_values] } }
+
+        it 'has nodes [started, not started, not started, not started]' do
+          expect(content_helper_values).to eq [
+            ['circle', :solid, :green, 'started'],
+            ['circle', :regular, :grey, 'not started'],
+            ['circle', :regular, :grey, 'not started'],
+            ['circle', :regular, :grey, 'not started'],
+          ]
+        end
+      end
+    end
+
+    context 'when on module intro page' do
+      before do
+        start_module(alpha)
+      end
+
+      describe 'classes' do
+        let(:classes) { decorator.progress_bar_info.map { |hash| hash[:class] } }
+
+        specify { expect(classes).to eq [nil, 'line line--grey', 'line line--grey', 'line line--grey'] }
+      end
+
+      describe 'bolds' do
+        let(:bolds) { decorator.progress_bar_info.map { |hash| hash[:bold] } }
+
+        specify { expect(bolds).to eq [true, false, false, false] }
+      end
+
+      describe 'content_helper_values' do
+        let(:content_helper_values) { decorator.progress_bar_info.map { |hash| hash[:content_helper_values] } }
+
+        it 'has nodes [completed, not started, not started, not started]' do
+          expect(content_helper_values).to eq [
+            ['circle-check', :solid, :green, 'completed'],
+            ['circle', :regular, :grey, 'not started'],
+            ['circle', :regular, :grey, 'not started'],
+            ['circle', :regular, :grey, 'not started'],
+          ]
+        end
+      end
+    end
+  end
+
+  context 'when on first submodule section' do
+    context 'when on sub_module_intro page' do
+      before do
+        start_first_submodule(alpha)
+      end
+
+      describe 'classes' do
+        let(:classes) { decorator.progress_bar_info.map { |hash| hash[:class] } }
+
+        specify { expect(classes).to eq [nil, 'line line--green', 'line line--grey', 'line line--grey'] }
+      end
+
+      describe 'bolds' do
+        let(:bolds) { decorator.progress_bar_info.map { |hash| hash[:bold] } }
+
+        specify { expect(bolds).to eq [false, true, false, false] }
+      end
+
+      describe 'content_helper_values' do
+        let(:content_helper_values) { decorator.progress_bar_info.map { |hash| hash[:content_helper_values] } }
+
+        it 'has nodes [completed, started, not started, not started]' do
+          expect(content_helper_values).to eq [
+            ['circle-check', :solid, :green, 'completed'],
+            ['circle', :solid, :green, 'started'],
+            ['circle', :regular, :grey, 'not started'],
+            ['circle', :regular, :grey, 'not started'],
+          ]
+        end
+      end
+    end
+
+    context 'when on first content page' do
+      before do
+        start_first_topic(alpha)
+      end
+
+      describe 'classes' do
+        let(:classes) { decorator.progress_bar_info.map { |hash| hash[:class] } }
+
+        specify { expect(classes).to eq [nil, 'line line--green', 'line line--grey', 'line line--grey'] }
+      end
+
+      describe 'bolds' do
+        let(:bolds) { decorator.progress_bar_info.map { |hash| hash[:bold] } }
+
+        specify { expect(bolds).to eq [false, true, false, false] }
+      end
+
+      describe 'content_helper_values' do
+        let(:content_helper_values) { decorator.progress_bar_info.map { |hash| hash[:content_helper_values] } }
+
+        it 'has nodes [completed, started, not started, not started]' do
+          expect(content_helper_values).to eq [
+            ['circle-check', :solid, :green, 'completed'],
+            ['circle', :solid, :green, 'started'],
+            ['circle', :regular, :grey, 'not started'],
+            ['circle', :regular, :grey, 'not started'],
+          ]
+        end
+      end
+    end
+
+    context 'when on final content page' do
+      before do
+        view_pages_before_formative_questionnaire(alpha)
+      end
+
+      describe 'classes' do
+        let(:classes) { decorator.progress_bar_info.map { |hash| hash[:class] } }
+
+        specify { expect(classes).to eq [nil, 'line line--green', 'line line--grey', 'line line--grey'] }
+      end
+
+      describe 'bolds' do
+        let(:bolds) { decorator.progress_bar_info.map { |hash| hash[:bold] } }
+
+        specify { expect(bolds).to eq [false, true, false, false] }
+      end
+
+      describe 'content_helper_values' do
+        let(:content_helper_values) { decorator.progress_bar_info.map { |hash| hash[:content_helper_values] } }
+
+        it 'has nodes [completed, completed, not started, not started]' do
+          expect(content_helper_values).to eq [
+            ['circle-check', :solid, :green, 'completed'],
+            ['circle-check', :solid, :green, 'completed'],
+            ['circle', :regular, :grey, 'not started'],
+            ['circle', :regular, :grey, 'not started'],
+          ]
         end
       end
     end
