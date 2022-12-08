2d61d1e3e06b50c311eceb06f383f213927f784d
Brett McHargue <brett@therubytroubadour.com>
2022-12-08 12:11:01 +0000

    Set up for use of preview subdomain and contentful environment

--- config/initializers/contentful_rails.rb
+++ config/initializers/contentful_rails.rb
@@ -4,14 +4,16 @@ require 'training/question'
 require 'training/confidence'
 
 ContentfulRails.configure do |config|
-  config.authenticate_webhooks = true # false here would allow the webhooks to process without basic auth
-  config.webhooks_username = ENV['CONTENTFUL_WEBHOOKS_USERNAME']
-  config.webhooks_password = ENV['CONTENTFUL_WEBHOOKS_PASSWORD']
+  # config.authenticate_webhooks = true # false here would allow the webhooks to process without basic auth
+  # config.webhooks_username = 'ey_recovery'
+  # config.webhooks_password = 'ey_recovery'
   config.enable_preview_domain = Rails.application.preview?
-  config.preview_domain = ENV.fetch('DOMAIN', 'preview').split('.').first if Rails.application.preview?
+  if Rails.application.preview?
+    config.preview_domain = ENV.fetch('DOMAIN', 'preview').split('.').first
+  end
+  config.environment = Rails.application.credentials.dig(:contentful, :environment)
   config.access_token = Rails.application.credentials.dig(:contentful, :delivery_access_token) # Required
   config.space = Rails.application.credentials.dig(:contentful, :space) # Required
-  config.environment = Rails.application.credentials.dig(:contentful, :environment)
   config.preview_access_token = Rails.application.credentials.dig(:contentful, :preview_access_token) # Optional - required if you want to use the preview API
   config.management_token = Rails.application.credentials.dig(:contentful, :management_token) # Optional - required if you want to update or create content
   config.default_locale = 'en-US' # Optional - defaults to 'en-US'
