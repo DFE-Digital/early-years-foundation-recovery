dc03c9ecd642b98db17d26ed1a150d906e097b91
Peter David Hamilton <pete@peterdavidhamilton.com>
2022-12-13 11:39:28 +0000

    - use appropriate matcher for variable accuracy of timeout counter response

--- spec/requests/timeout_warning_spec.rb
+++ spec/requests/timeout_warning_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe 'Session timeout warning', type: :request do
       # lag may have lowered the counter when the assertion is made
       it 'reports seconds before timeout' do
         expect(response).to have_http_status(:success)
-        expect(response.body).to eq('1500' || '1499') # ~25*60
+        expect(response.body.to_i).to be_within(1).of(1500) # ~25*60
       end
 
       it 'decreases over time' do
         travel_to 1.minute.from_now
         get check_session_timeout_path
-        expect(response.body).to eq('1440' || '1439')
+        expect(response.body.to_i).to be_within(1).of(1440)
       end
     end
 
