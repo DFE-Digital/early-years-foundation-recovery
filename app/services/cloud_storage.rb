fec53920be7a3ee006221262c70cd2981035e027
mohammed Aziz <mo_zag@yahoo.co.uk>
2022-12-13 02:38:10 +0000

    updated test creds

--- app/services/cloud_storage.rb
+++ app/services/cloud_storage.rb
@@ -4,7 +4,7 @@ class CloudStorage
   require 'google/cloud/storage'
 
   def gcs
-    google_credentials = JSON.parse(ENV['GOOGLE_CLOUD_STORAGE'])
+    google_credentials = JSON.parse(ENV.fetch('GOOGLE_CLOUD_STORAGE', Rails.application.credentials.google_cloud_storage))
     @gcs ||= Google::Cloud::Storage.new project: google_credentials['project_id'], credentials: google_credentials
   end
 end
