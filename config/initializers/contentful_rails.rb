13c6de4da3e9318a1d640e9f523b982d95252164
Brett McHargue <brett@therubytroubadour.com>
2023-01-11 12:25:09 +0000

    Make list of modules an array of module names

--- config/initializers/contentful_rails.rb
+++ config/initializers/contentful_rails.rb
@@ -17,8 +17,8 @@ ContentfulRails.configure do |config|
   config.default_locale = 'en-US' # Optional - defaults to 'en-US'
   config.eager_load_entry_mapping = false
   config.contentful_options = {
-    modules: ENV.fetch('CONTENTFUL_MODULES', 'child-development-and-the-eyfs, alpha'),
-    new_modules: ENV.fetch('NEW_CONTENTFUL_MODULES', 'module-6'),
+    modules: ENV.fetch('CONTENTFUL_MODULES', 'child-development-and-the-eyfs,alpha').split(','),
+    new_modules: ENV.fetch('NEW_CONTENTFUL_MODULES', 'module-6').split(','),
     delivery_api: { timeout_connect: 2, timeout_read: 6, timeout_write: 20 },
     management_api: { timeout_connect: 3, timeout_read: 100, timeout_write: 200 },
     entry_mapping: {
