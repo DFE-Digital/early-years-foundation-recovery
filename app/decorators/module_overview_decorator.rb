99d3471980e24a3f74fcc045c7d8f037e5c53eb0
Adam Goldstone <adam.goldstone@education.gov.uk>
2023-01-11 08:54:06 +0000

    add semantic html, refactor module overview decorator, update variable names

--- app/decorators/module_overview_decorator.rb
+++ app/decorators/module_overview_decorator.rb
@@ -13,19 +13,17 @@ class ModuleOverviewDecorator < DelegateClass(ModuleProgress)
     end
   end
 
-  # @return [Array<String, Symbol, Array>]
+  # @return [Array<Hash{Symbol => String, Symbol, Array}>]
   def sections
     mod.items_by_submodule.each.with_index(1).map do |(num, items), position|
       intro = items.first
       heading = num.nil? ? 'Module introduction' : intro.model.heading
-      icon = status(items)
-      topics = topics(submodule: num, items: items)
-      [
-        heading,                              # submodule intro heading
-        position,                             # position
-        icon,                                 # icon style
-        topics,                               # Array(String, Symbol)
-      ]
+      {
+        heading: heading,                                       # submodule intro heading
+        position: position,                                     # position
+        icon: status(items),                                    # icon style
+        subsections: subsections(submodule: num, items: items), # Array(String, Symbol)
+      }
     end
   end
 
@@ -52,70 +50,66 @@ private
   #
   # @param submodule [String]
   # @param items [Array<ModuleItems]
-  def topics(submodule:, items:)
-    if submodule.nil?
-      items.select.map do |topic|
-        section_content(submodule: submodule, topic_item: topic)
-      end
-    else
-      items[1..].select(&:topic?).map do |topic|
-        section_content(submodule: submodule, topic_item: topic)
-      end
+  def subsections(submodule:, items:)
+    updated_items = submodule.nil? ? items : items.drop(1)
+
+    updated_items.select(&:topic?).map do |subsection|
+      section_content(submodule: submodule, subsection_item: subsection)
     end
   end
 
   # @param submodule [String]
-  # @param topic_item [ModuleItem]
+  # @param subsection_item [ModuleItem]
   #
   # @return [Array<Array>]
-  def section_content(submodule:, topic_item:)
-    topic_status = if submodule.nil?
-                     status([topic_item])
-                   else
-                     status(topic_item.current_submodule_topic_items)
-                   end
+  def section_content(submodule:, subsection_item:)
+    subsection_status = if submodule.nil?
+                          status([subsection_item])
+                        else
+                          status(subsection_item.current_submodule_topic_items)
+                        end
 
     # providing the next page name enables the hyperlink
-    if clickable?(topic_item: topic_item, submodule: submodule)
-      furthest_topic_page_name = topic_status.eql?(:started) ? resume_page : topic_item
+    if clickable?(subsection_item: subsection_item, submodule: submodule)
+      furthest_topic_page_name = subsection_status.eql?(:started) ? resume_page : subsection_item
     end
 
     # SummativeAssessmentProgress conditional pass
     if failed_attempt?
-      topic_status = :failed if topic_item.assessment_intro?
-      furthest_topic_page_name = nil if topic_item.confidence_intro?
+      subsection_status = :failed if subsection_item.assessment_intro?
+      furthest_topic_page_name = nil if subsection_item.confidence_intro?
     elsif successful_attempt?
-      topic_status = :completed if topic_item.assessment_intro?
-      furthest_topic_page_name = topic_item if topic_item.confidence_intro?
+      subsection_status = :completed if subsection_item.assessment_intro?
+      furthest_topic_page_name = subsection_item if subsection_item.confidence_intro?
     end
 
     [
-      topic_item.training_module,   # TrainingModule [mod]
-      topic_item.model.heading,     # String (content page heading) [topic_heading]
-      furthest_topic_page_name,     # String/nil (module_item name) [next_item]
-      topic_status,                 # Symbol (all items viewed) [status]
+      subsection_item.training_module,   # TrainingModule [mod]
+      subsection_item.model.heading,     # String (content page heading) [subsection_heading]
+      furthest_topic_page_name,          # String/nil (module_item name) [next_item]
+      subsection_status,                 # Symbol (all items viewed) [status]
     ]
   end
 
   # @param submodule [String]
-  # @param topic_item [ModuleItem]
+  # @param subsection_item [ModuleItem]
   #
   # @return [Boolean]
-  def clickable?(submodule:, topic_item:)
-    return all?([topic_item]) if submodule.nil?
+  def clickable?(submodule:, subsection_item:)
+    return all?([subsection_item]) if submodule.nil?
 
     submodule_intro = mod.module_items_by_submodule(submodule).first
     return false unless visited?(submodule_intro)
 
-    current_topic = find_current_topic(topic_item.topic_name, submodule)
+    current_topic = find_topic(subsection_item.topic_name, submodule)
     current_topic_items = current_topic.values.first.to_a
 
     # if current_topic && previous_submodule
     #   all?(current_topic_items) && all?(previous_submodule_items)
     if current_topic
       all?(current_topic_items)
-    else
-      all?([topic_item])
+    # else
+    #   all?([subsection_item])
     end
   end
 
@@ -123,9 +117,9 @@ private
   # @param current_sub_num [String]
   #
   # @return [Array<Array>] [1,2] [item, item, item]
-  def find_current_topic(current_top_num, current_sub_num)
-    mod.items_by_topic.select do |(sub_num, topic_num), _items|
-      sub_num.eql?(current_sub_num) && current_top_num.eql?(topic_num)
+  def find_topic(topic_num, submodule_num)
+    mod.items_by_topic.select do |(match_sub_num, match_topic_num), _items|
+      match_sub_num.eql?(submodule_num) && match_topic_num.eql?(topic_num)
     end
   end
 end
