8e375f89c6a01a1da1799ef18f2ccdc06230645d
Adam Goldstone <adam.goldstone@education.gov.uk>
2023-02-01 13:33:05 +0000

    change wifi page url, add static page spec coverage, add content to interruption page

--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -1,13 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Static page', type: :request do
-  specify { expect('/terms-and-conditions').to be_successful }
-
   specify { expect('/accessibility-statement').to be_successful }
+
+  specify { expect('/cookie-policy').to be_successful }
+  
+  specify { expect('/new-registration').to be_successful }
+
+  specify { expect('/other-problems-signing-in').to be_successful }
 
   specify { expect('/privacy-policy').to be_successful }
 
+  specify { expect('/terms-and-conditions').to be_successful }
+
   specify { expect('/users/timeout').to be_successful }
+
+  specify { expect('/wifi-and-data').to be_successful }
+
+  specify { expect('/whats-new').to be_successful }
 
   context 'with errors' do
     specify do
