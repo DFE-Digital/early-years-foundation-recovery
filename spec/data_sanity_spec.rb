c759be4bf272e86b9933c73e9068df34e1c223a2
Peter David Hamilton <pete@peterdavidhamilton.com>
2023-02-14 15:22:57 +0000

    - update data sanity tallies - remove starting pages from module 5

--- spec/data_sanity_spec.rb
+++ spec/data_sanity_spec.rb
@@ -44,8 +44,9 @@ RSpec.describe 'Module content' do
       expect(questions['brain-development-and-how-children-learn'].count).to be 22
       expect(questions['personal-social-and-emotional-development'].count).to be 19
       expect(questions['module-4'].count).to be 19
+      expect(questions['module-5'].count).to be 16
 
-      expect(questions_total).to be 82
+      expect(questions_total).to be 98
     end
   end
 
@@ -64,8 +65,9 @@ RSpec.describe 'Module content' do
       expect(questions['brain-development-and-how-children-learn'].count).to be 10
       expect(questions['personal-social-and-emotional-development'].count).to be 10
       expect(questions['module-4'].count).to be 10
+      expect(questions['module-5'].count).to be 10
 
-      expect(questions_total).to be 48
+      expect(questions_total).to be 58
     end
   end
 
@@ -84,8 +86,9 @@ RSpec.describe 'Module content' do
       expect(questions['brain-development-and-how-children-learn'].count).to be 4
       expect(questions['personal-social-and-emotional-development'].count).to be 6
       expect(questions['module-4'].count).to be 6
+      expect(questions['module-5'].count).to be 5
 
-      expect(questions_total).to be 30
+      expect(questions_total).to be 35
     end
   end
 end
