816b140dbd09b21a7abee4afda3a24b5043945e7
Adam Goldstone <adam.goldstone@education.gov.uk>
2023-02-28 13:15:25 +0000

    use send() to run each method and print error

--- app/services/contentful_data_integrity.rb
+++ app/services/contentful_data_integrity.rb
@@ -7,34 +7,37 @@ class ContentfulDataIntegrity
 
   option :environment, default: proc { 'demo' }
   option :training_module, default: proc { 'alpha' }
-  option :cached, default: proc { true }
+  # option :cached, default: proc { true }
 
   def valid?
     raise 'Module does not exist' if find_module_by_name.nil?
-    
-    methods = { 
-      has_video_page?: 'Does not contain video page',
-    }
-    send()
 
-    if has_video_page? && first_page_is_interruption? && last_page_is_certificate?
-      return true
+    {
+      has_video_page?: 'Does not contain video page',
+      has_assessment_results?: 'Does not have assessments results page',
+      first_page_is_interruption?: 'First page is not interruption page',
+      second_page_is_sub_module_intro?: 'Second page is not submodule intro',
+      penultimate_page_is_thankyou?: 'Penultimate page is not thank you',
+      last_page_is_certificate?: 'Last page is not certificate',
+      has_ten_summative_questions?: 'Does not have ten summative questions',
+      consecutive_submodules_start_at_one?: "Submodules aren't consecutive or don't start with 1",
+      consecutive_topics_start_at_one?: "Topics aren't consecutive or don't start with 1",
+    }.each do |method, message|
+      unless send(method)
+        Rails.logger.debug message
+        return false
+      end
     end
 
-    false
+    true
   end
 
   # ------------------- Correct page order ---------------------
 
-  # # 1, 3, 1, 2 would fail
-  # # 1, 3, 4 would fail
-  # # should increase by one each time
-  # submodules and topics increment
-
-  # @param input [Array]
+  # @param input [Array] integers
   # @return [Boolean]
   def consecutive_nums_start_at_one?(input)
-    input.each_cons(2).all? { |a,b| b == a + 1 } && input.first == 1
+    input.each_cons(2).all? { |a, b| b == a + 1 } && input.first == 1
   end
 
   # @return [Boolean]
@@ -47,8 +50,8 @@ class ContentfulDataIntegrity
   def consecutive_topics_start_at_one?
     topic_nums = find_module_by_name.content_by_submodule_topic.keys.group_by(&:first)
 
-    result = topic_nums.each do |submodule_num, submod_topic_num|
-      return false if !consecutive_nums_start_at_one?(submod_topic_num.map(&:second))
+    topic_nums.each do |_submodule_num, submod_topic_num|
+      return false unless consecutive_nums_start_at_one?(submod_topic_num.map(&:second))
     end
 
     true
@@ -56,7 +59,7 @@ class ContentfulDataIntegrity
 
   def find_module_by_name
     # caching twice if used like this
-    # 
+    #
     # fetch_or_store(training_module) do
     #   Training::Module.by_name(training_module)
     # end
@@ -87,7 +90,7 @@ private
   def penultimate_page_is_thankyou?
     find_module_by_name.content[-2].page_type.eql?('thankyou')
   end
-  
+
   def last_page_is_certificate?
     find_module_by_name.content.last.page_type.eql?('certificate')
   end
@@ -96,11 +99,10 @@ private
     find_module_by_name.page_by_type('summative_questionnaire').count.eql? 10
   end
 
-  # ------------------- Notes ---------------------
+  # ------------------- Also to add? ---------------------
 
   # demo content tallies are met (this shouldn’t be necessary for genuine content)
   # count how many things we're expecting - in the data sanity check
-
 
   # depends on values are present for modules 2-10
   # order of the modules
--- spec/services/contentful_data_integrity_spec.rb
+++ spec/services/contentful_data_integrity_spec.rb
@@ -1,36 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe ContentfulDataIntegrity do
+  subject(:data_integrity) { described_class.new(environment: 'test', training_module: mod, cached: true) }
+
   let(:mod) { 'alpha' }
-  subject(:data_integrity) { described_class.new(environment: 'test', training_module: mod, cached: true)}
-  
+
   context 'when module does not exist' do
     let(:mod) { 'foo' }
-    
+
     it 'returns error message' do
       expect { data_integrity.valid? }.to raise_error 'Module does not exist'
     end
   end
 
+  context 'when module does exist and is valid' do
+    let(:mod) { 'alpha' }
+
+    it 'returns true' do
+      expect(data_integrity.valid?).to eq true
+    end
+  end
+
   describe 'when given an array of consecutive numbers' do
     context 'and is not consecutive' do
-      let(:nums) { [1,2,3,4,6] }
-      
+      let(:nums) { [1, 2, 3, 4, 6] }
+
       specify do
         expect(data_integrity.consecutive_nums_start_at_one?(nums)).to eq false
       end
     end
 
     context 'and is consecutive but does not start at 1' do
-      let(:nums) { [2,3,4,5] }
-      
+      let(:nums) { [2, 3, 4, 5] }
+
       specify do
         expect(data_integrity.consecutive_nums_start_at_one?(nums)).to eq false
       end
     end
 
     context 'and is consecutive and starts at 1' do
-      let(:nums) { [1,2,3,4,5] }
+      let(:nums) { [1, 2, 3, 4, 5] }
 
       specify do
         expect(data_integrity.consecutive_nums_start_at_one?(nums)).to eq true
@@ -38,53 +47,30 @@ RSpec.describe ContentfulDataIntegrity do
     end
   end
 
-  describe 'formative' do
-    let(:questions) { data_integrity.find_module_by_name.formative_questions }
-    
-    specify { expect(questions.count).to eq 3 }
-
-    let(:mod) { 'bravo' }
-    specify { expect(questions.count).to eq 1 }
-    
-
-    let(:mod) { 'charlie' }
-    specify { expect(questions.count).to eq 1 }
-      
-    # expect(questions['bravo'].count).to be 1
-    # expect(questions['charlie'].count).to be 1
-
-    # expect(questions['child-development-and-the-eyfs'].count).to be 17
-    # expect(questions['brain-development-and-how-children-learn'].count).to be 22
-    # expect(questions['personal-social-and-emotional-development'].count).to be 19
-    # expect(questions['module-4'].count).to be 19
-
-    # expect(questions_total).to be 82
-  end
-
   describe 'test modules' do
     let(:formative) { data_integrity.find_module_by_name.formative_questions }
     let(:summative) { data_integrity.find_module_by_name.summative_questions }
     let(:confidence) { data_integrity.find_module_by_name.confidence_questions }
-    
+
     describe 'alpha' do
       let(:mod) { 'alpha' }
-      
+
       specify { expect(formative.count).to eq 3 }
       specify { expect(summative.count).to eq 4 }
       specify { expect(confidence.count).to eq 3 }
     end
-    
+
     describe 'bravo' do
       let(:mod) { 'bravo' }
-      
+
       specify { expect(formative.count).to eq 1 }
       specify { expect(summative.count).to eq 2 }
       specify { expect(confidence.count).to eq 3 }
     end
-    
+
     describe 'charlie' do
       let(:mod) { 'charlie' }
-      
+
       specify { expect(formative.count).to eq 1 }
       specify { expect(summative.count).to eq 2 }
       specify { expect(confidence.count).to eq 3 }
