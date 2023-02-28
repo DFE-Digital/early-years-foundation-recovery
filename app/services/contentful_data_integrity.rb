4f63ffc165d44f8d68ac997934d6ef5f5f3abccb
Adam Goldstone <adam.goldstone@education.gov.uk>
2023-02-28 08:48:41 +0000

    continued wip

--- app/services/contentful_data_integrity.rb
+++ app/services/contentful_data_integrity.rb
@@ -3,80 +3,109 @@
 
 class ContentfulDataIntegrity
   extend Dry::Initializer
+  # extend Dry::Core::Cache
 
-  option :environment, required: true
-  option :training_module, required: true
-  option :cached, required: true
+  option :environment, default: proc { 'demo' }
+  option :training_module, default: proc { 'alpha' }
+  option :cached, default: proc { true }
 
   def valid?
-    mod = find_module_by_name(training_module)
-    return false if find_module_by_name(training_module).nil?
+    raise 'Module does not exist' if find_module_by_name.nil?
     
-    if has_video_page 
-      && first_page_is_interruption 
-      && last_page_is_certificate
+    methods = { 
+      has_video_page?: 'Does not contain video page',
+    }
+    send()
+
+    if has_video_page? && first_page_is_interruption? && last_page_is_certificate?
       return true
     end
 
     false
   end
 
-private
-
-  def find_module_by_name(name)
-    fetch_or_store(name) do
-      Training::Module.by_name(name)
-    end
-  end
+  # ------------------- Correct page order ---------------------
 
   # # 1, 3, 1, 2 would fail
   # # 1, 3, 4 would fail
   # # should increase by one each time
   # submodules and topics increment
-  def topics_in_order
-    mod.content_by_submodule_topic.keys
+
+  # @param input [Array]
+  # @return [Boolean]
+  def consecutive_nums_start_at_one?(input)
+    input.each_cons(2).all? { |a,b| b == a + 1 } && input.first == 1
   end
 
-  def submodules_in_order
-    mod.content_by_submodule.keys
+  # @return [Boolean]
+  def consecutive_submodules_start_at_one?
+    submodule_nums = find_module_by_name.content_by_submodule.keys
+    consecutive_nums_start_at_one?(submodule_nums)
   end
 
-  # # e.g. each module needs video page
-  # one of each required page type is present
-  def has_video_page
-    mod.content.any?(Training::Video)
-  end
-    
+  # @return [Boolean]
+  def consecutive_topics_start_at_one?
+    topic_nums = find_module_by_name.content_by_submodule_topic.keys.group_by(&:first)
 
-  # first page needs to be interruption
-  def first_page_is_interruption
-    mod.content.first.page_type.eql? 'interruption_page'
+    result = topic_nums.each do |submodule_num, submod_topic_num|
+      return false if !consecutive_nums_start_at_one?(submod_topic_num.map(&:second))
+    end
+
+    true
+  end
+
+  def find_module_by_name
+    # caching twice if used like this
+    # 
+    # fetch_or_store(training_module) do
+    #   Training::Module.by_name(training_module)
+    # end
+
+    Training::Module.by_name(training_module)
+  end
+
+private
+
+  # ------------------- Required page types ---------------------
+
+  def has_video_page?
+    find_module_by_name.content.any?(Training::Video)
+  end
+
+  def has_assessment_results?
+    find_module_by_name.content.map(&:page_type).any?('assessment_results')
+  end
+
+  def first_page_is_interruption?
+    find_module_by_name.content.first.page_type.eql?('interruption_page')
+  end
+
+  def second_page_is_sub_module_intro?
+    find_module_by_name.content[1].page_type.eql?('sub_module_intro')
+  end
+
+  def penultimate_page_is_thankyou?
+    find_module_by_name.content[-2].page_type.eql?('thankyou')
   end
   
-  # last page needs to be certificate
-  def last_page_is_certificate
-    mod.content.last.page_type.eql? 'certificate'
+  def last_page_is_certificate?
+    find_module_by_name.content.last.page_type.eql?('certificate')
   end
-  
-  # should be 10 summative questionnaire types
-  def has_ten_summative_questions
-    mod.page_by_type('summative_questionnaire').count.eql? 10
+
+  def has_ten_summative_questions?
+    find_module_by_name.page_by_type('summative_questionnaire').count.eql? 10
   end
-  
-  # types appear in the correct order
-  def modules_are_ordered_correctly
-    arr = mod.content.drop(1).map {|m| m.name}
-    arr == arr.sort
-  end
+
+  # ------------------- Notes ---------------------
+
+  # demo content tallies are met (this shouldn’t be necessary for genuine content)
+  # count how many things we're expecting - in the data sanity check
 
 
   # depends on values are present for modules 2-10
   # order of the modules
 
   # pages have a parent value
-
-  # demo content tallies are met (this shouldn’t be necessary for genuine content)
-  # count how many things we're expecting - in the data sanity check
 
   # training_module needs to have a thumbnail
   # should be validated on the contentful model
--- spec/services/contentful_data_integrity_spec.rb
+++ spec/services/contentful_data_integrity_spec.rb
@@ -3,14 +3,91 @@ require 'rails_helper'
 RSpec.describe ContentfulDataIntegrity do
   let(:mod) { 'alpha' }
   subject(:data_integrity) { described_class.new(environment: 'test', training_module: mod, cached: true)}
-
-  subject(:video_page) { Training::Video }
   
   context 'when module does not exist' do
     let(:mod) { 'foo' }
     
-    it 'returns false' do
-      expect(data_integrity.valid?).to be false
+    it 'returns error message' do
+      expect { data_integrity.valid? }.to raise_error 'Module does not exist'
+    end
+  end
+
+  describe 'when given an array of consecutive numbers' do
+    context 'and is not consecutive' do
+      let(:nums) { [1,2,3,4,6] }
+      
+      specify do
+        expect(data_integrity.consecutive_nums_start_at_one?(nums)).to eq false
+      end
+    end
+
+    context 'and is consecutive but does not start at 1' do
+      let(:nums) { [2,3,4,5] }
+      
+      specify do
+        expect(data_integrity.consecutive_nums_start_at_one?(nums)).to eq false
+      end
+    end
+
+    context 'and is consecutive and starts at 1' do
+      let(:nums) { [1,2,3,4,5] }
+
+      specify do
+        expect(data_integrity.consecutive_nums_start_at_one?(nums)).to eq true
+      end
+    end
+  end
+
+  describe 'formative' do
+    let(:questions) { data_integrity.find_module_by_name.formative_questions }
+    
+    specify { expect(questions.count).to eq 3 }
+
+    let(:mod) { 'bravo' }
+    specify { expect(questions.count).to eq 1 }
+    
+
+    let(:mod) { 'charlie' }
+    specify { expect(questions.count).to eq 1 }
+      
+    # expect(questions['bravo'].count).to be 1
+    # expect(questions['charlie'].count).to be 1
+
+    # expect(questions['child-development-and-the-eyfs'].count).to be 17
+    # expect(questions['brain-development-and-how-children-learn'].count).to be 22
+    # expect(questions['personal-social-and-emotional-development'].count).to be 19
+    # expect(questions['module-4'].count).to be 19
+
+    # expect(questions_total).to be 82
+  end
+
+  describe 'test modules' do
+    let(:formative) { data_integrity.find_module_by_name.formative_questions }
+    let(:summative) { data_integrity.find_module_by_name.summative_questions }
+    let(:confidence) { data_integrity.find_module_by_name.confidence_questions }
+    
+    describe 'alpha' do
+      let(:mod) { 'alpha' }
+      
+      specify { expect(formative.count).to eq 3 }
+      specify { expect(summative.count).to eq 4 }
+      specify { expect(confidence.count).to eq 3 }
+    end
+    
+    describe 'bravo' do
+      let(:mod) { 'bravo' }
+      
+      specify { expect(formative.count).to eq 1 }
+      specify { expect(summative.count).to eq 2 }
+      specify { expect(confidence.count).to eq 3 }
+    end
+    
+    describe 'charlie' do
+      let(:mod) { 'charlie' }
+      
+      specify { expect(formative.count).to eq 1 }
+      specify { expect(summative.count).to eq 2 }
+      specify { expect(confidence.count).to eq 3 }
     end
   end
 end
