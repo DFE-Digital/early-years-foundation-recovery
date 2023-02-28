2ef44c244678a0b714128ca2a2961027c4e68193
Brett McHargue <brett.mchargue@and.digital>
2023-02-28 07:33:52 +0000

    Update driver configuration for qa

--- ui/drivers/chrome.rb
+++ ui/drivers/chrome.rb
@@ -9,47 +9,43 @@ module Drivers
 
     # @return [Capybara]
     def self.register
-      capabilities = Selenium::WebDriver::Remote::Capabilities.chrome
-      capabilities['acceptInsecureCerts'] = true
+      options = Selenium::WebDriver::Options.chrome(accept_insecure_certs: true)
 
       Capybara.register_driver :chrome do |app|
-        Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: capabilities)
+        Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
       end
     end
 
     # @return [Capybara]
     def self.register_headless
       # https://chromedriver.chromium.org/capabilities
-      capabilities = Selenium::WebDriver::Remote::Capabilities.chrome
-      capabilities['acceptInsecureCerts'] = true
       # https://peter.sh/experiments/chromium-command-line-switches
-      capabilities['goog:chromeOptions'] = {
-        args: %w[
+
+      options = Selenium::WebDriver::Options.chrome(
+        accept_insecure_certs: true,
+        'goog:chromeOptions': %w[
           headless
           disable-extensions
           disable-gpu
           no-sandbox
           window-size=1280,800
         ],
-      }
-
+      )
       Capybara.register_driver :headless_chrome do |app|
-        Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: capabilities)
+        Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
       end
     end
 
     # @return [Capybara]
     def self.register_remote
-      capabilities = Selenium::WebDriver::Remote::Capabilities.chrome
-      capabilities['acceptInsecureCerts'] = true
       driver = ENV.fetch('DRIVER', 'localhost:4441')
-
+      options = Selenium::WebDriver::Options.chrome(accept_insecure_certs: true)
       Capybara.register_driver :standalone_chrome do |app|
         Capybara::Selenium::Driver.new(
           app,
           browser: :remote,
           url: "http://#{driver}/wd/hub",
-          capabilities: capabilities,
+          options: options,
         )
       end
     end
--- ui/drivers/firefox.rb
+++ ui/drivers/firefox.rb
@@ -9,47 +9,41 @@ module Drivers
 
     # @return [Capybara]
     def self.register
-      capabilities = Selenium::WebDriver::Remote::Capabilities.firefox
-      capabilities['acceptInsecureCerts'] = true
+      options = Selenium::WebDriver::Options.firefox(accept_insecure_certs: true)
 
       Capybara.register_driver :firefox do |app|
-        Capybara::Selenium::Driver.new(app, browser: :firefox, capabilities: capabilities)
+        Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
       end
     end
 
     # @return [Capybara]
     def self.register_headless
-      # https://developer.mozilla.org/en-US/docs/Web/WebDriver/Capabilities
-      capabilities = Selenium::WebDriver::Remote::Capabilities.firefox
-      capabilities['acceptInsecureCerts'] = true
-      # https://wiki.mozilla.org/Firefox/CommandLineOptions
-      capabilities['moz:firefoxOptions'] = {
-        args: %w[
-          -headless
-          disable-extensions
-          disable-gpu
-          no-sandbox
-          window-size=1280,800
-        ],
-      }
+      options = Selenium::WebDriver::Options.firefox(accept_insecure_certs: true,
+                                                     'moz:firefoxOptions': %w[
+                                                       -headless
+                                                       disable-extensions
+                                                       disable-gpu
+                                                       no-sandbox
+                                                       window-size=1280,80
+                                                     ])
 
       Capybara.register_driver :headless_firefox do |app|
-        Capybara::Selenium::Driver.new(app, browser: :firefox, capabilities: capabilities)
+        Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
       end
     end
 
     # @return [Capybara]
     def self.register_remote
-      capabilities = Selenium::WebDriver::Remote::Capabilities.firefox
-      capabilities['acceptInsecureCerts'] = true
       driver = ENV.fetch('DRIVER', 'localhost:4442')
+
+      options = Selenium::WebDriver::Options.firefox(accept_insecure_certs: true)
 
       Capybara.register_driver :standalone_firefox do |app|
         Capybara::Selenium::Driver.new(
           app,
           browser: :remote,
           url: "http://#{driver}/wd/hub",
-          capabilities: capabilities,
+          options: options,
         )
       end
     end
