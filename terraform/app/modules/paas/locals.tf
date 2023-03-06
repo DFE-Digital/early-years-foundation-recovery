5037b75a181d3252691fea79926bb8238bea7349
Peter David Hamilton <pete@peterdavidhamilton.com>
2023-03-06 14:05:03 +0000

    fix terraform module config

--- terraform/app/modules/paas/locals.tf
+++ terraform/app/modules/paas/locals.tf
@@ -4,4 +4,5 @@ locals {
   app_service_bindings     = concat(local.app_cf_service_instances)
   db_service_name          = "${var.web_app_name}-db"
   db_json_params           = jsonencode(var.postgres_json_params)
+  enable_worker            = var.app_environment == "content" ? false : true
 }
