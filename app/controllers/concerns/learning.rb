51148c072fd727e441a95e55d2e76587e31086ea
Peter David Hamilton <pete@peterdavidhamilton.com>
2023-04-21 16:23:56 +0100

    ensure unused controller module defines itself

--- app/controllers/concerns/learning.rb
+++ app/controllers/concerns/learning.rb
@@ -1,36 +1,32 @@
-# module Learning
-#   extend ActiveSupport::Concern
+#
+# OPTIMIZE: potentially dry-er course content controllers
+#
+module Learning
+  # def mod_name
+  #   params[:training_module_id]
+  # end
 
-#   # @return [String]
-#   def mod_name
-#     params[:training_module_id]
-#   end
+  # def content_name
+  #   params[:id]
+  # end
 
-#   # @return [String]
-#   def content_name
-#     params[:id]
-#   end
+  # def mod
+  #   Training::Module.by_name(mod_name)
+  # end
 
-#   # @return [Training::Module] shallow
-#   def mod
-#     Training::Module.by_name(mod_name)
-#   end
+  # def content
+  #   mod.page_by_name(content_name)
+  # end
 
-#   # @return [Training::Page, Training::Video, Training::Question]
-#   def content
-#     mod.page_by_name(content_name)
-#   end
+  # def module_progress
+  #   ContentfulModuleOverviewDecorator.new(progress)
+  # end
 
-#   def module_progress
-#     ContentfulModuleOverviewDecorator.new(progress)
-#   end
+  # def progress_bar
+  #   ContentfulModuleProgressBarDecorator.new(progress)
+  # end
 
-#   def progress_bar
-#     ContentfulModuleProgressBarDecorator.new(progress)
-#   end
-
-#   def progress
-#     helpers.cms_module_progress(mod)
-#   end
-
-# end
+  # def progress
+  #   helpers.cms_module_progress(mod)
+  # end
+end
