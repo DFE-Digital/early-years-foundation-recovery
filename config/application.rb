477a2313af5d203bf509b778ed2b73aaf3673d07
Peter David Hamilton <pete@peterdavidhamilton.com>
2023-04-24 11:24:26 +0100

    Try setting app TZ without fetch

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module EarlyYearsFoundationRecovery
     # config.eager_load_paths << Rails.root.join("extras")
 
     # config.time_zone = ENV.fetch('TZ', 'Europe/London')
+    config.time_zone = ENV['TZ']
 
     config.generators do |g|
       g.test_framework :rspec
