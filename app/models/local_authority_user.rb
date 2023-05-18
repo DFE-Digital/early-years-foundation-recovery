5a937f63c6f52f9c6b3d4e2f2eb37f453265f8aa
jack.coggin <jack.coggin@and.digital>
2023-05-18 16:26:45 +0100

    move local_authority_user into models and refactor for readability

--- app/models/local_authority_user.rb
+++ app/models/local_authority_user.rb
@@ -2,7 +2,7 @@ class LocalAuthorityUser
   include ToCsv
 
   # @return [String]
-  def self.to_csv
+  def to_csv
     all_users, registration_complete = get_local_auth_data
     CSV.generate do |csv|
       csv << ['Local Authority', 'Total Users', 'Registration Completed']
@@ -13,22 +13,29 @@ class LocalAuthorityUser
     end
   end
 
+private
+
   # @param users [ActiveRecord::Relation]
-  # @return [Hash]
-  def self.count_by_local_authority(users)
+  # @return [Hash] Hash of local authorities and their corresponding counts
+  def count_by_local_authority(users)
     users.group(:local_authority).count
   end
 
   # @return [Array<Hash>]
-  def self.get_local_auth_data
-    all_users = count_by_local_authority(post_public_beta)
+  def get_local_auth_data
+    all_users = count_by_local_authority(filter_users)
     registration_complete = count_by_local_authority(User.registration_complete)
     [all_users, registration_complete]
   end
 
-  # @return [ActiveRecord::Relation] Users created after public beta launch
-  def self.post_public_beta
-    User.where(created_at: Time.zone.local(2023, 2, 9, 15, 0, 0)..Time.zone.now)
+  # @return [ActiveRecord::Relation] Users created after public beta launch with non-nil LAs
+  def filter_users
+    User.where(created_at: public_beta_launch..Time.zone.now)
     .where.not(local_authority: nil)
+  end
+
+  # @return [ActiveSupport::TimeWithZone]
+  def public_beta_launch
+    Time.zone.local(2023, 2, 9, 15, 0, 0)
   end
 end
