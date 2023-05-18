12fdda1d391f57a1f7c3061e5d03fbc67fb4df02
jack.coggin <jack.coggin@and.digital>
2023-05-18 10:24:07 +0100

    update tests and class methods

--- app/services/local_authority_user.rb
+++ app/services/local_authority_user.rb
@@ -1,14 +1,14 @@
 class LocalAuthorityUser
   include ToCsv
 
-  def group_by_local_authority
+  def self.group_by_local_authority
     post_public_beta = User.where(created_at: Time.zone.local(2023, 2, 9, 15, 0, 0)..Time.zone.now).where.not(local_authority: nil)
     all_users = post_public_beta.group(:local_authority).count
     registration_complete = User.registration_complete.group(:local_authority).count
     [all_users, registration_complete]
   end
 
-  def to_csv
+  def self.to_csv
     all_users, registration_complete = group_by_local_authority
     CSV.generate do |csv|
       csv << ['Local Authority', 'Total Users', 'Registration Completed']
