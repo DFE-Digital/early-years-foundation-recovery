578d61c47e06e6bea006b2446a5accda6f3fcb7c
jack.coggin <jack.coggin@and.digital>
2023-05-18 09:59:53 +0100

    add unit tests for local_authority_user class

--- app/services/local_authority_user.rb
+++ app/services/local_authority_user.rb
@@ -1,22 +1,21 @@
 class LocalAuthorityUser
-    include ToCsv
+  include ToCsv
 
-    def self.group_by_local_authority
-        post_public_beta = User.where(created_at: DateTime.new(2023, 2, 9, 15, 0, 0)..DateTime.now).where.not(local_authority: nil)
-        all_users = post_public_beta.group(:local_authority).count
-        registration_complete = User.registration_complete.group(:local_authority).count
-        [all_users, registration_complete]
-    end
-  
-    def self.to_csv
-        all_users, registration_complete = self.group_by_local_authority
-        csv_data = CSV.generate do |csv|
-          csv << ['Local Authority', 'Total Users', 'Registration Completed']
-          all_users.each do |local_authority, count|
-            complete_count = registration_complete[local_authority]
-            csv << [local_authority, count, complete_count]
-          end
-        end
-        csv_data
+  def group_by_local_authority
+    post_public_beta = User.where(created_at: Time.zone.local(2023, 2, 9, 15, 0, 0)..Time.zone.now).where.not(local_authority: nil)
+    all_users = post_public_beta.group(:local_authority).count
+    registration_complete = User.registration_complete.group(:local_authority).count
+    [all_users, registration_complete]
+  end
+
+  def to_csv
+    all_users, registration_complete = group_by_local_authority
+    CSV.generate do |csv|
+      csv << ['Local Authority', 'Total Users', 'Registration Completed']
+      all_users.each do |local_authority, count|
+        complete_count = registration_complete[local_authority].to_i
+        csv << [local_authority, count, complete_count]
       end
     end
+  end
+end
---
+++ spec/models/services/local_authority_user_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+RSpec.describe LocalAuthorityUser do
+  subject(:local_auth) do
+    described_class.new
+  end
+
+  let(:registered) do
+    create(:user, :registered, local_authority: 'LA1', created_at: Time.zone.now)
+  end
+
+  let(:registered2) do
+    create(:user, :registered, local_authority: 'LA1', created_at: Time.zone.now)
+  end
+
+  let(:unregistered) do
+    create(:user, registration_complete: false, local_authority: 'LA2', created_at: Time.zone.now)
+  end
+
+  let(:pre_public_beta) do
+    create(:user, :registered, local_authority: 'LA3', created_at: Time.zone.local(2023, 2, 9, 14, 0, 0))
+  end
+
+  describe '.to_csv' do
+    context 'when no users exist' do
+      it 'creates csv data with columns but no rows' do
+        expect(local_auth.to_csv).to eq("Local Authority,Total Users,Registration Completed\n")
+      end
+    end
+
+    context 'when users created before 09/02/23 exist' do
+      it 'creates csv data and ignores users created before that data' do
+        expect(pre_public_beta.created_at).to eq(Time.zone.local(2023, 2, 9, 14, 0, 0))
+        expect(local_auth.to_csv).to eq("Local Authority,Total Users,Registration Completed\n")
+      end
+    end
+
+    context 'when users have not completed registration' do
+      it 'creates csv data and includes users in the total but not registration_completed' do
+        expect(unregistered.local_authority).to eq('LA2')
+        expect(unregistered.registration_complete).to eq(false)
+        expect(local_auth.to_csv).to eq("Local Authority,Total Users,Registration Completed\nLA2,1,0\n")
+      end
+    end
+
+    context 'when a user has completed registration' do
+      it 'creates csv data and includes the user in the total and registration_completed' do
+        expect(registered.local_authority).to eq('LA1')
+        expect(registered.registration_complete).to eq(true)
+        expect(local_auth.to_csv).to eq("Local Authority,Total Users,Registration Completed\nLA1,1,1\n")
+      end
+    end
+
+    context 'when multiple users have the same local authority' do
+      it 'creates csv data and includes the users in the total and registration_completed for that local authority' do
+        expect(registered.local_authority).to eq('LA1')
+        expect(registered2.local_authority).to eq('LA1')
+        expect(registered.registration_complete).to eq(true)
+        expect(registered2.registration_complete).to eq(true)
+        expect(local_auth.to_csv).to eq("Local Authority,Total Users,Registration Completed\nLA1,2,2\n")
+      end
+    end
+  end
+end
