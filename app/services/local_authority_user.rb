8d5c63c0008422c761e2a03e4d8da599cd64b537
jack.coggin <jack.coggin@and.digital>
2023-05-18 11:45:57 +0100

    break into private methods

--- app/services/local_authority_user.rb
+++ app/services/local_authority_user.rb
@@ -1,17 +1,9 @@
 class LocalAuthorityUser
   include ToCsv
 
-  # @return [Array<Hash>]
-  def self.group_by_local_authority
-    post_public_beta = User.where(created_at: Time.zone.local(2023, 2, 9, 15, 0, 0)..Time.zone.now).where.not(local_authority: nil)
-    all_users = post_public_beta.group(:local_authority).count
-    registration_complete = User.registration_complete.group(:local_authority).count
-    [all_users, registration_complete]
-  end
-
   # @return [String]
   def self.to_csv
-    all_users, registration_complete = group_by_local_authority
+    all_users, registration_complete = get_local_auth_data
     CSV.generate do |csv|
       csv << ['Local Authority', 'Total Users', 'Registration Completed']
       all_users.each do |local_authority, count|
@@ -19,5 +11,24 @@ class LocalAuthorityUser
         csv << [local_authority, count, complete_count]
       end
     end
+  end
+
+  # @param users [ActiveRecord::Relation]
+  # @return [Hash]
+  def self.count_by_local_authority(users)
+    users.group(:local_authority).count
+  end
+
+  # @return [Array<Hash>]
+  def self.get_local_auth_data
+    all_users = count_by_local_authority(post_public_beta)
+    registration_complete = count_by_local_authority(User.registration_complete)
+    [all_users, registration_complete]
+  end
+
+  # @return [ActiveRecord::Relation] Users created after public beta launch
+  def self.post_public_beta
+    User.where(created_at: Time.zone.local(2023, 2, 9, 15, 0, 0)..Time.zone.now)
+    .where.not(local_authority: nil)
   end
 end
