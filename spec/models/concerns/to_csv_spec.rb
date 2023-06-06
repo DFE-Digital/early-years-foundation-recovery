f1b5b25ee2066248795be1ccf4eba5bdcd597403
jack.coggin <jack.coggin@and.digital>
2023-06-06 10:57:09 +0100

    update to_csv spec

--- spec/models/concerns/to_csv_spec.rb
+++ spec/models/concerns/to_csv_spec.rb
@@ -1,22 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe ToCsv do
-  let(:to_csv) { Class.new { include ToCsv } }
-
   describe '.generate_csv' do
     context 'when given headers and data' do
+      let(:to_csv) do
+        Class.new do
+          include ToCsv
+
+          def self.column_names
+            ['Custom Column 1', 'Custom Column 2']
+          end
+
+          def self.dashboard
+            [%w[data_1 data_2], %w[data_3 data_4]]
+          end
+        end
+      end
+
       it 'returns a csv string' do
-        headers = %w[header_1 header_2]
-        data = [%w[data_1 data_2], %w[data_3 data_4]]
-        expect(to_csv.generate_csv(headers, data)).to eq("header_1,header_2\ndata_1,data_2\ndata_3,data_4\n")
+        expect(to_csv.generate_csv).to eq("Custom Column 1,Custom Column 2\ndata_1,data_2\ndata_3,data_4\n")
       end
     end
 
     context 'when given empty headers and data' do
+      let(:to_csv) do
+        Class.new do
+          include ToCsv
+
+          def self.column_names
+            []
+          end
+
+          def self.dashboard
+            []
+          end
+        end
+      end
+
       it 'returns an empty csv string' do
-        headers = []
-        data = []
-        expect(to_csv.generate_csv(headers, data)).to eq("\n")
+        expect(to_csv.generate_csv).to eq("\n")
       end
     end
   end
