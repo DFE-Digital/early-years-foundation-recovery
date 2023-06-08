04d0aced562cdf279141dbfbdf74908a1767f493
sunny-sidhu-and <97452339+sunny-sidhu-and@users.noreply.github.com>
2023-06-08 10:50:22 +0100

    ER-673-create-terraform-configuration-dev (#656)
    * ER-685: Resource Group
    
    * ER-673: README and LICENSE files
    
    * ER-684: Terraform Remote State
    
    * ER-673: Unused variable
    
    ---------
    
    Co-authored-by: Sunny Sidhu <ssidhu@and.digital>

---
+++ terraform-azure/main.tf
@@ -0,0 +1,26 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  # Common tags to be assigned to all resources
+  common_tags = {
+    "Environment"      = var.default_environment
+    "Parent Business"  = "Children’s Care"
+    "Portfolio"        = "Newly Onboarded"
+    "Product"          = "EY Recovery"
+    "Service"          = "Newly Onboarded"
+    "Service Line"     = "Newly Onboarded"
+    "Service Offering" = "EY Recovery"
+  }
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_name_prefix}-rg"
+  location = var.default_azure_region
+
+  tags = merge(local.common_tags, {
+    "Region" = var.default_azure_region
+  })
+}
