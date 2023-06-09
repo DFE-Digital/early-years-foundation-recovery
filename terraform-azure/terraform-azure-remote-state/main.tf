68c4fa4ce20d9792d4b4c3263e13ad57d9737927
Sunny Sidhu <ssidhu@and.digital>
2023-06-09 11:20:05 +0100

    ER-674: Failing unit tests

--- terraform-azure/terraform-azure-remote-state/main.tf
+++ terraform-azure/terraform-azure-remote-state/main.tf
@@ -48,8 +48,37 @@ resource "azurerm_storage_account" "tfstate" {
   resource_group_name             = azurerm_resource_group.tfstate.name
   location                        = var.default_azure_region
   account_tier                    = "Standard"
+  min_tls_version                 = "TLS1_2"
   account_replication_type        = "LRS"
   allow_nested_items_to_be_public = false
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    hour_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    minute_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+
+    #checkov:skip=CKV_AZURE_206:GRS not required
+    #checkov:skip=CKV_AZURE_59:Argument has been deprecated
+    #checkov:skip=CKV2_AZURE_18:Microsoft Managed keys are sufficient
+    #checkov:skip=CKV2_AZURE_1:Microsoft Managed keys are sufficient
+    #checkov:skip=CKV2_AZURE_21:Logging enabled through queue argument
+    #checkov:skip=CKV2_AZURE_38:Soft-delete not required
+  }
 
   tags = merge(local.common_tags, {
     "Region" = var.default_azure_region
