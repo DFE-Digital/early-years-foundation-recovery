725154f6bfd33910c0dc854a298b8747e8cb9b63
sunny-sidhu-and <97452339+sunny-sidhu-and@users.noreply.github.com>
2023-06-13 11:01:03 +0100

    Update terraform.tf

--- terraform-azure/terraform.tf
+++ terraform-azure/terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "=3.7.0"
     }
   }
 
@@ -14,5 +14,8 @@ terraform {
     storage_account_name = "eyrecoverytfstatekoi8mst"
     container_name       = "s187d01-eyrecovery-tfstate-stc"
     key                  = "terraform.tfstate"
+    use_oidc             = true
+    subscription_id      = var.subscription_id
+    tenant_id            = var.tenant_id
   }
 }
