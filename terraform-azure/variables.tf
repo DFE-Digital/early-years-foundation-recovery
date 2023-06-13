73dd6767e7a130fad24b93228d9cfb64ef75d5da
sunny-sidhu-and <97452339+sunny-sidhu-and@users.noreply.github.com>
2023-06-13 11:01:26 +0100

    Update variables.tf

--- terraform-azure/variables.tf
+++ terraform-azure/variables.tf
@@ -9,3 +9,11 @@ variable "default_environment" {
 variable "resource_name_prefix" {
   default = "s187d01-eyrecovery"
 }
+
+variable "subscription_id" {
+  default = "00000000-0000-0000-0000-000000000000"
+}
+
+variable "tenant_id" {
+  default = "00000000-0000-0000-0000-000000000000"
+}
