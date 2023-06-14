6eb158485ac5aa3d8f19f70512d50c2d829930e2
sunny-sidhu-and <97452339+sunny-sidhu-and@users.noreply.github.com>
2023-06-14 10:50:36 +0100

    ER-674-create-github-actions-workflow (#658)
    * ER-685: Resource Group
    
    * ER-673: README and LICENSE files
    
    * ER-684: Terraform Remote State
    
    * ER-673: Unused variable
    
    * ER-674: GitHub-Azure Login
    
    * ER-674: Terraform Unit Tests
    
    * ER-674: Incorrect syntax
    
    * ER-674: Change to Terraform directory before running tests
    
    * ER-674: Working Directory for Tests
    
    * ER-674: Working Directory Syntax
    
    * ER674: Formatting
    
    * ER-674: Failing unit tests
    
    * ER-674: Failing unit tests
    
    * ER-674: DEV Deploy Workflow
    
    * ER-674: Descriptive name
    
    * ER-674: Branch check not needed
    
    * ER-674: Workflow Dispatch trigger
    
    * ER-674: Terraform OIDC Authentication
    
    ---------
    
    Co-authored-by: Sunny Sidhu <ssidhu@and.digital>

--- terraform-azure/terraform-azure-remote-state/main.tf
+++ terraform-azure/terraform-azure-remote-state/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "=3.7.0"
     }
   }
 
@@ -44,12 +44,41 @@ resource "random_string" "resource_code" {
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "eyrecoverytfstate${random_string.resource_code.result}st"
-  resource_group_name      = azurerm_resource_group.tfstate.name
-  location                 = var.default_azure_region
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                            = "eyrecoverytfstate${random_string.resource_code.result}st"
+  resource_group_name             = azurerm_resource_group.tfstate.name
+  location                        = var.default_azure_region
+  account_tier                    = "Standard"
+  min_tls_version                 = "TLS1_2"
+  account_replication_type        = "LRS"
   allow_nested_items_to_be_public = false
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    hour_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+    minute_metrics {
+      enabled               = true
+      include_apis          = true
+      version               = "1.0"
+      retention_policy_days = 10
+    }
+
+    #checkov:skip=CKV_AZURE_206:GRS not required
+    #checkov:skip=CKV_AZURE_59:Argument has been deprecated
+    #checkov:skip=CKV2_AZURE_18:Microsoft Managed keys are sufficient
+    #checkov:skip=CKV2_AZURE_1:Microsoft Managed keys are sufficient
+    #checkov:skip=CKV2_AZURE_38:Soft-delete not required
+    #checkov:skip=CKV2_AZURE_33:VNet not configured
+  }
 
   tags = merge(local.common_tags, {
     "Region" = var.default_azure_region
@@ -60,4 +89,6 @@ resource "azurerm_storage_container" "tfstate" {
   name                  = "${var.resource_name_prefix}-tfstate-stc"
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
+
+  #checkov:skip=CKV2_AZURE_21:Logging not required
 }
