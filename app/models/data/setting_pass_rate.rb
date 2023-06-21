0879feecc0b8d8718e97a5840e60bea23184cd4b
jack.coggin <jack.coggin@and.digital>
2023-06-21 13:00:48 +0100

    fix error in case of nil data for role/setting type

--- app/models/data/setting_pass_rate.rb
+++ app/models/data/setting_pass_rate.rb
@@ -9,11 +9,25 @@ module Data
 
     # @return [Hash{Symbol => Mixed}]
     def self.dashboard
+      result = {
+        type: [],
+        pass_percentage: [],
+        pass_count: [],
+        fail_percentage: [],
+        fail_count: []
+
+      }
       data = SummativeQuiz.attribute_pass_percentage(:setting_type).map do |setting_type, percentages|
         [setting_type] + percentages.values
       end
-      keys = %i[type pass_percentage pass_count fail_percentage fail_count]
-      keys.zip(data.transpose).to_h.transform_values(&:flatten)
+      if !data.empty?
+        result[:type] = data.transpose[0]
+        result[:pass_percentage] = data.transpose[1]
+        result[:pass_count] = data.transpose[2]
+        result[:fail_percentage] = data.transpose[3]
+        result[:fail_count] = data.transpose[4]
+      end
+      result
     end
   end
 end
