50bcec988669df00db742c7e8380c977a1add0d4
jack.coggin <jack.coggin@and.digital>
2023-06-28 08:52:25 +0100

    update module overview spec

--- spec/models/data/module_overview_spec.rb
+++ spec/models/data/module_overview_spec.rb
@@ -70,6 +70,22 @@ RSpec.describe Data::ModuleOverview do
         pass_assessment: 0,
         fail_assessment: 0,
       },
+      {
+        module_name: 'delta',
+        total_users: 3,
+        not_started: 3,
+        started: 0,
+        in_progress: 0,
+        completed: 0,
+        true_false: Rails.application.cms? ? 0 : 'N/A',
+        module_start: 0,
+        module_complete: 0,
+        confidence_check_start: 0,
+        confidence_check_complete: 0,
+        start_assessment: 0,
+        pass_assessment: 0,
+        fail_assessment: 0,
+      },
     ]
   end
 
