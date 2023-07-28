376d3b10b76e9bd4e216d20f783e4cb2828dc8d0
jack.coggin <jack.coggin@and.digital>
2023-07-28 14:10:25 +0100

    wip: update returning users time period

--- app/models/data/returning_users.rb
+++ app/models/data/returning_users.rb
@@ -23,26 +23,23 @@ module Data
 
       # @return [Integer]
       def weekly_returning_users
-        previous_week_visits = Ahoy::Visit.where(started_at: 2.weeks.ago..1.week.ago)
-        current_week_visits = Ahoy::Visit.where(started_at: 1.week.ago..Time.zone.now)
-        returning_users = previous_week_visits.where(user_id: current_week_visits.pluck(:user_id))
-        returning_users.distinct.count(:user_id)
+        current_week_visits = Ahoy::Visit.where(started_at: last_week.beginning_of_week..last_week.end_of_week)
+        returning_users = current_week_visits.group(:user_id).having('count(user_id) > 1').count
+        returning_users.count
       end
 
       # @return [Integer]
       def monthly_returning_users
-        previous_month_visits = Ahoy::Visit.where(started_at: 2.months.ago..1.month.ago)
-        current_month_visits = Ahoy::Visit.where(started_at: 1.month.ago..Time.zone.now)
-        returning_users = previous_month_visits.where(user_id: current_month_visits.pluck(:user_id))
-        returning_users.distinct.count(:user_id)
+        current_month_visits = Ahoy::Visit.where(started_at: last_month.beginning_of_month..last_month.end_of_month)
+        returning_users = current_month_visits.group(:user_id).having('count(user_id) > 1').count
+        returning_users.count
       end
 
       # @return [Integer]
       def two_monthly_returning_users
-        previous_two_month_visits = Ahoy::Visit.where(started_at: 4.months.ago..2.months.ago)
         current_two_month_visits = Ahoy::Visit.where(started_at: 2.months.ago..Time.zone.now)
-        returning_users = previous_two_month_visits.where(user_id: current_two_month_visits.pluck(:user_id))
-        returning_users.distinct.count(:user_id)
+        returning_users = current_two_month_visits.group(:user_id).having('count(user_id) > 1').count
+        returning_users.count
       end
 
       # @return [Date]
@@ -50,17 +47,11 @@ module Data
         Time.zone.today.beginning_of_quarter
       end
 
-      # @return [Date]
-      def previous_quarter
-        current_quarter - 3.months
-      end
-
       # @return [Integer]
       def quarterly_returning_users
-        previous_quarter_visits = Ahoy::Visit.where(started_at: previous_quarter..current_quarter)
         current_quarter_visits = Ahoy::Visit.where(started_at: current_quarter..Time.zone.today)
-        returning_users = previous_quarter_visits.where(user_id: current_quarter_visits.pluck(:user_id))
-        returning_users.distinct.count(:user_id)
+        returning_users = current_quarter_visits.group(:user_id).having('count(user_id) > 1').count
+        returning_users.count
       end
     end
   end
