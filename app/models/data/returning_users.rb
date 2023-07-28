04cf3d9f5adfa8dd03afea6e8cb5ebda9cb3ab35
jack.coggin <jack.coggin@and.digital>
2023-07-28 15:43:38 +0100

    update returning users data model

--- app/models/data/returning_users.rb
+++ app/models/data/returning_users.rb
@@ -4,7 +4,7 @@ module Data
     class << self
       # @return [Array<String>]
       def column_names
-        ['Weekly Returning Users', 'Monthly Returning Users', '2 Monthly Returning Users', 'Quarterly Returning Users']
+        ['Weekly Returning Users', 'Monthly Returning Users', 'Quarterly Returning Users']
       end
 
       # @return [Array<Hash{Symbol => Mixed}>]
@@ -13,7 +13,6 @@ module Data
           {
             weekly_returning_users: weekly_returning_users,
             monthly_returning_users: monthly_returning_users,
-            two_monthly_returning_users: two_monthly_returning_users,
             quarterly_returning_users: quarterly_returning_users,
           },
         ]
@@ -23,33 +22,21 @@ module Data
 
       # @return [Integer]
       def weekly_returning_users
-        current_week_visits = Ahoy::Visit.where(started_at: last_week.beginning_of_week..last_week.end_of_week)
+        current_week_visits = Ahoy::Visit.where(started_at: Time.zone.today.last_week.beginning_of_week..Time.zone.today.last_week.end_of_week)
         returning_users = current_week_visits.group(:user_id).having('count(user_id) > 1').count
         returning_users.count
       end
 
       # @return [Integer]
       def monthly_returning_users
-        current_month_visits = Ahoy::Visit.where(started_at: last_month.beginning_of_month..last_month.end_of_month)
+        current_month_visits = Ahoy::Visit.where(started_at: Time.zone.today.last_month.beginning_of_month..Time.zone.today.last_month.end_of_month)
         returning_users = current_month_visits.group(:user_id).having('count(user_id) > 1').count
         returning_users.count
       end
 
       # @return [Integer]
-      def two_monthly_returning_users
-        current_two_month_visits = Ahoy::Visit.where(started_at: 2.months.ago..Time.zone.now)
-        returning_users = current_two_month_visits.group(:user_id).having('count(user_id) > 1').count
-        returning_users.count
-      end
-
-      # @return [Date]
-      def current_quarter
-        Time.zone.today.beginning_of_quarter
-      end
-
-      # @return [Integer]
       def quarterly_returning_users
-        current_quarter_visits = Ahoy::Visit.where(started_at: current_quarter..Time.zone.today)
+        current_quarter_visits = Ahoy::Visit.where(started_at: Time.zone.today.beginning_of_quarter..Time.zone.today)
         returning_users = current_quarter_visits.group(:user_id).having('count(user_id) > 1').count
         returning_users.count
       end
--- spec/models/data/returning_users_spec.rb
+++ spec/models/data/returning_users_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Data::ReturningUsers do
   let!(:user_1) { create(:user, :registered, :agency_setting, role_type: 'childminder') }
 
   let(:headers) do
-    ['Weekly Returning Users', 'Monthly Returning Users', '2 Monthly Returning Users', 'Quarterly Returning Users']
+    ['Weekly Returning Users', 'Monthly Returning Users', 'Quarterly Returning Users']
   end
 
   let(:rows) do
@@ -12,20 +12,18 @@ RSpec.describe Data::ReturningUsers do
       {
         weekly_returning_users: 1,
         monthly_returning_users: 1,
-        two_monthly_returning_users: 1,
         quarterly_returning_users: 1,
       },
     ]
   end
 
   before do
-    Ahoy::Visit.create!(id: 1, user_id: user_1.id, started_at: Time.zone.now)
-    Ahoy::Visit.create!(id: 9, user_id: user_1.id, started_at: 1.day.ago)
-    Ahoy::Visit.create!(id: 2, user_id: user_1.id, started_at: 1.week.ago)
-    Ahoy::Visit.create!(id: 3, user_id: user_1.id, started_at: 2.weeks.ago)
-    Ahoy::Visit.create!(id: 4, user_id: user_1.id, started_at: 1.month.ago)
-    Ahoy::Visit.create!(id: 5, user_id: user_1.id, started_at: 2.months.ago)
-    Ahoy::Visit.create!(id: 8, user_id: user_1.id, started_at: 3.months.ago.beginning_of_quarter)
+    Ahoy::Visit.create!(id: 1, user_id: user_1.id, started_at: Time.zone.today.last_week)
+    Ahoy::Visit.create!(id: 2, user_id: user_1.id, started_at: Time.zone.today.last_week)
+    Ahoy::Visit.create!(id: 3, user_id: user_1.id, started_at: Time.zone.today.last_month)
+    Ahoy::Visit.create!(id: 4, user_id: user_1.id, started_at: Time.zone.today.last_month)
+    Ahoy::Visit.create!(id: 5, user_id: user_1.id, started_at: Time.zone.today.beginning_of_quarter)
+    Ahoy::Visit.create!(id: 6, user_id: user_1.id, started_at: Time.zone.today.beginning_of_quarter)
   end
 
   it_behaves_like('a data export model')
