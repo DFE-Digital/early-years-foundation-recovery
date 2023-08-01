c36fce3846652e8911e5da3e7feed680c1cf0bda
jack.coggin <jack.coggin@and.digital>
2023-08-01 09:00:52 +0100

    remove unnecessary mail jobs from hook controller

--- app/controllers/hook_controller.rb
+++ app/controllers/hook_controller.rb
@@ -40,7 +40,6 @@ class HookController < ApplicationController
     )
 
     ContentCheckJob.enqueue
-    check_new_modules
 
     render json: { status: 'content change received' }, status: :ok
   end
@@ -59,7 +58,6 @@ private
   # @return [void]
   def check_new_modules
     mail_service = NudgeMail.new
-    mail_service.call
     existing_modules = TrainingModuleRecord.pluck(:module_id)
     new_modules = Training::Module.ordered.reject { |mod| existing_modules.include?(mod.id) }.reject(&:draft?)
     new_modules.each do |mod|
