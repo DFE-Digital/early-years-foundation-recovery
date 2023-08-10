1be52c9f3145ece061e07d5c25b5fc976c91cc6f
jack.coggin <jack.coggin@and.digital>
2023-08-10 13:50:35 +0100

    rubocop

--- app/jobs/mail_job.rb
+++ app/jobs/mail_job.rb
@@ -1,11 +1,11 @@
 class MailJob < Que::Job
-  
   def run
     return if queued?
+
     NudgeMail.new.call
   end
 
-  private
+private
 
   def queued?
     Que.job_stats.any? { |job| job[:job_class] == 'MailJob' && job[:count] > 1 }
