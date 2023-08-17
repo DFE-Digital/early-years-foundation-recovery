b43f535ae65f23235f2ce54484cfb05cd6ec3d16
Peter David Hamilton <pete@peterdavidhamilton.com>
2023-08-17 11:04:00 +0100

    Export the current month's visits in the daily data export.
    The data will accumulate and reset at the start of the following month

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -6,5 +6,5 @@ class Ahoy::Visit < ApplicationRecord
   has_many :events, class_name: 'Ahoy::Event'
   belongs_to :user, optional: true
 
-  scope :dashboard, -> { where.not(referrer: nil) }
+  scope :dashboard, -> { where(started_at: Time.zone.now.beginning_of_month..Time.zone.now) }
 end
