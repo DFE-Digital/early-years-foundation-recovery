117ff6e30a532a73e36ef6f1f6cf279f4114c80b
Brett McHargue <ebrett@users.noreply.github.com>
2023-08-21 10:50:23 +0100

    Update DfE Header

---
+++ config/initializers/govuk_components.rb
@@ -0,0 +1,5 @@
+Govuk::Components.configure do |config|
+  # config.brand = 'dfe'
+  config.default_header_service_name = 'Early years child development training'
+  config.default_phase_banner_phase = 'beta'
+end
