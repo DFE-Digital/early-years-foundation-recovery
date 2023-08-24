150f0f49f0a0310029777cc68b5af90ff0492e5d
Brett McHargue <ebrett@users.noreply.github.com>
2023-08-24 12:56:07 +0100

    Omitted `if`

--- spec/lib/seed_snippets_spec.rb
+++ spec/lib/seed_snippets_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SeedSnippets do
   subject(:locales) { described_class.new.call }
 
   it 'converts all translations' do
-    expect(locales.count).to be 171
+    expect(locales.count).to be 173
   end
 
   it 'dot separated key -> Page::Resource#name' do
