5c50c6de8a340267c8803f85af75021a77c87daa
Brett McHargue <ebrett@users.noreply.github.com>
2023-08-24 14:28:40 +0100

    Unnecessary configuration

--- config/initializers/govuk_components.rb
+++
@@ -1,5 +0,0 @@
-Govuk::Components.configure do |config|
-  # config.brand = 'dfe'
-  config.default_header_service_name = 'Early years child development training'
-  config.default_phase_banner_phase = 'beta'
-end
