65e80eaa831a01f531232f05b022e66fbece8ab0
Brett McHargue <ebrett@users.noreply.github.com>
2023-08-24 17:30:54 +0100

    Use existing configuration

--- config/initializers/govuk_components.rb
+++ config/initializers/govuk_components.rb
@@ -1,5 +1,5 @@
 Govuk::Components.configure do |config|
   # config.brand = 'dfe'
-  config.default_header_service_name = 'Early years child development training'
+  config.default_header_service_name = Rails.configuration.service_name
   config.default_phase_banner_phase = 'beta'
 end
