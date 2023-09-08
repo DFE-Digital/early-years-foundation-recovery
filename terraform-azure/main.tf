1e787cc847ad3752ad75714854cfd3a57f94f247
Sunny Sidhu <ssidhu@and.digital>
2023-09-08 15:05:15 +0100

    ER-790: Manually assign APP SP Object ID

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -70,6 +70,7 @@ module "webapp" {
   location                                 = var.azure_region
   resource_group                           = azurerm_resource_group.rg.name
   resource_name_prefix                     = var.resource_name_prefix
+  as_service_principal_object_id           = var.as_service_principal_object_id
   asp_sku                                  = var.asp_sku
   webapp_admin_email_address               = var.admin_email_address
   webapp_worker_count                      = var.webapp_worker_count
