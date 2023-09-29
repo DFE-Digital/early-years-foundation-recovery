97fbf862bbf6367512eee0121f46c737a4ac8053
Brett McHargue <ebrett@users.noreply.github.com>
2023-09-29 09:21:05 +0100

    Updates for rubocop (#866)

--- spec/models/data/confidence_check_scores_spec.rb
+++ spec/models/data/confidence_check_scores_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Data::ConfidenceCheckScores do
   before do
-    skip unless ENV['DISABLE_USER_ANSWER'].present?
+    skip if ENV['DISABLE_USER_ANSWER'].blank?
+    create(:response, :confidence_check, training_module: 'module_1', question_name: 'q1', answers: [1])
+    create(:response, :confidence_check, training_module: 'module_1', question_name: 'q2', answers: [2])
+    create(:response, :confidence_check, training_module: 'module_1', question_name: 'q2', answers: [2])
   end
 
   let(:headers) do
@@ -29,12 +32,6 @@ RSpec.describe Data::ConfidenceCheckScores do
         count: 2,
       },
     ]
-  end
-
-  before do
-    create(:response, :confidence_check, training_module: 'module_1', question_name: 'q1', answers: [1])
-    create(:response, :confidence_check, training_module: 'module_1', question_name: 'q2', answers: [2])
-    create(:response, :confidence_check, training_module: 'module_1', question_name: 'q2', answers: [2])
   end
 
   it_behaves_like 'a data export model'
