b27e7b38ca5558b18d8e3d6b0c0d4300d9af8f8a
Peter David Hamilton <pete@peterdavidhamilton.com>
2023-10-25 13:44:29 +0100

    Contentful live-preview

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -214,7 +214,10 @@ Devise.setup do |config|
 
   # Options to be passed to the created cookie. For instance, you can set
   # secure: true in order to force SSL only cookies.
-  # config.rememberable_options = {}
+  config.rememberable_options = {
+    same_site: :none,
+    secure: true
+  }
 
   # ==> Configuration for :validatable
   # Range for password length.
