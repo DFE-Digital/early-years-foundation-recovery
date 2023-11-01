48f4ae55aa6a2b473b80efdb367a34189d887d8c
jack.coggin <jack.coggin@and.digital>
2023-11-01 09:26:46 +0000

    update migration number

--- db/migrate/20231031094611_add_id_token_to_users.rb
+++ db/migrate/20231031094611_add_id_token_to_users.rb
@@ -1,4 +1,4 @@
-class AddIdTokenToUsers < ActiveRecord::Migration[7.1]
+class AddIdTokenToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :id_token, :string
     add_index :users, :id_token, unique: true
