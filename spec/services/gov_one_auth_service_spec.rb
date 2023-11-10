d297b42eaadec941d65376a5584f9f28b9e2ea87
jack.coggin <jack.coggin@and.digital>
2023-11-10 10:22:27 +0000

    update gov one auth service spec

--- spec/services/gov_one_auth_service_spec.rb
+++ spec/services/gov_one_auth_service_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
+RSpec.shared_examples 'an unsuccessful request' do
+  it 'returns an empty hash' do
+    allow(mock_response).to receive(:body).and_return({}.to_json)
+    expect(result).to eq({})
+  end
+end
+
+RSpec.shared_examples 'a successful request' do |payload|
+  it 'returns a hash of user data' do
+    allow(mock_response).to receive(:body).and_return(payload.to_json)
+    expect(result).to eq(payload)
+  end
+end
+
 RSpec.describe GovOneAuthService do
   let(:code) { 'mock_code' }
   let(:mock_response) { instance_double('response') }
@@ -14,47 +28,29 @@ RSpec.describe GovOneAuthService do
   end
 
   describe '#tokens' do
-    let(:token_payload) { { 'access_token' => 'mock_access_token', 'id_token' => 'mock_id_token' } }
+    let(:payload) { { 'access_token' => 'mock_access_token', 'id_token' => 'mock_id_token' } }
+    let(:result) { auth_service.tokens }
 
     context 'when the request is successful' do
-      it 'returns a hash of tokens' do
-        allow(mock_response).to receive(:body).and_return(token_payload.to_json)
-
-        result = auth_service.tokens
-        expect(result).to eq(token_payload)
-      end
+      it_behaves_like 'a successful request'
     end
 
     context 'when the request is unsuccessful' do
-      it 'returns an empty hash' do
-        allow(mock_response).to receive(:body).and_return({}.to_json)
-
-        result = auth_service.tokens
-        expect(result).to eq({})
-      end
+      it_behaves_like 'an unsuccessful request'
     end
   end
 
   describe '#user_info' do
     let(:access_token) { 'mock_access_token' }
-    let(:user_info_payload) { { 'email' => 'test@test.com' } }
+    let(:payload) { { 'email' => 'test@test.com' } }
+    let(:result) { auth_service.user_info(access_token) }
 
     context 'when the request is successful' do
-      it 'returns a hash of user info' do
-        allow(mock_response).to receive(:body).and_return(user_info_payload.to_json)
-
-        result = auth_service.user_info(access_token)
-        expect(result).to eq(user_info_payload)
-      end
+      it_behaves_like 'a successful request'
     end
 
     context 'when the request is unsuccessful' do
-      it 'returns an empty hash' do
-        allow(mock_response).to receive(:body).and_return({}.to_json)
-
-        result = auth_service.user_info(access_token)
-        expect(result).to eq({})
-      end
+      it_behaves_like 'an unsuccessful request'
     end
   end
 end
