bda11baf0025a9f573346faeee5b0d3ab3d25191
Peter David Hamilton <pete@peterdavidhamilton.com>
2023-11-17 10:24:58 +0000

    Users::OmniauthCallbacksController   with an existing gov-one user     redirects to /my-modules   with an existing non-gov-one user     redirects to /my-modules     updates the account   with a new user     creates an account     redirects to complete registration

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -1,58 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Users::OmniauthCallbacksController, type: :controller do
-  before do
-    request.env['devise.mapping'] = Devise.mappings[:user]
+  let(:auth_service) { instance_double(GovOneAuthService) }
+  let(:access_token) { 'mock_access_token' }
+  let(:id_token) { 'mock_id_token' }
+  let(:email) { 'test@example.com' }
+  let(:params) do
+    { 'code' => 'mock_code', 'state' => 'mock_state' }
   end
 
-  describe '#openid_connect' do
-    let(:auth_service) { instance_double(GovOneAuthService) }
-    let(:access_token) { 'mock_access_token' }
-    let(:id_token) { 'mock_id_token' }
-    let(:email) { 'test@example.com' }
+  before do
+    request.env['devise.mapping'] = Devise.mappings[:user]
+    session[:gov_one_auth_state] = 'mock_state'
 
+    allow(GovOneAuthService).to receive(:new).and_return(auth_service)
+    allow(auth_service).to receive(:tokens).and_return({ 'access_token' => access_token, 'id_token' => id_token })
+    allow(auth_service).to receive(:user_info).and_return({ 'email' => email })
+    allow(auth_service).to receive(:jwt_assertion).and_return('mock_jwt_assertion')
+    allow(auth_service).to receive(:decode_id_token).and_return([{ 'sub' => 'mock_sub' }])
+  end
+
+  context 'with a new user' do
     before do
-      allow(GovOneAuthService).to receive(:new).and_return(auth_service)
-      allow(auth_service).to receive(:tokens).and_return({ 'access_token' => access_token, 'id_token' => id_token })
-      allow(auth_service).to receive(:user_info).and_return({ 'email' => email })
-      allow(auth_service).to receive(:jwt_assertion).and_return('mock_jwt_assertion')
-      allow(auth_service).to receive(:decode_id_token).and_return([{ 'sub' => 'mock_sub' }])
-      session[:gov_one_auth_state] = 'mock_state'
+      get :openid_connect, params: params
     end
 
-    context 'when a User does not exist' do
-      it 'creates the user with the email and id_token' do
-        get :openid_connect, params: { 'code' => 'mock_code', 'state' => 'mock_state' }
-        expect(User.find_by(email: email)).to be_truthy
-        expect(User.find_by(gov_one_id: 'mock_sub')).to be_truthy
-        expect(response).to redirect_to(edit_registration_name_path)
-        expect(session[:id_token]).to eq(id_token)
-      end
+    it 'creates an account' do
+      expect(User.find_by(email: email)).to be_truthy
+      expect(User.find_by(gov_one_id: 'mock_sub')).to be_truthy
     end
 
-    context 'when a User email exists' do
-      before do
-        create :user, :registered, email: email
-      end
+    it 'redirects to complete registration' do
+      expect(session[:id_token]).to eq id_token
+      expect(response).to redirect_to edit_registration_name_path
+    end
+  end
 
-      it 'updates the user id_token and signs them in' do
-        get :openid_connect, params: { 'code' => 'mock_code', 'state' => 'mock_state' }
-        expect(User.find_by(gov_one_id: 'mock_sub')).to be_truthy
-        expect(response).to redirect_to(my_modules_path)
-        expect(session[:id_token]).to eq(id_token)
-      end
+  context 'with an existing non-gov-one user' do
+    before do
+      create :user, :registered, email: email
+      get :openid_connect, params: params
     end
 
-    context 'when a User id_token exists' do
-      before do
-        create :user, :registered, gov_one_id: 'mock_sub'
-      end
+    it 'updates the account' do
+      expect(User.find_by(gov_one_id: 'mock_sub')).to be_truthy
+    end
 
-      it 'signs the user in' do
-        get :openid_connect, params: { 'code' => 'mock_code', 'state' => 'mock_state' }
-        expect(response).to redirect_to(my_modules_path)
-        expect(session[:id_token]).to eq(id_token)
-      end
+    it 'redirects to /my-modules' do
+      expect(session[:id_token]).to eq id_token
+      expect(response).to redirect_to my_modules_path
+    end
+  end
+
+  context 'with an existing gov-one user' do
+    before do
+      create :user, :registered, gov_one_id: 'mock_sub'
+      get :openid_connect, params: params
+    end
+
+    it 'redirects to /my-modules' do
+      expect(session[:id_token]).to eq id_token
+      expect(response).to redirect_to my_modules_path
     end
   end
 end
