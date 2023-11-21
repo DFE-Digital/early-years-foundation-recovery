771550c1cfd0c40e3abaa7ca4149da1fadb58400
jack.coggin <jack.coggin@and.digital>
2023-11-21 10:03:51 +0000

    mock session nonce in controller spec

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe Users::OmniauthCallbacksController, type: :controller do
   before do
     request.env['devise.mapping'] = Devise.mappings[:user]
     session[:gov_one_auth_state] = 'mock_state'
+    session[:gov_one_auth_nonce] = 'mock_nonce'
 
     allow(GovOneAuthService).to receive(:new).and_return(auth_service)
     allow(auth_service).to receive(:tokens).and_return({ 'access_token' => access_token, 'id_token' => id_token })
