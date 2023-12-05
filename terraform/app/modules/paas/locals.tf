cd83ebc9829862aa8873ade5574651e30fe55a00
jack.coggin <jack.coggin@and.digital>
2023-12-05 12:42:16 +0000

    Merge remote-tracking branch 'origin/main' into new-module-indicator

--- terraform/app/modules/paas/locals.tf
+++
@@ -1,8 +0,0 @@
-# ------------------------------------------------------------------------------
-locals {
-  app_cf_service_instances = [cloudfoundry_service_instance.postgres_instance.id]
-  app_service_bindings     = concat(local.app_cf_service_instances)
-  db_service_name          = "${var.web_app_name}-db"
-  db_json_params           = jsonencode(var.postgres_json_params)
-  enable_worker            = var.app_environment == "content" ? false : true
-}
