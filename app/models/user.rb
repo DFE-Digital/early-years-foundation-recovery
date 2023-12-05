f0b41337fc6b80d8a298773e41741fb7e2449e76
jack.coggin <jack.coggin@and.digital>
2023-12-05 12:51:18 +0000

    revert erroneous changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -91,12 +91,12 @@ class User < ApplicationRecord
   scope :last_visit_4_weeks_ago, -> { where(id: month_old_visits).where.not(id: visits_within_month) }
 
   # emails
-  scope :training_email_recipients, -> { where(training_emails: [true, nil]) }
-  scope :early_years_email_recipients, -> { where(early_years_emails: true) }
+  scope :training_email_recipients, -> { order(:id).where(training_emails: [true, nil]) }
+  scope :early_years_email_recipients, -> { order(:id).where(early_years_emails: true) }
   scope :start_training_mail_job_recipients, -> { order(:id).training_email_recipients.month_old_confirmation.registration_complete.not_started_training }
   scope :complete_registration_mail_job_recipients, -> { order(:id).training_email_recipients.month_old_confirmation.registration_incomplete }
   scope :continue_training_mail_job_recipients, -> { order(:id).training_email_recipients.last_visit_4_weeks_ago.distinct(&:course_in_progress?) }
-  scope :new_module_mail_job_recipients, -> { order(:id).training_email_recipients.completed_available_modules.to_a }
+  scope :new_module_mail_job_recipients, -> { order(:id).training_email_recipients }
 
   # data
   scope :dashboard, -> { not_closed }
