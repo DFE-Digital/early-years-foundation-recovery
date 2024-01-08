5ca52970a2e3c81f5f1085d1b23f23df2cab74da
Peter Hamilton <pete@peterdavidhamilton.com>
2024-01-08 16:43:23 +0000

    ER-872 GovOne Banner (#1038)
    * Add banner regarding change to Gov One authentication
    
    * Update link to external content

--- spec/system/front_page_spec.rb
+++ spec/system/front_page_spec.rb
@@ -12,9 +12,17 @@ RSpec.describe 'Front page' do
       if Rails.application.gov_one_login?
         expect(page).not_to have_text 'Learn more about this training'
         expect(page).not_to have_text 'Start your training now'
+
+        # banner
+        expect(page).not_to have_text 'Access to this website is changing'
+        expect(page).not_to have_link href: 'https://www.gov.uk/using-your-gov-uk-one-login'
       else
         expect(page).not_to have_text 'Learn more and enrol'
         expect(page).not_to have_text 'Sign in to continue learning'
+
+        # banner
+        expect(page).to have_text 'Access to this website is changing'
+        expect(page).to have_link href: 'https://www.gov.uk/using-your-gov-uk-one-login'
       end
     end
   end
@@ -26,9 +34,17 @@ RSpec.describe 'Front page' do
       if Rails.application.gov_one_login?
         expect(page).to have_text 'Learn more about this training'
         expect(page).to have_text 'Start your training now'
+
+        # banner
+        expect(page).not_to have_text 'Access to this website is changing'
+        expect(page).not_to have_link href: 'https://www.gov.uk/using-your-gov-uk-one-login'
       else
         expect(page).to have_text 'Learn more and enrol'
         expect(page).to have_text 'Sign in to continue learning'
+
+        # banner
+        expect(page).to have_text 'Access to this website is changing'
+        expect(page).to have_link href: 'https://www.gov.uk/using-your-gov-uk-one-login'
       end
     end
   end
