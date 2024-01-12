8f32c88a312fd83b752683a2099c48b2927c5599
Katherine Martin <78093815+martikat@users.noreply.github.com>
2024-01-12 13:39:49 +0000

    wip: Update to include other text field

---
+++ spec/system/opinion_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'End of module feedback form' do
+  include_context 'with progress'
+  include_context 'with user'
+
+  it '' do
+    visit '/modules/alpha/questionnaires/feedback2'
+    expect(page).to have_content('Did the module meet your expectations')
+  end
+end
