08897f4345f0adfa2c74cd88453b79288aa4aa64
Peter David Hamilton <pete@peterdavidhamilton.com>
2024-01-15 11:39:04 +0000

    WiP

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -41,13 +41,6 @@ module ContentHelper
                 aria: { label: "#{icon} icon" }
   end
 
-  # @return [String]
-  def print_button(*additional_classes)
-    button = '<button class="govuk-link gem-c-print-link__button" onclick="window.print()" data-module="print-link" >Print this page</button>'.html_safe
-    classes = ['gem-c-print-link', 'print-button'] + additional_classes
-    content_tag :div, button, class: classes
-  end
-
   # @param success [Boolean]
   # @param score [Integer]
   # @return [String]
--- config/initializers/grover.rb
+++ config/initializers/grover.rb
@@ -1,6 +1,16 @@
+# https://github.com/Studiosity/grover/blob/main/README.md#debugging
+# The headless option disabled is not compatible with exporting of the PDF.
+#
 Grover.configure do |config|
   config.options = {
     executable_path: '/usr/bin/chromium-browser',
     launch_args: ['--disable-dev-shm-usage'],
+    format: 'A4',
+    margin: {
+      top: '20px',
+      bottom: '20px',
+      left: '20px',
+      right: '20px',
+    },
   }
 end
