7339c68a9fa34e5f28ee0c023af9b3ccfcbd76d8
jack.coggin <jack.coggin@and.digital>
2024-01-15 14:13:17 +0000

    update seeds snippets spec locales count

--- spec/lib/seed_snippets_spec.rb
+++ spec/lib/seed_snippets_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SeedSnippets do
   subject(:locales) { described_class.new.call }
 
   it 'converts all translations' do
-    expect(locales.count).to be 208
+    expect(locales.count).to be 209
   end
 
   it 'dot separated key -> Page::Resource#name' do
