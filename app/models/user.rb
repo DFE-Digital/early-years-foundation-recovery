719268ee6eb12a42849eab7a747cae00a0227765
jack.coggin <jack.coggin@and.digital>
2024-02-12 13:44:25 +0000

    add one login to user data export

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
     registration_complete_any?
     registered_at
     terms_and_conditions_agreed_at
+    gov_one?
   ].freeze
 
   # @return [Array<String>]
@@ -161,6 +162,11 @@ class User < ApplicationRecord
   # @return [Boolean]
   def notes?
     notes.any?(&:filled?)
+  end
+
+  # @return [Boolean]
+  def gov_one?
+    !self.gov_one_id.nil?
   end
 
   # @see Devise database_authenticatable
