c655e2132fac3923d6b29b85502d5374624fcf59
jack.coggin <jack.coggin@and.digital>
2024-02-22 15:52:48 +0000

    update migrations

--- db/migrate/20240207134812_change_responses_user_id_to_null.rb
+++
@@ -1,5 +0,0 @@
-class ChangeResponsesUserIdToNull < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :responses, :user_id, true
-  end
-end
--- db/migrate/20240209141502_add_text_input_to_responses.rb
+++
@@ -1,5 +0,0 @@
-class AddTextInputToResponses < ActiveRecord::Migration[7.0]
-  def change
-    add_column :responses, :text_input, :text
-  end
-end
