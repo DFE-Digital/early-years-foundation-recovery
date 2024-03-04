9eb9dd6508880090082e498d2791bb2c7716802a
jack.coggin <jack.coggin@and.digital>
2024-03-04 16:07:34 +0000

    add form builder link for yard docs

--- app/forms/form_builder.rb
+++ app/forms/form_builder.rb
@@ -1,3 +1,4 @@
+# @see https://govuk-form-builder.netlify.app/
 class FormBuilder < GOVUKDesignSystemFormBuilder::FormBuilder
   def govuk_text_field(attribute_name, options = {})
     super(attribute_name, **options.reverse_merge(width: 'two-thirds'))
