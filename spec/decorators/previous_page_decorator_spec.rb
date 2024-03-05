4ddc09471a3ff7255925d31bcb7f890babef9554
Peter David Hamilton <pete@peterdavidhamilton.com>
2024-03-05 15:59:48 +0000

    Unit test for previous page

--- spec/decorators/previous_page_decorator_spec.rb
+++ spec/decorators/previous_page_decorator_spec.rb
@@ -1,3 +1,13 @@
+# Helps to make notes:
+#
+#   feedback-intro (opinion_intro)
+#   end-of-module-feedback-1 (feedback)
+#   end-of-module-feedback-3 (feedback)
+#   feedback-freetext (feedback)
+#   end-of-module-feedback-5 (feedback) <-- SKIPPABLE
+#   1-3-3-5 (thankyou)
+#
+#
 require 'rails_helper'
 
 RSpec.describe PreviousPageDecorator do
@@ -10,31 +20,40 @@ RSpec.describe PreviousPageDecorator do
   let(:content) { mod.page_by_name('1-1-2') }
   let(:assessment) { double }
 
-  it '#name' do
-    expect(decorator.name).to eq '1-1-1'
-  end
-
-  it '#text' do
-    expect(decorator.text).to eq 'Previous'
-  end
-
-  context 'when starting a section' do
-    let(:content) { mod.page_by_name('1-2') }
-
-    it '#text' do
+  describe '#text' do
+    it do
       expect(decorator.text).to eq 'Previous'
     end
   end
 
-  context 'when finishing a module and skipping feedback form' do
-    let(:content) { mod.page_by_name('1-3-3-5') }
-
-    it '#text' do
-      expect(decorator.text).to eq 'Previous'
+  describe '#name' do
+    it 'is previous page name' do
+      expect(decorator.name).to eq '1-1-1'
     end
 
-    it '#name' do
-      expect(decorator.name).to eq 'feedback-intro'
+    context 'when previous page is skippable' do
+      let(:content) { mod.page_by_name('1-3-3-5') }
+
+      context 'and unanswered' do
+        it 'is one step back' do
+          expect(decorator.name).to eq 'end-of-module-feedback-5'
+        end
+      end
+
+      context 'and answered' do
+        before do
+          create :response,
+            question_name: 'end-of-module-feedback-5',
+            training_module: 'alpha',
+            answers: [1],
+            correct: true,
+            user: create(:user)
+        end
+
+        it 'is two steps back' do
+          expect(decorator.name).to eq 'feedback-freetext'
+        end
+      end
     end
   end
 end
