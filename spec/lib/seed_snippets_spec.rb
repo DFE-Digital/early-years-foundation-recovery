165099ff9c45043a663423d619571478506c6921
jack.coggin <jack.coggin@and.digital>
2024-03-12 13:39:53 +0000

    update seeds snippets spec

--- spec/lib/seed_snippets_spec.rb
+++ spec/lib/seed_snippets_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe SeedSnippets do
   subject(:locales) { described_class.new.call }
 
   it 'converts all translations' do
-    expect(locales.count).to be 208
+    expect(locales.count).to be 201
   end
 
   it 'dot separated key -> Page::Resource#name' do
-    expect(locales.first[:name]).to eq 'activemodel.errors.models.user.attributes.current_password.blank'
+    expect(locales.first[:name]).to eq 'activemodel.errors.models.user.attributes.first_name.blank'
   end
 
   it 'value -> Page::Resource#body' do
-    expect(locales.first[:body]).to eq 'Enter your current password.'
+    expect(locales.first[:body]).to eq 'Enter a first name.'
   end
 end
