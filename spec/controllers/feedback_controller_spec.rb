39b36e9db628ccf4c2c808056614462c393b3459
jack.coggin <jack.coggin@and.digital>
2024-03-14 17:10:14 +0000

    fix feedback controller spec params and add tests for tracking

--- spec/controllers/feedback_controller_spec.rb
+++ spec/controllers/feedback_controller_spec.rb
@@ -4,14 +4,26 @@ RSpec.describe FeedbackController, type: :controller do
   context 'when user is signed in' do
     let(:user) { create :user, :registered }
     let(:valid_attributes) do
-      { id: 1, answers: %w[Yes], answers_custom: 'Custom answer', training_module: nil, question_name: 'feedback-radiobutton' }
+      {
+        id: 'feedback-radiobutton',
+        training_module: nil,
+        response: {
+          answers: %w[Yes],
+          answers_custom: 'Custom answer',
+        },
+      }
     end
 
     before { sign_in user }
 
     describe 'GET #show' do
+      it 'tracks feedback start' do
+        expect(controller).to receive(:track_feedback_start)
+        get :show, params: valid_attributes
+      end
+
       it 'returns a success response' do
-        get :show, params: { id: 1 }
+        get :show, params: valid_attributes
         expect(response).to be_successful
       end
     end
@@ -34,6 +46,11 @@ RSpec.describe FeedbackController, type: :controller do
         it 'redirects to the next feedback path' do
           post :update, params: valid_attributes
           expect(response).to redirect_to(feedback_path(2))
+        end
+
+        it 'tracks feedback complete' do
+          expect(controller).to receive(:track_feedback_complete)
+          post :update, params: valid_attributes
         end
       end
 
