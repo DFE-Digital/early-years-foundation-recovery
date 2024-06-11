b7b065a6daac4c780577c74526605ea8e550c8eb
Peter Hamilton <pete@peterdavidhamilton.com>
2024-06-11 10:46:14 +0100

    ER-1218 closing GovOne accounts (#1219)
    - Fix JS functions
    - Prevent closed accounts from logging back in
    - All markdown in account flash messages
    - Add spec for altering GovOne id when closing accounts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,19 +3,8 @@ import '@fortawesome/fontawesome-free/js/all';
 
 import './controllers';
 
-import { initAll } from "govuk-frontend";
+import { initAll } from 'govuk-frontend';
 
-function nodeListForEach (nodes, callback) {
-  if (window.NodeList.prototype.forEach) {
-    return nodes.forEach(callback)
-  }
-  for (var i = 0; i < nodes.length; i++) {
-    callback.call(window, nodes[i], i, nodes)
-  }
-}
-
-/*
 document.addEventListener('turbo:load', function() {
   initAll();
 })
-*/
