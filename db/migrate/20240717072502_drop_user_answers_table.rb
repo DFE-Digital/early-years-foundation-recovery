2f5fce411504ee6977d469a547bb4c8303fe87b2
Katherine Martin <78093815+martikat@users.noreply.github.com>
2024-07-17 08:51:11 +0100

    Fix rubocop errors

--- db/migrate/20240717072502_drop_user_answers_table.rb
+++ db/migrate/20240717072502_drop_user_answers_table.rb
@@ -1,5 +1,18 @@
 class DropUserAnswersTable < ActiveRecord::Migration[7.1]
-  def change
+  def up
     drop_table :user_answers
+  end
+
+  def down
+    create_table :user_answers do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :questionnaire_id, null: false, index: true
+      t.string :question
+      t.string :answer
+      t.boolean :correct
+      t.boolean :archived
+      t.index %i[questionnaire_id user_id]
+      t.timestamps
+    end
   end
 end
