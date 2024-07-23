09fe371033222a8314e9b562f8b0e786f5e144b7
Katherine Martin <78093815+martikat@users.noreply.github.com>
2024-07-23 09:52:33 +0100

    WIP: update based on PR comments and spec update

--- app/models/training/page.rb
+++ app/models/training/page.rb
@@ -5,11 +5,6 @@ module Training
       'page'
     end
 
-    # @return [Boolean]
-    def updated_content?
-      created_at < updated_at
-    end
-
     # @return [Array<String, Hash>]
     def schema
       [name, page_type, heading, (notes? ? { note: 'hello world' } : Types::EMPTY_HASH)]
