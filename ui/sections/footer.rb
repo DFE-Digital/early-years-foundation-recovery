5de737dfb55222eba16b3602ff2a8bd6e6e2116e
Peter Hamilton <pete@peterdavidhamilton.com>
2024-07-23 13:33:55 +0100

    Retire old and unused site prism specs and related overhead (#1281)
    * Remove unused browser checks
    * Update README
    * Update Dockerfile and Gemfile

--- ui/sections/footer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Sections
-  class Footer < SitePrism::Section
-    element :terms_and_conditions, "a[href='/terms-and-conditions']", text: 'Terms and conditions'
-  end
-end
