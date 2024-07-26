b7dd4004fa248d73590b8fbb0e9d9dcdca6b7703
Peter David Hamilton <pete@peterdavidhamilton.com>
2024-07-26 10:28:49 +0100

    Update lying spec!

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -12,8 +12,11 @@ RSpec.describe ApplicationController, type: :controller do
     end
 
     context 'with a browser' do
+      let(:current_visit) { create(:visit) }
+
       it 'uses the current visit' do
-        allow(controller).to receive(:current_visit).and_return(create(:visit))
+        allow(controller).to receive(:current_visit).and_return(current_visit)
+        expect(guest.visit.id).to eq current_visit.id
         expect(guest).to be_a Guest
       end
     end
@@ -21,10 +24,12 @@ RSpec.describe ApplicationController, type: :controller do
     context 'with a cookie' do
       let(:cookie_token) { 'some-token' }
 
-      it 'fetches a previous visit' do
-        allow(controller).to receive(:current_visit).and_return(create(:visit))
+      before do
         create(:visit, visit_token: cookie_token)
         request.cookies[:course_feedback] = cookie_token
+      end
+
+      it 'fetches a previous visit' do
         expect(guest).to be_a Guest
         expect(guest.visit_token).to eq cookie_token
       end
