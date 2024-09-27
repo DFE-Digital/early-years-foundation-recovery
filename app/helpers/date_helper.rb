6831cdf5030b19f6b72560db9ef4115dfa6ccf5d
David-DfE <45995847+David-Feetenby@users.noreply.github.com>
2024-09-27 10:34:58 +0100

    Revert "Er 1219 add updated and published date"

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,7 +1,6 @@
 module DateHelper
-  # @param datetime [DateTime]
   # @return [String]
-  def format_date(datetime)
+  def certificate_date(datetime)
     datetime.to_date.strftime('%-d %B %Y')
   end
 end
