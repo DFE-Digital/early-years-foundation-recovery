8af97f4539b4ee723db69d8b550bf97d57781ad8
dfeetenby <David.Feetenby@digital.education.gov.uk>
2024-10-07 15:52:09 +0100

    Add refresh to before_action if refresh is present
    Added a before_action to application controller if a refresh parameter is present so that we can ensure the redirect to my_modules isn't interrupted.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
+  controller do
+    # Add an index action for testing purposes
+    def index
+      render plain: "Test page"
+    end
+  end
+
   describe '#guest' do
     subject(:guest) { controller.send(:guest) }
 
@@ -33,6 +40,26 @@ RSpec.describe ApplicationController, type: :controller do
         expect(guest).to be_a Guest
         expect(guest.visit_token).to eq cookie_token
       end
+    end
+  end
+
+  # New block for testing refresh param handling
+  describe 'GET #index with refresh param' do
+    it 'redirects and removes the refresh param' do
+      # Simulate a request with the refresh parameter
+      get :index, params: { refresh: true }
+
+      # Check that the response is a redirect
+      expect(response).to have_http_status(:found)
+
+      # Check that it redirects to the correct path
+      expect(response).to redirect_to('/anonymous')
+
+      # Simulate the follow-up request after the redirect
+      get :index
+
+      # Ensure that the refresh parameter is not in the final request
+      expect(request.query_parameters[:refresh]).to be_nil
     end
   end
 end
