f80cbdb4acb41cc275a2575c588d47fef63f3414
David-DfE <45995847+david-dfe@users.noreply.github.com>
2025-07-29 14:20:08 +0100

    Add not applicable to role type when choosing other. (#1543)
    Co-authored-by: dfeetenby <David.Feetenby@digital.education.gov.uk>

--- app/forms/registration/setting_type_other_form.rb
+++ app/forms/registration/setting_type_other_form.rb
@@ -12,6 +12,7 @@ module Registration
         setting_type: 'other',
         setting_type_id: 'other',
         setting_type_other: setting_type_other,
+        role_type: I18n.t(:na),
         local_authority: I18n.t(:na),
       )
     end
