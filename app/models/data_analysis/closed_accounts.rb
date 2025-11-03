cf88ca5719aa3550607924583218081d1dee9553
dfeetenby <David.Feetenby@digital.education.gov.uk>
2025-11-03 14:06:35 +0000

    Add user id to closed_accounts

--- app/models/data_analysis/closed_accounts.rb
+++ app/models/data_analysis/closed_accounts.rb
@@ -6,6 +6,7 @@ module DataAnalysis
       # @return [Array<String>]
       def column_names
         [
+          'User ID',
           'Closed',
           'Setting',
           'Custom setting',
@@ -20,6 +21,7 @@ module DataAnalysis
       def dashboard
         User.closed.map do |user|
           {
+            user_id: user.id,
             closed_at: user.closed_at.strftime('%Y-%m-%d %H:%M:%S'),
             setting_type: user.setting_type,
             setting_type_other: user.setting_type_other,
