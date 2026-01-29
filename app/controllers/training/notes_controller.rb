eeaeb78ef16650a047f5abde0b5d437a62ea80f7
David-DfE <45995847+david-dfe@users.noreply.github.com>
2026-01-29 14:42:16 +0000

    wip

--- app/controllers/training/notes_controller.rb
+++ app/controllers/training/notes_controller.rb
@@ -1,4 +1,3 @@
-
 class Training::NotesController < ApplicationController
   include Learning
 
@@ -7,51 +6,51 @@ class Training::NotesController < ApplicationController
   helper_method :note,
                 :content
 
-    # GET /my-account/learning-log
-    def show
-      @training_modules = current_user.active_modules
-      render 'notes/show'
-    end
+  # GET /my-account/learning-log
+  def show
+    @training_modules = current_user.active_modules
+    render 'notes/show'
+  end
 
   # POST /my-account/learning-log
-    def create
-      nav_keys = %i[next_page_name next_page_module module_item_id]
-      attrs = note_params.except(*nav_keys)
-      note_instance = existing_note || Note.new(attrs)
-      if note_instance.save
-        track('user_note_created')
-        redirect_to next_page_path
-      else
-        Rails.logger.error("Learning log save failed for user #{current_user.id}: #{note_instance.errors.full_messages.join(', ')}")
-        render_current_page
-      end
+  def create
+    nav_keys = %i[next_page_name next_page_module module_item_id]
+    attrs = note_params.except(*nav_keys)
+    note_instance = existing_note || Note.new(attrs)
+    if note_instance.save
+      track('user_note_created')
+      redirect_to next_page_path
+    else
+      Rails.logger.error("Learning log save failed for user #{current_user.id}: #{note_instance.errors.full_messages.join(', ')}")
+      render_current_page
     end
+  end
 
   # PATCH/PUT /my-account/learning-log
-    def update
-      nav_keys = %i[next_page_name next_page_module module_item_id]
-      attrs = note_params.except(*nav_keys)
-      if note.update(attrs)
-        track('user_note_updated')
-        redirect_to next_page_path
-      else
-        Rails.logger.error("Learning log update failed for user #{current_user.id}: #{note.errors.full_messages.join(', ')}")
-        render_current_page
-      end
+  def update
+    nav_keys = %i[next_page_name next_page_module module_item_id]
+    attrs = note_params.except(*nav_keys)
+    if note.update(attrs)
+      track('user_note_updated')
+      redirect_to next_page_path
+    else
+      Rails.logger.error("Learning log update failed for user #{current_user.id}: #{note.errors.full_messages.join(', ')}")
+      render_current_page
     end
+  end
 
 private
 
   # @see Learning
   # @return [String]
   def content_name
-      note_params[:name]
+    note_params[:name]
   end
 
   # @see Learning
   # @return [String]
   def mod_name
-      note_params[:training_module]
+    note_params[:training_module]
   end
 
   # @return [String]
@@ -68,28 +67,28 @@ private
 
   # defensive
   def render_current_page
-      render "training/pages/#{params[:page_type] || 'content'}", local: { note: note }
+    render "training/pages/#{params[:page_type] || 'content'}", local: { note: note }
   end
 
   # @return [Note]
   def note
-      nav_keys = %i[next_page_name next_page_module module_item_id]
-      attrs = note_params.except(*nav_keys)
-      existing_note || Note.new(attrs)
+    nav_keys = %i[next_page_name next_page_module module_item_id]
+    attrs = note_params.except(*nav_keys)
+    existing_note || Note.new(attrs)
   end
 
   # @return [Note]
   def existing_note
-      current_user.notes.find_by(training_module: note_params[:training_module], name: note_params[:name])
+    current_user.notes.find_by(training_module: note_params[:training_module], name: note_params[:name])
   end
 
   # @see Tracking
   # @return [Hash]
   def tracking_properties
-      {
-        length: note_params[:body].length,
-        **note_params.except(:body, :module_item_id, :user),
-      }
+    {
+      length: note_params[:body].length,
+      **note_params.except(:body, :module_item_id, :user),
+    }
   end
 
   def note_params
