b0612e5cbe1f7d08326bdc6f717f75b355c4f89a
David-DfE <45995847+david-dfe@users.noreply.github.com>
2026-01-29 13:35:25 +0000

    Refactor notes controller and form to include next page navigation fields so that we dont make contentful calls upon save/update

--- app/controllers/training/notes_controller.rb
+++ app/controllers/training/notes_controller.rb
@@ -1,3 +1,4 @@
+
 class Training::NotesController < ApplicationController
   include Learning
 
@@ -6,88 +7,94 @@ class Training::NotesController < ApplicationController
   helper_method :note,
                 :content
 
-  # GET /my-account/learning-log
-  def show
-    @training_modules = current_user.active_modules
-    render 'notes/show'
-  end
+    # GET /my-account/learning-log
+    def show
+      @training_modules = current_user.active_modules
+      render 'notes/show'
+    end
 
   # POST /my-account/learning-log
-  def create
-    if note.save
-      track('user_note_created')
-      redirect_to next_page_path
-    else
-      Rails.logger.error("Learning log save failed for user #{current_user.id}: #{note.errors.full_messages.join(', ')}")
-      render_current_page
+    def create
+      nav_keys = %i[next_page_name next_page_module module_item_id]
+      attrs = note_params.except(*nav_keys)
+      note_instance = existing_note || Note.new(attrs)
+      if note_instance.save
+        track('user_note_created')
+        redirect_to next_page_path
+      else
+        Rails.logger.error("Learning log save failed for user #{current_user.id}: #{note_instance.errors.full_messages.join(', ')}")
+        render_current_page
+      end
     end
-  rescue StandardError => e
-    Rails.logger.error("Learning log create exception for user #{current_user.id}: #{e.class} - #{e.message}")
-    raise
-  end
 
   # PATCH/PUT /my-account/learning-log
-  def update
-    if note.update(note_params.except(:module_item_id))
-      track('user_note_updated')
-      redirect_to next_page_path
-    else
-      Rails.logger.error("Learning log update failed for user #{current_user.id}: #{note.errors.full_messages.join(', ')}")
-      render_current_page
+    def update
+      nav_keys = %i[next_page_name next_page_module module_item_id]
+      attrs = note_params.except(*nav_keys)
+      if note.update(attrs)
+        track('user_note_updated')
+        redirect_to next_page_path
+      else
+        Rails.logger.error("Learning log update failed for user #{current_user.id}: #{note.errors.full_messages.join(', ')}")
+        render_current_page
+      end
     end
-  rescue StandardError => e
-    Rails.logger.error("Learning log update exception for user #{current_user.id}: #{e.class} - #{e.message}")
-    raise
-  end
 
 private
 
   # @see Learning
   # @return [String]
   def content_name
-    note_params[:name]
+      note_params[:name]
   end
 
   # @see Learning
   # @return [String]
   def mod_name
-    note_params[:training_module]
+      note_params[:training_module]
   end
 
   # @return [String]
   def next_page_path
-    training_module_page_path(mod.name, content.next_item.name)
+    # Prefer next page info from form, fallback to current page
+    if note_params[:next_page_module].present? && note_params[:next_page_name].present?
+      training_module_page_path(note_params[:next_page_module], note_params[:next_page_name])
+    elsif note_params[:training_module] && note_params[:name]
+      training_module_page_path(note_params[:training_module], note_params[:name])
+    else
+      user_notes_path
+    end
   end
 
   # defensive
   def render_current_page
-    render "training/pages/#{content.page_type}", local: { note: note }
+      render "training/pages/#{params[:page_type] || 'content'}", local: { note: note }
   end
 
   # @return [Note]
   def note
-    existing_note || Note.new(note_params.except(:module_item_id))
+      nav_keys = %i[next_page_name next_page_module module_item_id]
+      attrs = note_params.except(*nav_keys)
+      existing_note || Note.new(attrs)
   end
 
   # @return [Note]
   def existing_note
-    current_user.notes.find_by(training_module: mod.name, name: content.name)
+      current_user.notes.find_by(training_module: note_params[:training_module], name: note_params[:name])
   end
 
   # @see Tracking
   # @return [Hash]
   def tracking_properties
-    {
-      length: note_params[:body].length,
-      uid: content.id,
-      mod_uid: mod.id,
-      **note_params.except(:body, :module_item_id, :user),
-    }
+      {
+        length: note_params[:body].length,
+        **note_params.except(:body, :module_item_id, :user),
+      }
   end
 
   def note_params
     params.require(:note)
-      .permit(:title, :body, :training_module, :name, :module_item_id)
+      .permit(:title, :body, :training_module, :name, :module_item_id, :next_page_name, :next_page_module)
       .with_defaults(user: current_user)
   end
 end
