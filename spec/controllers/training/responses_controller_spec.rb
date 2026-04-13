46cd7ec1142f30d0e40aa0d176d1daadd7ad491e
David-DfE <45995847+david-dfe@users.noreply.github.com>
2026-04-13 16:20:12 +0100

    wip

--- spec/controllers/training/responses_controller_spec.rb
+++ spec/controllers/training/responses_controller_spec.rb
@@ -1,24 +1,19 @@
 require 'rails_helper'
 
-
 RSpec.describe Training::ResponsesController, type: :controller do
-  let(:user) { create(:user, :registered) }
-  let(:records) { Response.count }
-
   before do
-    sign_in user
+    sign_in create(:user, :registered)
+
+    patch :update, params: {
+      training_module_id: 'alpha',
+      id: question_name,
+      response: { answers: answers, text_input: 'Text input' },
+    }
   end
 
-  def patch_with_nonce(params, summative: false)
-    if summative
-      session[:form_nonce] = 'test-nonce'
-      patch :update, params: params.merge(submission_nonce: 'test-nonce')
-    else
-      patch :update, params: params
-    end
+  let(:records) do
+    Response.count
   end
-
-
 
   describe '#update' do
     context 'with formative' do
@@ -28,8 +23,6 @@ RSpec.describe Training::ResponsesController, type: :controller do
         context 'with correct answer' do
           let(:answers) { [1] }
 
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }) }
-
           specify { expect(response).to have_http_status(:redirect) }
           specify { expect(records).to be 1 }
         end
@@ -37,16 +30,12 @@ RSpec.describe Training::ResponsesController, type: :controller do
         context 'with incorrect answer' do
           let(:answers) { [2] }
 
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }) }
-
           specify { expect(response).to have_http_status(:redirect) }
           specify { expect(records).to be 1 }
         end
 
         context 'and no answer' do
           let(:answers) { nil }
-
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }) }
 
           specify { expect(response).to have_http_status(:unprocessable_content) }
           specify { expect(records).to be 0 }
@@ -59,8 +48,6 @@ RSpec.describe Training::ResponsesController, type: :controller do
         context 'with correct answers' do
           let(:answers) { [1, 3] }
 
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }) }
-
           specify { expect(response).to have_http_status(:redirect) }
           specify { expect(records).to be 1 }
         end
@@ -68,16 +55,12 @@ RSpec.describe Training::ResponsesController, type: :controller do
         context 'with incorrect answers' do
           let(:answers) { [1, 2] }
 
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }) }
-
           specify { expect(response).to have_http_status(:redirect) }
           specify { expect(records).to be 1 }
         end
 
         context 'and no answers' do
           let(:answers) { nil }
-
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }) }
 
           specify { expect(response).to have_http_status(:unprocessable_content) }
           specify { expect(records).to be 0 }
@@ -91,8 +74,6 @@ RSpec.describe Training::ResponsesController, type: :controller do
         context 'with text input' do
           let(:text_input) { 'Text input for feedback question' }
 
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: text_input } }) }
-
           specify { expect(response).to have_http_status(:redirect) }
           specify { expect(records).to be 1 }
         end
@@ -100,97 +81,8 @@ RSpec.describe Training::ResponsesController, type: :controller do
         context 'with no text input' do
           let(:text_input) { nil }
 
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: text_input } }) }
-
           specify { expect(response).to have_http_status(:redirect) }
           specify { expect(records).to be 1 }
-        end
-      end
-    end
-
-    context 'with summative' do
-      context 'when single choice (radio buttons)' do
-        let(:question_name) { '1-3-2-1' }
-
-        context 'with correct answer' do
-          let(:answers) { [1] }
-
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }, summative: true) }
-
-          specify { expect(response).to have_http_status(:redirect) }
-          specify { expect(records).to be 1 }
-        end
-
-        context 'with incorrect answer' do
-          let(:answers) { [2] }
-
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }, summative: true) }
-
-          specify { expect(response).to have_http_status(:redirect) }
-          specify { expect(records).to be 1 }
-        end
-
-        context 'and no answer' do
-          let(:answers) { nil }
-
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }, summative: true) }
-
-          specify { expect(response).to have_http_status(:unprocessable_content) }
-          specify { expect(records).to be 0 }
-        end
-      end
-
-      context 'when multiple choice (check boxes)' do
-        let(:question_name) { '1-3-2-2' }
-
-        context 'with correct answers' do
-          let(:answers) { [1, 3] }
-
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }, summative: true) }
-
-          specify { expect(response).to have_http_status(:redirect) }
-          specify { expect(records).to be 1 }
-        end
-
-        context 'with incorrect answers' do
-          let(:answers) { [1, 2] }
-
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }, summative: true) }
-
-          specify { expect(response).to have_http_status(:redirect) }
-          specify { expect(records).to be 1 }
-        end
-
-        context 'and no answers' do
-          let(:answers) { nil }
-
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: 'Text input' } }, summative: true) }
-
-          specify { expect(response).to have_http_status(:unprocessable_content) }
-          specify { expect(records).to be 0 }
-        end
-      end
-
-      context 'when the question expects text and is answered' do
-        let(:question_name) { 'summative-textarea-only' }
-        let(:answers) { [] }
-
-        context 'with text input' do
-          let(:text_input) { 'Text input for summative question' }
-
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: text_input } }, summative: true) }
-
-          specify { expect(response).to have_http_status(:redirect) }
-          specify { expect(records).to be 1 }
-        end
-
-        context 'with no text input' do
-          let(:text_input) { nil }
-
-          before { patch_with_nonce({ training_module_id: 'alpha', id: question_name, response: { answers: answers, text_input: text_input } }, summative: true) }
-
-          specify { expect(response).to have_http_status(:unprocessable_content) }
-          specify { expect(records).to be 0 }
         end
       end
     end
