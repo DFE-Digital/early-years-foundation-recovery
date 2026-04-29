76fe698b6beed6eb2af67b3627b59a03b1d90a12
martikat <78093815+martikat@users.noreply.github.com>
2026-04-29 14:28:22 +0100

    Update completed link update logic and remove spec

--- spec/system/module_overview_progress_spec.rb
+++ spec/system/module_overview_progress_spec.rb
@@ -181,31 +181,6 @@ RSpec.describe 'Module overview page progress' do
     specify { expect(page).not_to have_link 'Retake test' }
   end
 
-  context 'with just a completion event' do
-    before do
-      module_complete_event(alpha)
-      visit '/modules/alpha'
-    end
-
-    it 'all the indicators are "complete"' do
-      within '#section-content-1' do
-        expect(page).to have_content 'complete', count: 4
-      end
-
-      within '#section-content-2' do
-        expect(page).to have_content 'complete', count: 1
-      end
-
-      within '#section-content-3' do
-        expect(page).to have_content 'complete', count: 3
-      end
-
-      within '#section-content-4' do
-        expect(page).to have_content 'complete', count: 1
-      end
-    end
-  end
-
   context 'when controller logic for progress decorator is applied' do
     before do
       visit '/modules/alpha'
