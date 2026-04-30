01d4c5e7e0221e4320163d042135cf59619c0302
MathewBeldon <mathew.beldon@pm.me>
2026-04-30 13:37:30 +0100

    DTK-1308 - broken access (#1742)

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,4 @@ class Users::SessionsController < Devise::SessionsController
   def new
     render 'gov_one'
   end
-
-  # @return [String, nil]
-  def sign_in_test_user
-    sign_in_and_redirect User.test_user if User.test_user
-  end
 end
