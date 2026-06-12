9265d685fee38789bde1e6b6c50330928035f17b
David-DfE <45995847+david-dfe@users.noreply.github.com>
2026-06-12 11:27:20 +0100

    Harden CSP for ITHC 5.1.3 (remove unsafe-inline, add nonces)
    Tighten CSP to reduce XSS/data exfiltration risk from the pentest finding:
    
    remove unsafe-inline from script/style
    enable nonce-based inline analytics scripts
    set stricter explicit directives (incl. default-src 'none')
    replace inline style attributes so strict style-src can be enforced
    add report-only toggle (CSP_REPORT_ONLY) for safe rollout

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -13,10 +13,6 @@
 #
 # Add URLs in order to import images or javascript from another domain
 #
-GOVUK_DOMAINS = %w[
-  *.education.gov.uk
-].freeze
-
 GOOGLE_ANALYTICS_DOMAINS = %w[
   www.google-analytics.com
   ssl.google-analytics.com
@@ -51,65 +47,64 @@ Rails.application.config.content_security_policy do |policy|
   # @see https://www.contentful.com/developers/docs/tutorials/general/live-preview/#set-up-live-preview
   policy.frame_ancestors :self, 'https://app.contentful.com'
 
-  policy.default_src :self,
-                     :https,
-                     *GOVUK_DOMAINS
+  policy.default_src :none
+  policy.base_uri    :self
+  policy.form_action :self
+
   policy.font_src    :self,
-                     :https,
-                     *GOVUK_DOMAINS,
                      *GOOGLE_STATIC_DOMAINS,
                      :data
+
   policy.frame_src   :self,
                      *GOOGLE_ANALYTICS_DOMAINS,
                      *OPTIMIZE_DOMAINS
+
   policy.img_src     :self,
-                     *GOVUK_DOMAINS,
                      *GOOGLE_ANALYTICS_DOMAINS, # Tracking pixels
                      *OPTIMIZE_DOMAINS,
                      *CLARITY_DOMAINS,
-                     '*.ctfassets.net',
+                     'images.ctfassets.net',
                      'github.com', # eyrecovery-dev.azurewebsites.net
                      :data # Base64 encoded images
 
+  policy.media_src   :self,
+                     'player.vimeo.com',
+                     'i.vimeocdn.com'
+
   policy.object_src  :none
+
   policy.script_src  :self,
-                     *GOVUK_DOMAINS,
                      *GOOGLE_ANALYTICS_DOMAINS,
                      *GOOGLE_STATIC_DOMAINS,
                      *OPTIMIZE_DOMAINS,
-                     *CLARITY_DOMAINS,
-                     # Allow all inline scripts until we can conclusively
-                     # document all the inline scripts we use,
-                     # and there's a better way to filter out junk reports
-                     :unsafe_inline
+                     *CLARITY_DOMAINS
+
   policy.style_src   :self,
-                     *GOVUK_DOMAINS,
                      *GOOGLE_STATIC_DOMAINS,
-                     *OPTIMIZE_DOMAINS,
-                     :unsafe_inline
+                     *OPTIMIZE_DOMAINS
 
   webpack_dev_server = %w[http://localhost:3035 ws://localhost:3035] if Rails.env.development?
 
   policy.connect_src :self,
-                     :https,
-                     :wss,
-                     *GOVUK_DOMAINS,
                      *GOOGLE_ANALYTICS_DOMAINS,
                      *CLARITY_DOMAINS,
                      *webpack_dev_server.to_a
+
+  policy.upgrade_insecure_requests
+  policy.block_all_mixed_content
 end
 
 # If you are using UJS then enable automatic nonce generation
 #
-# Rails.application.config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
+Rails.application.config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
 #
 # Set the nonce only to specific directives
 #
-# Rails.application.config.content_security_policy_nonce_directives = %w(script-src)
+Rails.application.config.content_security_policy_nonce_directives = %w[script-src style-src]
 #
 # Report CSP violations to a specified URI
 # For further information see the following documentation
 #
 # - {https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only}
 #
-# Rails.application.config.content_security_policy_report_only = true
+Rails.application.config.content_security_policy_report_only = ENV.fetch('CSP_REPORT_ONLY', (!Rails.env.production?).to_s) == 'true'
