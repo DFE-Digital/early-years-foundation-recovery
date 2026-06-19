bf2f52aff81dcc49dceb62f9d6b7c7fbba8e7375
David-DfE <45995847+david-dfe@users.noreply.github.com>
2026-06-19 10:20:59 +0100

    Increase log retention time to 180 days

--- terraform-azure/terraform-azure-monitor/logs.tf
+++ terraform-azure/terraform-azure-monitor/logs.tf
@@ -4,7 +4,7 @@ resource "azurerm_log_analytics_workspace" "log_analytics" {
   location            = var.location
   resource_group_name = var.resource_group
   sku                 = "PerGB2018"
-  retention_in_days   = 30
+  retention_in_days   = 180
   daily_quota_gb      = 1
 
   lifecycle {
