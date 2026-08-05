b329e26595ae7ea1c8f6e47923b565e24b2b483b
Katherine Martin <78093815+martikat@users.noreply.github.com>
2026-08-05 13:35:35 +0100

    EYCDTK-1397 Update continue module email trigger (#1823)

--- spec/jobs/continue_training_mail_job_spec.rb
+++ spec/jobs/continue_training_mail_job_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe ContinueTrainingMailJob do
   include_context 'with progress'
 
-  let(:user) { create(:user, :registered, confirmed_at: 4.weeks.ago) }
+  let(:user) { create(:user, :registered, confirmed_at: 7.days.ago) }
 
   let(:user_two) do
     create :user, :registered,
-           confirmed_at: 4.weeks.ago,
+           confirmed_at: 7.days.ago,
            module_time_to_completion: { alpha: 0 }
   end
 
   let(:user_three) do
     create :user, :registered,
-           confirmed_at: 4.weeks.ago,
+           confirmed_at: 7.days.ago,
            module_time_to_completion: { alpha: 0 }
   end
 
@@ -29,28 +29,28 @@ RSpec.describe ContinueTrainingMailJob do
            id: 9,
            visitor_token: '345',
            user_id: user.id,
-           started_at: 4.weeks.ago
+           started_at: 7.days.ago
 
     create :visit,
            id: 7,
            visitor_token: '123',
            user_id: user_two.id,
-           started_at: 4.weeks.ago
+           started_at: 7.days.ago
 
     create :visit,
            id: 8,
            visitor_token: '234',
            user_id: user_two.id,
-           started_at: 2.weeks.ago
+           started_at: 2.days.ago
 
     create :visit,
            id: 10,
            visitor_token: '456',
            user_id: user_three.id,
-           started_at: 5.weeks.ago
+           started_at: 2.weeks.ago
 
-    # Travel to 4 weeks ago so that the module start event won't count as a recent visit
-    travel_to 4.weeks.ago
+    # Travel to 7 days ago so that the module start event won't count as a recent visit
+    travel_to 7.days.ago
     start_module(alpha)
     travel_back
   end
