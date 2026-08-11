f3b36167927dd05289761a6e92d2d8dbb0f7a600
Katherine Martin <78093815+martikat@users.noreply.github.com>
2026-08-11 13:27:26 +0100

    EYCDTK-1554 Create new important banner (#1811)

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,4 +4,22 @@ describe 'ApplicationHelper', type: :helper do
   it '#html_title' do
     expect(helper.html_title(:foo, 'bar')).to eq 'Early years child development training : foo : bar'
   end
+
+  describe '#show_important_banner' do
+    context 'when SHOW_IMPORTANT_BANNER is true' do
+      before { allow(ENV).to receive(:[]).with('SHOW_IMPORTANT_BANNER').and_return('true') }
+
+      it 'returns true' do
+        expect(helper.show_important_banner?).to be true
+      end
+    end
+
+    context 'when SHOW_IMPORTANT_BANNER is false' do
+      before { allow(ENV).to receive(:[]).with('SHOW_IMPORTANT_BANNER').and_return('false') }
+
+      it 'returns false' do
+        expect(helper.show_important_banner?).to be false
+      end
+    end
+  end
 end
