1b515710cf63c4c3802991572409ac76bddb3bcb
David-DfE <45995847+david-dfe@users.noreply.github.com>
2026-09-02 14:32:27 +0100

    Refactor JavaScript class assignment to application.js and remove inline script from layouts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,5 +6,6 @@ import './controllers';
 import { initAll } from 'govuk-frontend';
 
 document.addEventListener('turbo:load', function() {
+  document.body.classList.add('js-enabled', 'govuk-frontend-supported');
   initAll();
 })
