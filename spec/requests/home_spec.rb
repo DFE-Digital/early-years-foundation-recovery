698ec2d68a39be545d93545dada048fd82698298
Jonas-AND <135846299+Jonas-AND@users.noreply.github.com>
2026-09-07 16:51:11 +0100

    EYCDTK-1636 Show dynamic module count on the home page (#1846)
    * Show dynamic live module count on the home page
    
    * Fix the test due to updating en.yml

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe 'Homes', type: :request do
   specify { expect('/').to be_successful }
 
   specify { expect('/health').to be_successful }
+
+  it 'shows the number of live modules' do
+    get root_path
+
+    expected_summary = I18n.t(
+      'home.modules_summary',
+      count: Training::Module.live.count,
+    )
+    expect(response.body).to include(expected_summary)
+  end
 end
